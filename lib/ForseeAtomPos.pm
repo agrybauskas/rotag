a3ce9efa9dbb99e70e33f8cedbb1e3b6ae9f21f5
algirdas <algirdas@lama>
2016-10-14 14:57:45 +0000

    trunk/ (algirdas@tasmanijos-velnias) Fixed missplaced up, mid and side atom coordinates.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@15 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/ForseeAtomPos.pm
+++ lib/ForseeAtomPos.pm
@@ -14,25 +14,25 @@ use warnings;
 #
 
 sub create_ref_frame{
-    my ($up_atom_x,   $up_atom_y,   $up_atom_z,
-        $side_atom_x, $side_atom_y, $side_atom_z,
-        $mid_atom_x,  $mid_atom_y,  $mid_atom_z) = @_;
+    my ($mid_atom_x,  $mid_atom_y,  $mid_atom_z,
+        $up_atom_x,   $up_atom_y,   $up_atom_z,
+        $side_atom_x, $side_atom_y, $side_atom_z) = @_;
 
     my @local_ref;
 
     # Let local z-axis be colinear to bond between mid and up atoms.
-    $local_ref_frame[2][0] = $side_atom_x - $up_atom_x;
-    $local_ref_frame[2][1] = $side_atom_y - $up_atom_y;
-    $local_ref_frame[2][2] = $side_atom_z - $up_atom_z;
+    $local_ref_frame[2][0] = $up_atom_x - $mid_atom_x;
+    $local_ref_frame[2][1] = $up_atom_y - $mid_atom_y;
+    $local_ref_frame[2][2] = $up_atom_z - $mid_atom_z;
 
     # Let local x-axis be perpendicular to bonds between mid, up and mid, side
     # atoms.
-    $local_ref_frame[0][0] = ($mid_atom_y - $up_atom_y) * $local_ref_frame[2][2]
-        - ($mid_atom_z - $up_atom_z) * $local_ref_frame[2][1];
-    $local_ref_frame[0][1] = - ($mid_atom_x - $up_atom_x) * $local_ref_frame[2][2]
-        + ($mid_atom_z - $up_atom_z) * $local_ref_frame[2][0];
-    $local_ref_frame[0][2] = ($mid_atom_x - $up_atom_x) * $local_ref_frame[2][1]
-        - ($mid_atom_y - $up_atom_y) * $local_ref_frame[2][0];
+    $local_ref_frame[0][0] = ($side_atom_y - $mid_atom_y) * $local_ref_frame[2][2]
+        - ($side_atom_z - $mid_atom_z) * $local_ref_frame[2][1];
+    $local_ref_frame[0][1] = - ($side_atom_x - $mid_atom_x) * $local_ref_frame[2][2]
+        + ($side_atom_z - $mid_atom_z) * $local_ref_frame[2][0];
+    $local_ref_frame[0][2] = ($side_atom_x - $mid_atom_x) * $local_ref_frame[2][1]
+        - ($side_atom_y - $mid_atom_y) * $local_ref_frame[2][0];
 
     # Let local y-axis be in the same plane as mid-up and mid-side bonds.
     $local_ref_frame[1][0] = $local_ref_frame[2][1] * $local_ref_frame[0][2]
