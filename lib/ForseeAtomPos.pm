3f6920bb0c896d957638ade1ddf0b6cb866f4ec2
algirdas <algirdas@lama>
2016-10-28 16:38:15 +0000

    trunk/lib/ (algirdas@tasmanijos-velnias) Changed variable name in cif_to_hash heavily. Also added to the same function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@47 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/ForseeAtomPos.pm
+++ lib/ForseeAtomPos.pm
@@ -5,36 +5,43 @@ use warnings;
 
 # ------------------------------ PDBx/mmCIF parser ---------------------------- #
 
+#
+# Converts cif format to hash table, that it is more convenient to work with.
+# Only reads _atom_site category and its items with data in it.
+#
+
 sub cif_to_hash{
-    my @cif_category;
-    my $read_atom_site = 0;  
-    my @cif_table;  
-    my $cif_line_counter = 0;
-    my %cif_hash;
+    my @atom_site;       # List of all data items and data itself in 
+                         # _atom_site category.
+    my @atom_site_table;     # Temporary table for storing actual data-item data.
+
+    my %atom_site_hash;  # It is basically an @atom_site_table converted to hash.
+
+    my $is_reading_lines = 0; # Starts/stops reading lines at certain flags.
 
     foreach( @_ ){
-        if( $_ =~ /_atom_site.(.+)\n$/ ){
-            push( @cif_category, $1, [] );
-            $read_atom_site = 1;
-            $cif_line_counter += 1;
-        }elsif( $read_atom_site == 1 ){
-            push( @cif_table, split( /\s+/, $_ ) );
-        }elsif( $read_atom_site == 1 && $_ =~ /[^_]/ ){
+        if( $_ =~ /_atom_site.(.+)\n$/ ){ # Creates pre-hash (place holder) list 
+            push( @atom_site, $1, [] );   # for %atom_site_hash.
+            $is_reading_lines = 1;
+        }elsif( $is_reading_lines == 1 ){                 # Pushes item data
+            push( @atom_site_table, split( /\s+/, $_ ) ); # to separate table.
+        }elsif( $is_reading_lines == 1 && $_ =~ /[^_]/ ){ 
             last;
         }
     }
 
-    my $cif_table_size = scalar( @cif_table );
-    my $category_size = scalar( @cif_category ) / 2;
-
-    for( my $cif_entry = 0; $cif_entry < $cif_table_size; $cif_entry++ ){
-        my $push_to_category =  $cif_entry % $category_size * 2 + 1;
-        push @{ $cif_category[$push_to_category] }, $cif_table[$cif_entry];
+    # Matches atom_site_table data to atom_site (pre-hash list) and transfers
+    # the data.
+    for( my $i = 0; $i <= $#atom_site_table; $i++ ){
+        # This formula makes pushing each item to every second item, that is a
+        # list, possible.
+        my $to_category = ( $i % ( ( $#atom_site + 1 ) / 2 ) ) * 2 + 1;
+        push @{ $atom_site[$to_category] }, $atom_site_table[$i];
     }
 
-    %cif_hash = @cif_category;
+    %atom_site_hash = @atom_site;
 
-    return \%cif_hash;
+    return \%atom_site_hash;
 }
 
 # ------------------------------- Linear algebra ------------------------------ #
@@ -48,7 +55,8 @@ my $EPSILON = 1.0 / ( 2 ** 52 ); # Machine accuracy for 64-bit floating point
                                  # numbers.
 
 #
-# Creates local reference frame for any three given atoms.
+# Creates local reference frame for any three given atoms positions in cartesian
+# coordinate system.
 #
 
 sub create_ref_frame{
@@ -147,8 +155,8 @@ sub find_euler_angles{
 #      \    0         0     1 /   \ z /   \              0              /
 # 
 #
-# Description. A wrapper function for Maxima 5.24.0. Takes argument from amino 
-# acid model function and performs symbolic matrix multiplications with unknown 
+# A wrapper function for Maxima 5.24.0. Takes argument from amino acid
+# model function and performs symbolic matrix multiplications with unknown 
 # variables, such as bond dihedral chi angles, and simplifies the expression.
 #
 
