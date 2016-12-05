c0f217f9d511a8df6838ab388debec8dfc61bda3
algirdas <algirdas@lama>
2016-12-05 19:38:21 +0000

    Added test program, inputs, outputs and cases.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@98 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ test/case/min_max_002.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+../programs/min_max < ../input/seq_of_numbers_002.dat
+
---
+++ test/case/min_max_005.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+../programs/min_max < ../input/seq_of_numbers_005.dat
