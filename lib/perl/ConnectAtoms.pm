2f253f568a49dcde8663cd369fe7d0f8c8c60fe1
algirdas <algirdas@lama>
2016-12-06 17:47:21 +0000

    Decided to remove min_max function and implement directly to create_box function due to clearer readability.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@101 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/ConnectAtoms.pm
+++ lib/perl/ConnectAtoms.pm
@@ -3,20 +3,6 @@ package ConnectAtoms;
 use List::Util qw(min max);
 
 #
-# Returns lowest and highest number of given sequence of numbers.
-#
-
-sub min_max
-{
-    my @seq_of_numbers = @_;
-
-    my $min_number = min( @seq_of_numbers );
-    my $max_number = max( @seq_of_numbers );
-
-    return $min_number, $max_number;
-}
-
-#
 # Given the cartesian coordinates (x, y, z) of atoms, function returns the
 # dimensions of smallest possible box that contains all atoms.
 #
@@ -26,21 +12,17 @@ sub create_box
     my @atom_coordinates = @_;
 
     # Directions are adapted to right-handed Cartesian coordinate system.
-    my ( $most_left_x_coord,     $most_right_x_coord,
-	 $most_backward_y_coord, $most_forward_y_coord,
-	 $most_down_z_coord,     $most_up_z_coord );
-
     # Looking for leftmost and rightmost coordinates of X-axis.
-    ( $most_left_x_coord, $most_right_x_coord ) = 
-    	@{ min_max( $atom_coordinates[0] ) };
+    my $most_left_x_coord     = min( $atom_coordinates[0] );
+    my $most_right_x_coord    = max( $atom_coordinates[0] );
 
     # Looking for most backward and forward coordinates of Y-axis.
-    ( $most_backward_y_coord, $most_forward_y_coord ) =
-    	@{ min_max( $atom_coordinates[1] ) };
+    my $most_backward_y_coord = min( $atom_coordinates[1] );
+    my $most_forward_y_coord  = max( $atom_coordinates[1] );
 
     # Looking for downmost and upmost coordinates of Z-axis.
-    ( $most_down_z_coord, $most_up_z_coord ) =
-    	@{ min_max( $atom_coordinates[2] ) };
+    my $most_down_z_coord     = min( $atom_coordinates[2] );
+    my $most_up_z_coord       = max( $atom_coordinates[2] );
 
     # Coordinates of minimum bounding box that contains all given atoms.
     my @box_coordinates;
@@ -61,6 +43,8 @@ sub create_box
     	[ $most_right_x_coord, $most_forward_y_coord,  $most_up_z_coord   ];
     $box_coordinates[7] =
     	[ $most_right_x_coord, $most_forward_y_coord,  $most_down_z_coord ];
+
+    print Dumper \@box_coordinates;
 
     return \@box_coordinates;
 }
