92fe824c8e64ca96ccc3c20956183f0eca341eef
algirdas <algirdas@lama>
2016-12-06 19:01:00 +0000

    Added output file for create_box test.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@103 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/ConnectAtoms.pm
+++ lib/perl/ConnectAtoms.pm
@@ -1,6 +1,10 @@
 package ConnectAtoms;
 
+use strict;
+use warnings;
+
 use List::Util qw(min max);
+use Data::Dumper;
 
 #
 # Given the cartesian coordinates (x, y, z) of atoms, function returns the
@@ -13,40 +17,21 @@ sub create_box
 
     # Directions are adapted to right-handed Cartesian coordinate system.
     # Looking for leftmost and rightmost coordinates of X-axis.
-    my $most_left_x_coord     = min( $atom_coordinates[0] );
-    my $most_right_x_coord    = max( $atom_coordinates[0] );
+    my $most_left_x_coord     = min( @{ $atom_coordinates[0] } );
+    my $most_right_x_coord    = max( @{ $atom_coordinates[0] } );
 
     # Looking for most backward and forward coordinates of Y-axis.
-    my $most_backward_y_coord = min( $atom_coordinates[1] );
-    my $most_forward_y_coord  = max( $atom_coordinates[1] );
+    my $most_backward_y_coord = min( @{ $atom_coordinates[1] } );
+    my $most_forward_y_coord  = max( @{ $atom_coordinates[1] } );
 
     # Looking for downmost and upmost coordinates of Z-axis.
-    my $most_down_z_coord     = min( $atom_coordinates[2] );
-    my $most_up_z_coord       = max( $atom_coordinates[2] );
+    my $most_down_z_coord     = min( @{ $atom_coordinates[2] } );
+    my $most_up_z_coord       = max( @{ $atom_coordinates[2] } );
 
     # Coordinates of minimum bounding box that contains all given atoms.
-    my @box_coordinates;
-
-    $box_coordinates[0] =
-	[ $most_left_x_coord,  $most_backward_y_coord, $most_down_z_coord ];
-    $box_coordinates[1] =
-	[ $most_left_x_coord,  $most_backward_y_coord, $most_up_z_coord   ];
-    $box_coordinates[2] =
-	[ $most_left_x_coord,  $most_forward_y_coord,  $most_up_z_coord   ];
-    $box_coordinates[3] =
-    	[ $most_left_x_coord,  $most_forward_y_coord,  $most_down_z_coord ];
-    $box_coordinates[4] =
-    	[ $most_right_x_coord, $most_backward_y_coord, $most_down_z_coord ];
-    $box_coordinates[5] =
-    	[ $most_right_x_coord, $most_backward_y_coord, $most_up_z_coord   ];
-    $box_coordinates[6] =
-    	[ $most_right_x_coord, $most_forward_y_coord,  $most_up_z_coord   ];
-    $box_coordinates[7] =
-    	[ $most_right_x_coord, $most_forward_y_coord,  $most_down_z_coord ];
-
-    print Dumper \@box_coordinates;
-
-    return \@box_coordinates;
+    return $most_left_x_coord,     $most_right_x_coord,
+           $most_backward_y_coord, $most_forward_y_coord,
+           $most_down_z_coord,     $most_up_z_coord;
 }
 
 #
