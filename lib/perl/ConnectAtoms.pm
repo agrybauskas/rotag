1f25e53217a503efbee924aaae68ecc965aee867
algirdas <algirdas@lama>
2016-12-07 06:39:47 +0000

    Fixed bug that produced wrong rows in create_box test function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@104 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/ConnectAtoms.pm
+++ lib/perl/ConnectAtoms.pm
@@ -13,20 +13,23 @@ use Data::Dumper;
 
 sub create_box
 {
-    my @atom_coordinates = @_;
+    my @atom_coord = @_;
+    my @atom_coord_x = map { $_->[0] } @atom_coord;
+    my @atom_coord_y = map { $_->[1] } @atom_coord;
+    my @atom_coord_z = map { $_->[2] } @atom_coord;
 
     # Directions are adapted to right-handed Cartesian coordinate system.
     # Looking for leftmost and rightmost coordinates of X-axis.
-    my $most_left_x_coord     = min( @{ $atom_coordinates[0] } );
-    my $most_right_x_coord    = max( @{ $atom_coordinates[0] } );
+    my $most_left_x_coord     = min( @atom_coord_x );
+    my $most_right_x_coord    = max( @atom_coord_x );
 
     # Looking for most backward and forward coordinates of Y-axis.
-    my $most_backward_y_coord = min( @{ $atom_coordinates[1] } );
-    my $most_forward_y_coord  = max( @{ $atom_coordinates[1] } );
+    my $most_backward_y_coord = min( @atom_coord_y );
+    my $most_forward_y_coord  = max( @atom_coord_y );
 
     # Looking for downmost and upmost coordinates of Z-axis.
-    my $most_down_z_coord     = min( @{ $atom_coordinates[2] } );
-    my $most_up_z_coord       = max( @{ $atom_coordinates[2] } );
+    my $most_down_z_coord     = min( @atom_coord_z );
+    my $most_up_z_coord       = max( @atom_coord_z );
 
     # Coordinates of minimum bounding box that contains all given atoms.
     return $most_left_x_coord,     $most_right_x_coord,
