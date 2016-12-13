7d190d0a0053a0787d1c91e9f596fbe246f9bdc2
algirdas <algirdas@lama>
2016-12-13 17:42:56 +0000

    Fixed speed problem in connect_atoms function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@119 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/ConnectAtoms.pm
+++ lib/perl/ConnectAtoms.pm
@@ -57,118 +57,72 @@ sub connect_atoms
     # Creates smallest box that contain all atoms.
     my @boundary_box = create_box( @all_atom_coord );
 
-    # Divides box into cells, edges of which has length of bond.
-    my $cell_number_x =
-    	int( ( $boundary_box[1] - $boundary_box[0] ) / $bond_length ) + 1;
-    my $cell_number_y =
-    	int( ( $boundary_box[3] - $boundary_box[2] ) / $bond_length ) + 1;
-    my $cell_number_z =
-    	int( ( $boundary_box[5] - $boundary_box[4] ) / $bond_length ) + 1;
+    my %grid_box;
 
-    my @grid_box;
+    my $cell_index_x;
+    my $cell_index_y;
+    my $cell_index_z;
 
-    foreach my $idx_x ( ( 1..$cell_number_x ) ) {
-    	foreach my $idx_y ( ( 1..$cell_number_y ) ) {
-    	    foreach my $idx_z ( ( 1..$cell_number_z ) ) {
-    		push( @grid_box, [ $idx_x, $idx_y, $idx_z ], [] );
-    	    }
-    	}
+    # Assign atoms to cells in grid box.
+    foreach my $atom_coord ( @all_atom_coord ) {
+	$cell_index_x =
+	    int( ( $atom_coord->[0] - $boundary_box[0] ) / $bond_length ) + 1;
+	$cell_index_y =
+	    int( ( $atom_coord->[1] - $boundary_box[2] ) / $bond_length ) + 1;
+	$cell_index_z =
+	    int( ( $atom_coord->[2] - $boundary_box[4] ) / $bond_length ) + 1;
+
+	if( exists $grid_box{"$cell_index_x,$cell_index_y,$cell_index_z"} ) {
+	    push( @{ $grid_box{"$cell_index_x,$cell_index_y,$cell_index_z"} },
+		  $atom_coord );
+	} else {
+	          $grid_box{"$cell_index_x,$cell_index_y,$cell_index_z"} =
+		  [ $atom_coord ];
+	}
     }
 
-    # Assigns atoms to cells in grid box.
-    # foreach my $atom_coord ( @all_atom_coord ) {
-    # 	for( my $cell = 0; $cell < $#grid_box; $cell += 2 ) {
-    # 	    if( $atom_coord->[0] <
-    # 		$boundary_box[0] + ( $grid_box[$cell]->[0] - 1 ) * $bond_length ) {
-    # 		next;
-    # 	    }
-
-    # 	    if( $atom_coord->[0] >=
-    # 		$boundary_box[0] +   $grid_box[$cell]->[0] * $bond_length ) {
-    # 		next;
-    # 	    }
-
-    # 	    if( $atom_coord->[1] <
-    # 		$boundary_box[2] + ( $grid_box[$cell]->[1] - 1 ) * $bond_length ) {
-    # 		next;
-    # 	    }
-
-    # 	    if( $atom_coord->[1] >=
-    # 		$boundary_box[2] +   $grid_box[$cell]->[1] * $bond_length ) {
-    # 		next;
-    # 	    }
-
-    # 	    if(	$atom_coord->[2] <
-    # 		$boundary_box[4] + ( $grid_box[$cell]->[2] - 1 ) * $bond_length ) {
-    # 		next;
-    # 	    }
-
-    # 	    if( $atom_coord->[2] >=
-    # 		$boundary_box[4] +   $grid_box[$cell]->[2] * $bond_length ) {
-    # 		push( @{ $grid_box[$cell+1] }, [ @$atom_coord ] );
-    # 	    }
-    # 	}
-    # }
-
-    # Removes empty cells.
-    # my @grid_box_full; # Without empty cells.
-    # my %grid_box_full; # Hash version of the same array. Will be esier to find
-    #                    # neighbouring cells.
-
-    # for( my $cell = 0; $cell < $#grid_box; $cell += 2 ) {
-    #     if( $#{ $grid_box[$cell+1] } != -1 ) {
-    #         push( @grid_box_full, 
-    # 		  join( ",", @{ $grid_box[$cell] } ), ( $grid_box[$cell+1] ) );
-    #     }
-    # }
-
-    # %grid_box_full = @grid_box_full;
-
-    # my @cell_idx;
+    my @cell_idx;
 
     # For each cell, checks neighbouring cells.
-    # foreach my $cell ( keys %grid_box_full ) {
-    # 	@cell_idx = split( ",", $cell );
+    foreach my $cell ( keys %grid_box ) {
+    	@cell_idx = split( ",", $cell );
 
-    # 	my @neighbour_cells; # The array will contain all atoms of the neighbouring
-    # 	                     # 26 cells.
+    	my @neighbour_cells; # The array will contain all atoms of the
+                             # neighbouring 26 cells.
 
-	# TODO: maybe introduce regular expression instead of looking for every
-	# possible cell.
 	# $i represents x, $j - y, $k - z coordinates.
-	# for my $i ( ( $cell_idx[0] - 1..$cell_idx[0] + 1 ) ) {
-	#     for my $j ( ( $cell_idx[1] - 1..$cell_idx[1] + 1 ) ) {
-	# 	for my $k ( ( $cell_idx[2] - 1..$cell_idx[2] + 1 ) ) {      
-	# 	    if ( exists $grid_box_full{"$i,$j,$k"} ) {
-	# 		push( @neighbour_cells, @{ $grid_box_full{"$i,$j,$k"} } );
-	# 	    }
-	# 	}
-	#     }
-	# }
+	for my $i ( ( $cell_idx[0] - 1..$cell_idx[0] + 1 ) ) {
+	    for my $j ( ( $cell_idx[1] - 1..$cell_idx[1] + 1 ) ) {
+		for my $k ( ( $cell_idx[2] - 1..$cell_idx[2] + 1 ) ) {      
+		    if ( exists $grid_box{"$i,$j,$k"} ) {
+			push( @neighbour_cells, @{ $grid_box{"$i,$j,$k"} } );
+		    }
+		}
+	    }
+	}
 
 	# Checks distance between neighbouring atoms by formula:
 	# x^2+y^2+z^2 < (bond_length)^2
-	# my $distance_btw_atoms;
+	my $distance_btw_atoms;
 
 	# TODO: add atoms that are in the center cell.
-    	# foreach my $cell_atom_coord ( @{ $grid_box_full{$cell} } ) {
-    	#     foreach my $neighbour_atom ( @neighbour_cells ) {
-	# 	$distance_btw_atoms =  
-	# 	      ( $neighbour_atom->[0] - $cell_atom_coord->[0] ) ** 2
-	# 	    + ( $neighbour_atom->[1] - $cell_atom_coord->[1] ) ** 2
-	# 	    + ( $neighbour_atom->[2] - $cell_atom_coord->[2] ) ** 2;
+    	foreach my $cell_atom_coord ( @{ $grid_box{$cell} } ) {
+    	    foreach my $neighbour_atom ( @neighbour_cells ) {
+	    	$distance_btw_atoms =  
+		    ( $neighbour_atom->[0] - $cell_atom_coord->[0] ) ** 2
+	    	  + ( $neighbour_atom->[1] - $cell_atom_coord->[1] ) ** 2
+	    	  + ( $neighbour_atom->[2] - $cell_atom_coord->[2] ) ** 2;
 
-	# 	# TODO: deal with approximate distances in mmCif.
-	# 	if( ( $distance_btw_atoms >
-	# 	      ( $bond_length - 0.1 * $bond_length ) ** 2 )
-	# 	 && ( $distance_btw_atoms <
-	# 	      ( $bond_length + 0.1 * $bond_length ) ** 2 ) ) {
-	# 	    print sqrt( $distance_btw_atoms ) . "\n";
-	# 	}
-    	
-	#     }
-	# }
-    # }
+		# TODO: deal with approximate distances in mmCif.
+		if( ( $distance_btw_atoms >
+		      ( $bond_length - 0.1 * $bond_length ) ** 2 )
+		 && ( $distance_btw_atoms <
+		      ( $bond_length + 0.1 * $bond_length ) ** 2 ) ) {
+		    print sqrt( $distance_btw_atoms ) . "\n";
+		}    	
+	    }
+	}
+    }
 }
 
 1;
