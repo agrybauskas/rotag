b62c7136f156ec26ce20043157fa56c689c877fd
algirdas <algirdas@lama>
2016-12-16 16:32:10 +0000

    Added test cases for each 20 aminocid and coresponding output.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@127 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ test/case/connect_atoms_018.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+cif_file=../input/tryptohan_018.cif
+
+bond_length=1.360
+bond_length_error=0.174
+
+../programs/connect_atoms ${bond_length} ${bond_length_error} < ${cif_file} \
+| sort
