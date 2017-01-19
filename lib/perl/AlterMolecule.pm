df6c651705debfd90ab63c9081d99871f20bfe22
algirdas <algirdas@lama>
2017-01-19 18:44:20 +0000

    End of the session. Started writing rotate_bond function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@175 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/AlterMolecule.pm
+++ lib/perl/AlterMolecule.pm
@@ -3,8 +3,49 @@ package AlterMolecule;
 use strict;
 use warnings;
 
+use Data::Dumper;
+
+#
+# Makes a rotational transformation matrix of the bond.
+# Input  ():
+# Output ():
+#
+
 sub rotate_bond
 {
+    my ( $mid_atom_coord,
+	 $up_atom_coord,
+	 $side_atom_coord,
+	 $target_atom_coord ) = @_;
+
+    # Transformations that transforms global reference frame to local.
+    # Negative translation matrix. Places mid-atom to 
+    my $neg_transl_matrix =
+	[ [ 1, 0, 0, - ( $mid_atom_coord->[0] ) ],
+	  [ 0, 1, 0, - ( $mid_atom_coord->[1] ) ],
+	  [ 0, 0, 1, - ( $mid_atom_coord->[2] ) ],
+	  [ 0, 0, 0,              1             ] ];
+
+    # Positive translation matrix. Brings back to original mid-atom position.
+    my $pos_transl_matrix =
+	[ [ 1, 0, 0, $mid_atom_coord->[0] ],
+	  [ 0, 1, 0, $mid_atom_coord->[1] ],
+	  [ 0, 0, 1, $mid_atom_coord->[2] ],
+	  [ 0, 0, 0,          1           ] ];
+    
+    # my ( $mid_atom_x,  $mid_atom_y,  $mid_atom_z,
+    #      $up_atom_x,   $up_atom_y,   $up_atom_z,
+    #      $side_atom_x, $side_atom_y, $side_atom_z ) = @_    
 }
 
+#
+# Changes the length of the bond.
+# Input  ():
+# Ouptut ():
+#
+
+sub change_bond_len
+{
+
+}
 1;
