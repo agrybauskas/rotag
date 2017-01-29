18e4ca698f79bfb2bfcaa9baaf9c40fa652fb4ba
algirdas <algirdas@lama>
2017-01-29 20:51:20 +0000

    Made rotate_bond function testable by rotating around the bond by 2*pi and getting the same coordinates.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@185 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- experiments/symbolic_math.perl
+++ experiments/symbolic_math.perl
@@ -10,12 +10,13 @@ print( "-" x 80, "\n" );
 #
 # Trying cos(x)^2 + sin(x)^2 = 1 type expression.
 #
+my $PI = 4 * atan2( 1, 1 );
 
 my ( $chi ) = symbols( qw( chi ) );
 
-my $expression = $chi**2;
+my $expression = cos( $chi )**2;
 
-$chi = 2;
+$chi = 0.3 * $PI;
 
 print( eval( $expression ), "\n" );
 
--- test/case/rotate_bond_001.sh
+++ test/case/rotate_bond_001.sh
@@ -2,7 +2,10 @@
 cd "$(dirname "$0")"
 
 cif_file=../input/serine_001.cif
+
 atom_specifier="label_atom_id CA,C,CB,OG"
 data_specifier="Cartn_x,Cartn_y,Cartn_z"
 
-../programs/rotate_bond "${atom_specifier}" "${data_specifier}" < ${cif_file}
+rotational_angle="2*\$pi"
+
+../programs/rotate_bond "${atom_specifier}" "${data_specifier}" "${rotational_angle}" < ${cif_file}
