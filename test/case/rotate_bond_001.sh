b0fe52985ace055325bfe6488144947fb01ad5a2
algirdas <algirdas@lama>
2017-02-02 18:52:25 +0000

    Made rotate_bond test function usable in Jmol. Now, it is possible to look at the results. Noticed that rotation along chi1 bond does not produce correct conformations in rotate_bond_001 case.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@187 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- test/case/rotate_bond_001.sh
+++ test/case/rotate_bond_001.sh
@@ -6,6 +6,7 @@ cif_file=../input/serine_001.cif
 atom_specifier="label_atom_id CA,C,CB,OG"
 data_specifier="Cartn_x,Cartn_y,Cartn_z"
 
-rotational_angle="2*\$pi"
+num_of_points=17 # Number of points that will be generated
 
-../programs/rotate_bond "${atom_specifier}" "${data_specifier}" "${rotational_angle}" < ${cif_file}
+../programs/rotate_bond "${atom_specifier}" "${data_specifier}" \
+			"${num_of_points}" < ${cif_file}
