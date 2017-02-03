36f5326656e3e8eb64e13595dab368e162a91c14
algirdas <algirdas@lama>
2017-02-03 15:57:09 +0000

    Creating SidechainModels for describing sidechain models for each amino acid.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@189 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/perl/SidechainModels.pm
@@ -0,0 +1,27 @@
+package SidechainModels
+
+use strict;
+use warnings;
+
+use lib qw( ./ );
+use AlterMolecules;
+
+use Data::Dumper;
+
+# ---------------------- Constraint-free Sidechain models --------------------- #
+
+#
+# Discribes sidechain models that are not restrained by van der Waals radius.
+# These models are idealistic and more developmental than final.
+#
+
+#
+# Model of serine sidechain.
+# Input  (1 arg):
+# Output (1 arg):
+#
+
+sub cf_serine # cf - abbreviation of constraint-free.
+{
+    
+}
