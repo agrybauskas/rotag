1e7fd9bf1215396b80b7dcd24aa8eb5629d2d148
algirdas <algirdas@lama>
2017-02-13 18:49:22 +0000

    Added necessary test programs, cases and ethalon output files for rotate_x_axis, rotate_y_axis and rotate_z_axis functions.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@205 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ test/case/rotate_x_axis_001.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+cif_file=../input/serine_001.cif
+
+atom_specifier="label_atom_id CA,N,CB,OG"
+data_specifier="Cartn_x,Cartn_y,Cartn_z"
+angle="1.5708" # In radians.
+
+../programs/rotate_x_axis "${atom_specifier}" "${data_specifier}" \
+			  "${angle}" < ${cif_file}
