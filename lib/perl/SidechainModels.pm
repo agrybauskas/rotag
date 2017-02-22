bb974d9fc1c7ecbede5effc289c7e2ce4109d143
algirdas <algirdas@lama>
2017-02-22 18:55:05 +0000

    Started writing cf_serine function. Decided that first two arguments should be a bond length and error of the bond length and the third - data of atom coordinates.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@218 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/SidechainModels.pm
+++ lib/perl/SidechainModels.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use lib qw( ./ );
 use CifParser;
+use ConnectAtoms;
 use AlterMolecule;
 
 use Data::Dumper;
@@ -24,7 +25,12 @@ use Data::Dumper;
 
 sub cf_serine # cf - abbreviation for constraint-free.
 {
-    my ( $serine_atom_data, $target_atom ) = @_;
+    my @amino_acid_data = @_;
+
+    my @atom_connections =
+	ConnectAtoms::connect_atoms( 1.51, # HACK: approximate bond-length
+				     0.15, # HACK: approximate bond-length-error
+				     @amino_acid_data );    
 }
 
 1;
