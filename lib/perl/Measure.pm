24572742883f5ef62f6eacad3e569b53d1cc1250
algirdas <algirdas@lama>
2017-02-27 18:24:18 +0000

    Finished bond_angle function. However, it looks like it calculates angle inaccurately.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@223 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/Measure.pm
+++ lib/perl/Measure.pm
@@ -3,6 +3,13 @@ package Measure;
 use strict;
 use warnings;
 
+use Math::Trig;
+
+use lib "./";
+use LinearAlgebra;
+
+use Data::Dumper;
+
 # ----------------------------- Molecule parameters --------------------------- #
 
 #
@@ -36,9 +43,41 @@ sub bond_length
 
 sub bond_angle
 {
-    my @atom_coord = @_;
+    my $atom_coord = shift;
+    my @atom_coord = @$atom_coord;
 
-    # my $bond_angle
+    my $bond_angle;
+
+    # Angle between three atoms (in radians) in 3-D space can be calculated by
+    # the formula:
+    #                            ->   ->      ->         ->
+    #            theta = arccos( AB * BC / || AB || * || BC || )
+
+    # This formula is applied to atoms where vectors are the substraction of
+    # coordinates of two atoms. Suppose, one of the side atom is A, B - middle
+    # and C - remaining atom. Order of side atoms is irrelevant.
+    my @vector_ab = ( $atom_coord[1][0] - $atom_coord[0][0],
+		      $atom_coord[1][1] - $atom_coord[0][1],
+		      $atom_coord[1][2] - $atom_coord[0][2] );
+    my @vector_bc = ( $atom_coord[2][0] - $atom_coord[1][0],
+		      $atom_coord[2][1] - $atom_coord[1][1],
+		      $atom_coord[2][2] - $atom_coord[1][2] );
+
+    # TODO: prove/disprove the correctness of the expression.
+    my $vector_product = $vector_ab[2] * $vector_bc[0] +
+	               + $vector_ab[1] * $vector_bc[1]
+	               + $vector_ab[0] * $vector_bc[2];
+
+    my $length_ab = sqrt( $vector_ab[0]**2
+			+ $vector_ab[1]**2
+			+ $vector_ab[2]**2 );
+    my $length_bc = sqrt( $vector_bc[0]**2
+			+ $vector_bc[1]**2
+			+ $vector_bc[2]**2 );
+
+    $bond_angle = acos( $vector_product / ( $length_ab * $length_bc ) );
+
+    return $bond_angle;
 }
 
 #
--- test/case/bond_angle_001.sh
+++ test/case/bond_angle_001.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 cif_file=../input/serine_001.cif
-atom_specifier="label_atom_id CB,CA,C"
+atom_specifier="label_atom_id CA,C,CB"
 data_specifier="Cartn_x,Cartn_y,Cartn_z"
 
 ../programs/bond_angle "${atom_specifier}" "${data_specifier}" < ${cif_file}
