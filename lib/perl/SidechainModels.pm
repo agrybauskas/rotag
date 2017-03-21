fbc7c50f30ccd1cecdf844ff5949b034e47906c8
algirdas <algirdas@lama>
2017-03-21 19:06:42 +0000

    Decided to make general (for all sidechains) model and changed name of the function to rotation_only. Function will be working by going through connected atoms and finding only rotatable bonds.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@237 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/SidechainModels.pm
+++ lib/perl/SidechainModels.pm
@@ -10,7 +10,7 @@ use AlterMolecule;
 
 use Data::Dumper;
 
-# ---------------------- Constraint-free Sidechain models --------------------- #
+# ------------------------ Idealistic sidechain models ------------------------ #
 
 #
 # Discribes sidechain models that are not restrained by van der Waals radius.
@@ -18,20 +18,26 @@ use Data::Dumper;
 #
 
 #
-# Model of serine sidechain.
-# Input  (1 arg):
+# Model that uses only rotation around single bonds.
+# Input  (1 arg): array of arrays containing amino acid data, such as id,
+#                 type_symbol, label_alt_id, label_comp_id, Cartn_x, Cartn_y,
+#                 Cartn_z:
+#                 Ex.: ( [ 151, "CB", "SER", -54.506, -111.705, -5.149 ],
+#                        [ 152, "OG", "SER", -54.962, -111.220, -3.896 ] )
 # Output (1 arg):
 #
 
-sub cf_serine # cf - abbreviation for constraint-free.
+sub rotation_only
 {
     my @amino_acid_data = @_;
 
     my %atom_connections =
 	%{ ConnectAtoms::connect_atoms( 1.51, # HACK: approximate bond-length
-					0.15, # HACK: approximate bond-length-error
+					0.15, # HACK: bond-length-error
 					@amino_acid_data ) };
 
+    # Picks any atom in atom_connections hash and start to go through
+    # connections.
 }
 
 1;
--- tests/cases/rotation_only_001.sh
+++ tests/cases/rotation_only_001.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 cif_file=../inputs/serine_001.cif
 atom_specifier="label_seq_id 18"
 
-../programs/cf_serine "${atom_specifier}" < ${cif_file}
+../programs/rotation_only "${atom_specifier}" < ${cif_file}
