eed4de1dc6e1681219c42e811bcbaa77e166d9ca
algirdas <algirdas@lama>
2017-05-01 10:16:39 +0000

    Added all remaining information about rotatable side chain bonds.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@280 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotation_only_002.sh
+++ tests/cases/rotation_only_002.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 cif_file=../inputs/aspartic_acid_006.cif
 target_atom="label_atom_id OD1"
 
-num_of_points=289 # Number of pseudo-atoms that will be generated.
+num_of_points=17 # Number of pseudo-atoms that will be generated.
 
 ../programs/rotation_only "${target_atom}" "${num_of_points}" < ${cif_file}
