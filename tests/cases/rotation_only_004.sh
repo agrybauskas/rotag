b2d5c81a1face0b97218349790f21499568e4819
algirdas <algirdas@lama>
2017-07-07 14:06:00 +0000

    Noticed that quantity of points generated in rotation_only has non-integer as 4th root.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@327 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotation_only_004.sh
+++ tests/cases/rotation_only_004.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 cif_file=../inputs/lysine_005.cif
 angle_range="chi0 0-2*pi & chi1 0-2*pi & chi2 0-2*pi & chi3 0-2*pi"
 target_atom="label_atom_id NZ"
-num_of_points=8 # Number of pseudo-atoms that will be generated.
+num_of_points=16 # Number of pseudo-atoms that will be generated.
 
 ../programs/rotation_only "${target_atom}" "${angle_range}" "${num_of_points}" \
 			  ${cif_file}
