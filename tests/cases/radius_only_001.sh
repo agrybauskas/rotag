e7e657bd6d911550c982f0018660096205888877
algirdas <algirdas@lama>
2017-07-07 16:55:29 +0000

    Working on radius_only function. Test shows that clashes does not include real (not pseudo) atoms.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@329 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/radius_only_001.sh
+++ tests/cases/radius_only_001.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 cif_file=../inputs/serine_001.cif
 target_atom="label_atom_id OG"
 angle_range="chi0 0-2*pi"
-num_of_points=16 # Number of pseudo-atoms that will be generated.
+num_of_angles="chi0 16" # Number of pseudo-atoms that will be generated.
 
-../programs/radius_only "${target_atom}" "${angle_range}" "${num_of_points}" \
+../programs/radius_only "${target_atom}" "${angle_range}" "${num_of_angles}" \
 			${cif_file}
