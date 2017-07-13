4b3f859dfcb64f264091f060476ca53226ca5089
algirdas <algirdas@lama>
2017-07-13 22:33:00 +0000

    Created tests for higher rotational order amino acids. Definitely, have to find out how to exclude atoms that are one atom away. Those are usually defined as clashes in radius_only function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@336 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/radius_only_002.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+cif_file=../inputs/aspartic_acid_006.cif
+target_atom="label_atom_id OD1"
+clash_specifier="is_pseudo_atom 1"
+angle_range="chi0 0-2*pi & chi1 0-2*pi"
+num_of_angles="chi0 4 & chi1 4" # Number of angles witll be generated for each
+                                # dihedral angle.
+
+../programs/radius_only "${target_atom}" "${clash_specifier}" "${angle_range}" \
+			"${num_of_angles}" ${cif_file}
