a8eb0da97eeaf52b66a8b3e094ea1bd2eb641183
algirdas <algirdas@lama>
2017-08-22 18:11:48 +0000

    Finished refactoring LoadParams package.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@356 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/LoadParams.pm
+++ lib/perl/LoadParams.pm
@@ -2,21 +2,20 @@ package LoadParams;
 
 use strict;
 use warnings;
-use Data::Dumper;
+
 use Exporter qw( import );
-our @EXPORT_OK = qw( covalent_radii
-                     rotatable_bonds
-                     vdw_radii );
+our @EXPORT_OK = qw( covalent_radii rotatable_bonds vdw_radii );
 
 #
-# Converts parameter file that identifies rotatable side-chain bonds to hash of
-# hashes.
-# Input  (1 arg): destination of csv parameter file.
-# Output (1 arg): data structure: hashes of hashes of multidimentional array.
-# Ex.:
-# {
-#   "SER" => { OG => [ [ "CA", "CB" ] ] }
-# }
+# Converts parameter file that identifies rotatable side-chain bonds to special
+# data structure.
+# Input:
+#     $parameter_file - destination of csv parameter file describing rotatable
+#     bonds in specific side-chains.
+# Output:
+#     %ROTATABLE_BONDS - special data structure describing rotatable bonds of
+#     each side-chain.
+#     Ex.: { "SER" => { OG => [ [ "CA", "CB" ] ] } }
 #
 
 sub rotatable_bonds
@@ -42,13 +41,15 @@ sub rotatable_bonds
 #
 # Converts parameter file that identifies atom Van der Waals radius to simple
 # hash.
-# Input  (1 arg): destination of csv parameter file.
-# Output (1 arg): hash of values of atom vdw radii.
-# Ex.:
-# {
-#   "N" => 1.66,
-#   "C" => 1.77
-# }
+# Input:
+#     $parameter_file - destination of csv parameter file describing vdw radius
+#     of specific atom.
+# Output:
+#     %VDW_radii - hash describing atom vdw radii.
+#     Ex.: {
+#            "N" => 1.66,
+#            "C" => 1.77
+#          }
 #
 
 sub vdw_radii
@@ -61,9 +62,9 @@ sub vdw_radii
 
     open( my $fh, "<", $parameter_file )
     	or die "Can't open < vdw_radii.csv: $!";
-    foreach( <$fh> ) {
-    	chomp( $_ );
-    	( $atom_name, $atom_radius ) = split( ",", $_ );
+    for my $row ( <$fh> ) {
+    	chomp( $row );
+    	( $atom_name, $atom_radius ) = split( ",", $row );
     	$VDW_RADII{"$atom_name"} = $atom_radius;
     }
     close($fh);
@@ -71,17 +72,19 @@ sub vdw_radii
     return \%VDW_RADII;
 }
 
-
 #
 # Converts parameter file that identifies bond length and converts to a specific
 # data structure.
-# Input  (1 arg): destination of csv parameter file.
-# Output (1 arg): data structure describing bond lengths.
-# Ex.:
-# {
-#   "C" => "length" => [ 1.513, 1.455 ],
-#          "error"  => [ 0.014, 0.011 ]
-# }
+# Input:
+#     $parameter_file - destination of csv parameter file describing covalent
+#     radii and its error.
+# Output:
+#     %COVALENT_RADII - special data structure describing atom bond lengths and
+#     bond length errors.
+#     Ex.: {
+#            "C" => "length" => [ 1.513, 1.455 ],
+#                   "error"  => [ 0.014, 0.011 ]
+#          }
 #
 
 sub covalent_radii
