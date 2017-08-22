7f2ae1be6ff416871b73aa16f707d5d9b30f34e0
algirdas <algirdas@lama>
2017-08-22 17:40:14 +0000

    Finished refactoring Combinatorics package.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@355 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/perl/Combinatorics.pm
+++ lib/perl/Combinatorics.pm
@@ -9,14 +9,22 @@ our @EXPORT_OK = qw( permutation );
 # ---------------------------------- Combinatorics ---------------------------- #
 
 #
-# Carries out permutation for list of arrays.
+# Carries out permutation for list of arrays (recursive function).
+# Input:
+#     $size - size of a list.
+#     $base - list of items that permutation list will be generated from.
+#     $list - one item of whole permutation product.
+#     $permuted_list - permutation product that is tracked through all
+#     recursions.
+# Output:
+#     $permuted_list - final permutation product.
 #
 
 sub permutation
 {
     my ( $size, $base, $list, $permuted_list ) = @_;
     my $base_size = scalar( @{ $base } );
-    
+
     if( $base_size == $size ) {
 	push( @{ $permuted_list }, $base );
     } else {
@@ -24,7 +32,7 @@ sub permutation
 	    permutation( $size, [ @{ $base }, $i ], $list, $permuted_list );
 	}
     }
-    
+
     return $permuted_list;
 }
 
