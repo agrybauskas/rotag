ce50b43e53e1cbd02bb1dfefc89363bb28e10952
algirdas <algirdas@lama>
2017-08-29 18:28:33 +0000

    Continue on refactoring rotation_only function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@365 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotation_only_002.sh
+++ tests/cases/rotation_only_002.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 pdbx_file=../inputs/aspartic_acid_006.cif
-angle_range="chi0 -pi,pi & chi1 -pi,pi"
+angle_range="chi0 0,2*pi & chi1 0,2*pi"
 atom_specifier="label_atom_id OD1"
 num_of_angles="chi0 4 & chi1 4"  # Number of angles witll be generated for each
                                  # dihedral angle.
--- tests/cases/rotation_only_004.sh
+++ tests/cases/rotation_only_004.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 pdbx_file=../inputs/lysine_005.cif
-angle_range="chi0 -pi,pi & chi1 -pi,pi & chi2 -pi,pi & chi3 -pi,pi"
+angle_range="chi0 0,2*pi & chi1 0,2*pi & chi2 0,2*pi & chi3 0,2*pi"
 atom_specifier="label_atom_id NZ"
 num_of_angles="chi0 2 & chi1 2 & chi2 2 & chi3 2" # Number of angles witll be
                                                   # generated for each dihedral
