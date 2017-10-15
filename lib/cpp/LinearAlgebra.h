f2960cb9ff504b7f660788d0fab91ba639b586fd
algirdas <algirdas@lama>
2017-10-15 18:43:35 +0000

    Added find_euler_angles cpp function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@430 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/cpp/LinearAlgebra.h
+++ lib/cpp/LinearAlgebra.h
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+double pi();
+
+double epsilon();
+
 vector< vector<double> > create_ref_frame( double mid_atom_x,
 					   double mid_atom_y,
 					   double mid_atom_z,
@@ -14,5 +18,15 @@ vector< vector<double> > create_ref_frame( double mid_atom_x,
 					   double side_atom_x,
 					   double side_atom_y,
 					   double side_atom_z );
+
+vector<double> find_euler_angles( double mid_atom_x,
+				  double mid_atom_y,
+				  double mid_atom_z,
+				  double up_atom_x,
+				  double up_atom_y,
+				  double up_atom_z,
+				  double side_atom_x,
+				  double side_atom_y,
+				  double side_atom_z );
 
 #endif
