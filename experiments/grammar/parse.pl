af3d5b9ae10172a26f6f5a3020703a007b64b2e8
algirdas <algirdas@lama>
2018-04-01 13:12:13 +0000

    Decided to change & to | because or operator only append atoms, not excludes.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@603 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- experiments/grammar/parse.pl
+++ experiments/grammar/parse.pl
@@ -7,9 +7,10 @@ use Data::Dumper;
 
 use CommandLineParser;
 
-my $input = 'resname ASP,ASN';
+my $input = 'resid 20 | resname ASP,ASN | resid 2-10,1';
 
 my $parser = new CommandLineParser();
 $parser->parser( $input );
 
 print Dumper $parser->{'include'}{'resname'};
+print Dumper $parser->{'include'}{'resid'};
