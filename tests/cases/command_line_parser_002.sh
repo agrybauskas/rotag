97e3277740eb96c2a101fd3e2001e45c1f756577
algirdas <algirdas@lama>
2018-04-09 22:23:59 +0000

    Stress test on CommandLineParser->parser() reveals that selection is very unoptimized. Should look at both filter, obtain_atom_site in PDBxParser.pm and around_distance in ConnectAtoms.pm.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@620 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/command_line_parser_002.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/5svd.dump
+selection_command="resid 57-74 around 5"
+
+"$(dirname "$0")"/../scripts/command_line_parser "${selection_command}" \
+		                                 "${pdbx_dump_file}"
