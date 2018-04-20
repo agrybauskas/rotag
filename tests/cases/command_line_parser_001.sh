43deb91feca390df19225c122674f0ef95c930ba
algirdas <algirdas@lama>
2018-04-20 05:50:54 +0000

    Updated test outputs.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@662 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/command_line_parser_001.sh
+++ tests/cases/command_line_parser_001.sh
@@ -3,7 +3,7 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/5svd.dump
-selection_command="resname ASP & atoname CA"
+selection_command="resname ASP & atomname CA"
 
 "$(dirname "$0")"/../scripts/command_line_parser "${selection_command}" \
 		                                 "${pdbx_dump_file}"
