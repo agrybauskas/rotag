389905b862a1a1e97078a59bb582bfb73b61504f
algirdas <algirdas@lama>
2018-04-25 12:46:03 +0000

    Renamed CommandLineParser.yp to SelectionParser.yp. More readable.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@677 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/selection_parser_002.sh
+++ tests/cases/selection_parser_002.sh
@@ -5,5 +5,5 @@ export PERL5LIB=$(dirname "$0")/../../lib
 pdbx_dump_file=$(dirname "$0")/../inputs/5svd.dump
 selection_command="resid 57-74 around 5"
 
-"$(dirname "$0")"/../scripts/command_line_parser "${selection_command}" \
-		                                 "${pdbx_dump_file}"
+"$(dirname "$0")"/../scripts/selection_parser "${selection_command}" \
+		                              "${pdbx_dump_file}"
