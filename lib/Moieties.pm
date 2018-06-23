fe31dae38edcaba8e97cc0f7a08632a01ab67ce6
algirdas <algirdas@lama>
2018-06-23 16:10:01 +0000

    Adding new package Moieties that can be used to add new sidechains.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@798 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/Moieties.pm
@@ -0,0 +1,15 @@
+package Moieties;
+
+use strict;
+use warnings;
+
+use Exporter qw( import );
+our @EXPORT_OK = qw( %SIDECHAINS );
+
+# ---------------------------------- Moieties --------------------------------- #
+
+my %SIDECHAINS = (
+
+);
+
+1;
