91356188fc418aec5080c6e535bf86375377e403
algirdas <algirdas@lama>
2018-06-25 10:55:53 +0000

    Adding test cases for add_hydrogen with add_only_clear_positions option.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@813 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/add_hydrogens_040.sh
+++ tests/cases/add_hydrogens_040.sh
@@ -2,7 +2,7 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/alanine-001.dump
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/valine-001.dump
 add_only_clear_positions=1
 
 $(dirname "$0")/../scripts/add_hydrogens ${pdbx_dump_file} \
