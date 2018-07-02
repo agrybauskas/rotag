55eec842df53ae6274acd416b46db8320f0b575b
algirdas <algirdas@lama>
2018-07-02 12:17:22 +0000

    Modified test cases for rotag_library so, they could be easily tested.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@844 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_001.sh
+++ tests/cases/rotag_library_001.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/asparagine-surrounded-001.cif
 
-rotag_library -i ${pdbx_file} --cutoff-atom 20 --cutoff-residue 0
+rotag_library -i ${pdbx_file} --cutoff-atom 10 --cutoff-residue 0 --top-rank 1
