c2bb44987ae087c9db64a952785939742de3e58b
algirdas <algirdas@lama>
2018-07-02 21:02:27 +0000

    Adding synthetic cifs and tests for mainly simple clashes.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@847 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_002.sh
+++ tests/cases/rotag_library_002.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/asparagine-H-bonding-001.cif
+pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-002.cif
 
 rotag_library -i ${pdbx_file} --cutoff-atom 10 --cutoff-residue 0 --top-rank 1
--- tests/cases/rotag_library_003.sh
+++ tests/cases/rotag_library_003.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-001.cif
+pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-003.cif
 
 rotag_library -i ${pdbx_file} --cutoff-atom 10 --cutoff-residue 0 --top-rank 1
