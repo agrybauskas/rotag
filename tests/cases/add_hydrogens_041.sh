c8c6f1369d8ca0888ece97a9dc833efc74abad91
algirdas <algirdas@lama>
2018-07-05 19:50:34 +0000

    Patches some tests.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@856 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/add_hydrogens_041.sh
+++ tests/cases/add_hydrogens_041.sh
@@ -2,6 +2,6 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/polipeptide/dipeptide-001.dump
+pdbx_dump_file=$(dirname "$0")/../inputs/polipeptides/dipeptide-001.dump
 
 $(dirname "$0")/../scripts/add_hydrogens ${pdbx_dump_file}
