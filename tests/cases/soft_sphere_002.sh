196e67c38e79d2dc54c5c2cfed4a15782586c036
algirdas <algirdas@lama>
2018-08-12 11:02:16 +0000

    Adding proper soft_sphere() tests.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@966 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/soft_sphere_002.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/synthetic/single-atoms-001.dump
+atom_i_id=1
+atom_j_id=3
+
+$(dirname "$0")/../scripts/soft_sphere ${atom_i_id} \
+                                       ${atom_j_id} \
+				       ${pdbx_dump_file}
