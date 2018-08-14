3f8e347ecffbd2236b90a9c2ba7ce3976907ccef
algirdas <algirdas@lama>
2018-08-14 09:56:52 +0000

    Adding additional test cases to h_bond_explicit() and h_bond_implicit().
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@977 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/h_bond_explicit_004.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/synthetic/h-donor-acceptor-004.dump
+donor_id=2
+hydrogen_id=4
+acceptor_id=3
+
+$(dirname "$0")/../scripts/h_bond_explicit ${donor_id} \
+                                           ${hydrogen_id} \
+                                           ${acceptor_id} \
+                                           ${pdbx_dump_file}
