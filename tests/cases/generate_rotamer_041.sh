bc6c2e7228f1544aa8f84a5b7529b89f10c298c0
algirdas <algirdas@lama>
2018-08-17 20:51:59 +0000

    Adding test cases to make sure that xaa amino acid is treated right by generate_rotamer().
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@993 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/generate_rotamer_041.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/synthetic/xaa-007.dump
+residue_unique_key="18,A,1,."
+angle_values="chi0 0"
+
+$(dirname "$0")/../scripts/generate_rotamer "${residue_unique_key}" \
+	                                    "${angle_values}" \
+					    ${pdbx_dump_file}
