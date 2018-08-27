a6f3e5dd77fbd836cf8ef2d4f9fbe30af0fbabf9
algirdas <algirdas@lama>
2018-08-27 10:38:40 +0000

    Added a test input that was generated by rotag_library. It is then recalculated with rotag_energy.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1027 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_and_rotag_energy_001.sh
+++ tests/cases/rotag_library_and_rotag_energy_001.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-001.cif
+pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-library-001.cif
 
-rotag_library -c 'Inf' -C 'Inf' ${pdbx_file}
+rotag_library -c 'Inf' -C 'Inf' -t 1 ${pdbx_file}
+rotag_dihedral -r ${pdbx_file}
+rotag_energy -r ${pdbx_file}
