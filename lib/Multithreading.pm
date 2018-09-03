93dc625c068f86559caded6c04d18b5de86c1fe5
algirdas <algirdas@lama>
2018-09-03 10:28:23 +0000

    Refactored Multithreading.pm.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1052 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Multithreading.pm
+++ lib/Multithreading.pm
@@ -6,7 +6,19 @@ use warnings;
 use Exporter qw( import );
 our @EXPORT_OK = qw( multithreading );
 
-# ---------------------------------------------------------------------------- #
+our $VERSION = '1.0.0';
+
+# -------------------- Preparing data for multi-threading --------------------- #
+
+#
+# Divides data arrays into smaller arrays that are being passed to multithreading
+# function.
+# Input:
+#     $arrays - list of arrays of data;
+#     $threads - number of threads.
+# Output:
+#     @list_of_array_blocks - list of arrays of arrays of data.
+#
 
 sub divide_arrays_into_blocks
 {
@@ -19,20 +31,19 @@ sub divide_arrays_into_blocks
 
         # Arrays have to have equal lengths.
         if( defined $array_length && scalar( @array ) ne $array_length ) {
-            die( "List of arrays have different lengths." );
+            die 'List of arrays have different lengths.';
         }
-        $array_length =  scalar( @array );
+        $array_length =  scalar @array;
 
         # Splits the array into blocks/chunks.
         my @array_blocks;
-        my $max_block_size =
-            int( scalar( @array ) / $threads );
+        my $max_block_size = int( scalar @array / $threads );
 
         # If block size is smaller that the number of threads, then
         # thread number is reduced.
         my $reduce_threads = 0;
         if( ! $max_block_size ) {
-            $reduce_threads = scalar( @array );
+            $reduce_threads = scalar @array;
             $max_block_size = 1;
         }
 
@@ -40,19 +51,31 @@ sub divide_arrays_into_blocks
             my $block_start = $i * $max_block_size;
             my $block_end = $block_start + $max_block_size - 1;
             if( $i ne $threads-$reduce_threads-1 ) {
-                push( @array_blocks, [ @array[$block_start..$block_end] ] );
+                push @array_blocks, [ @array[$block_start..$block_end] ];
             } else {
                 $block_end = $#array;
-                push( @array_blocks, [ @array[$block_start..$block_end] ] );
+                push @array_blocks, [ @array[$block_start..$block_end] ];
             }
         }
 
-        push( @list_of_array_blocks, \@array_blocks );
+        push @list_of_array_blocks, \@array_blocks;
     }
 
     return \@list_of_array_blocks;
 }
 
+# ----------------------------- Multi-threading ------------------------------- #
+
+#
+# Performs multithreading where function, arguments and data blocks are given.
+# Input:
+#     $function - reference to the function that will be run;
+#     $arguments - arguments that will be passed to a function;
+#     $divisible_arrays - array of data;
+#     $threads - number of threads.
+# Output:
+#     @joined_block_results - results from all threads.
+#
 
 sub multithreading
 {
@@ -68,14 +91,14 @@ sub multithreading
                              $arguments,
                              [ map { $allowed_array_blocks->[$_][$i] }
                                    0..$#{$allowed_array_blocks} ] );
-        push( @block_results, $thread_task );
+        push @block_results, $thread_task;
     }
 
     my @joined_block_results;
     for my $block_result ( @block_results ) {
         $block_result = $block_result->join();
         for( my $i = 0; $i <= $#{ $block_result }; $i++ ) {
-            push( @{ $joined_block_results[$i] }, @{ $block_result->[$i] } );
+            push @{ $joined_block_results[$i] }, @{ $block_result->[$i] };
         }
     }
 
