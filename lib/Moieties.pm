1ec72bd0a61d7a3f62d1e78c5b40889643704597
algirdas <algirdas@lama>
2018-09-09 22:07:48 +0000

    Almost finished replace_with_moiety function. However, atom coordinates are off due to absence of translational transformation.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1100 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Moieties.pm
+++ lib/Moieties.pm
@@ -8,7 +8,16 @@ our @EXPORT_OK = qw( %ATOMS
                      %SIDECHAINS
                      replace_with_moiety );
 
-use PDBxParser qw( filter_by_unique_residue_key );
+use List::Util qw( max );
+use Math::Trig qw( acos );
+
+use AtomProperties qw( @MAINCHAIN_NAMES );
+use Constants qw( $SIG_FIGS );
+use PDBxParser qw( filter
+                   filter_by_unique_residue_key );
+use LinearAlgebra qw( mult_matrix_product
+                      switch_ref_frame );
+use Measure qw( bond_angle );
 use Version qw( $VERSION );
 
 our $VERSION = $VERSION;
@@ -93,12 +102,78 @@ sub replace_with_moiety
 {
     my ( $atom_site, $unique_residue_key, $moiety, $options ) = @_;
 
-    my ( $isomer ) = $options->{'isomer'};
+    my ( $isomer, $last_atom_id ) =
+        ( $options->{'isomer'},
+          $options->{'last_atom_id'}, );
+
     $isomer //= 'R';
+    $last_atom_id //= max( keys %{ $atom_site } );
 
     my $residue_site =
         filter_by_unique_residue_key( $atom_site, $unique_residue_key );
 
+    my @sidechain_ids =
+        @{ filter( { 'atom_site' => $residue_site,
+                     'exclude' =>
+                         # TODO: make proper list of mainchain atoms.
+                         { 'label_atom_id' =>
+                               [ grep { $_ ne 'CB' } @MAINCHAIN_NAMES ] },
+                     'data' => [ 'id' ],
+                     'is_list' => 1 } ) };
+
+    my $n_atom_coord =
+        filter( { 'atom_site' => $residue_site,
+                  'include' => { 'label_atom_id' => [ 'N' ] },
+                  'data' => [ 'Cartn_x', 'Cartn_y', 'Cartn_z' ] } )->[0];
+    my $ca_atom_coord =
+        filter( { 'atom_site' => $residue_site,
+                  'include' => { 'label_atom_id' => [ 'CA' ] },
+                  'data' => [ 'Cartn_x', 'Cartn_y', 'Cartn_z' ] } )->[0];
+    my $c_atom_coord =
+        filter( { 'atom_site' => $residue_site,
+                  'include' => { 'label_atom_id' => [ 'C' ] },
+                  'data' => [ 'Cartn_x', 'Cartn_y', 'Cartn_z' ] } )->[0];
+
+    # TODO: should be refactored, because the code is familiar to
+    # PseudoAtoms::add_hydrogen().
+    my $bond_angle =
+        bond_angle( [ $n_atom_coord, $ca_atom_coord, $c_atom_coord ] );
+
+    my $moiety_angle = acos( ( - 4 - 2 * cos $bond_angle ) / 10 );
+
+    # Generates transformation matrix for transfering atoms to local
+    # reference frame.
+    my ( $transf_matrix ) =
+        @{ switch_ref_frame( $ca_atom_coord,
+                             $n_atom_coord,
+                             $c_atom_coord,
+                             'global' ) };
+
+    # Adds moiety.
+    for my $atom_id ( sort keys %{ $SIDECHAINS{$moiety} } ) {
+        my $moiety_atom = $SIDECHAINS{$moiety}{$atom_id};
+        my ( $transf_atom_coord ) =
+            @{ mult_matrix_product( [ $transf_matrix,
+                                      [ [ $moiety_atom->{'Cartn_x'} ],
+                                        [ $moiety_atom->{'Cartn_y'} ],
+                                        [ $moiety_atom->{'Cartn_z'} ],
+                                        [ 1 ] ] ] ) };
+
+        $moiety_atom->{'id'} = $last_atom_id;
+        $moiety_atom->{'Cartn_x'}= sprintf $SIG_FIGS, $transf_atom_coord->[0][0];
+        $moiety_atom->{'Cartn_y'}= sprintf $SIG_FIGS, $transf_atom_coord->[1][0];
+        $moiety_atom->{'Cartn_z'}= sprintf $SIG_FIGS, $transf_atom_coord->[2][0];
+
+        $atom_site->{$last_atom_id} = $moiety_atom;
+        $last_atom_id++;
+    }
+
+    # Removes old side-chain atoms.
+    foreach( @sidechain_ids ) {
+        delete $atom_site->{$_};
+    }
+
+    return;
 }
 
 1;
