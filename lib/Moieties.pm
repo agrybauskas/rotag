2af103ae1bfde52caab6b3a3bf079b898c1e515b
algirdas <algirdas@lama>
2018-09-09 09:36:48 +0000

    Added correct serine moiety.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1099 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Moieties.pm
+++ lib/Moieties.pm
@@ -8,6 +8,7 @@ our @EXPORT_OK = qw( %ATOMS
                      %SIDECHAINS
                      replace_with_moiety );
 
+use PDBxParser qw( filter_by_unique_residue_key );
 use Version qw( $VERSION );
 
 our $VERSION = $VERSION;
@@ -35,39 +36,43 @@ our %SIDECHAINS = (
             'group_PDB' => 'ATOM',
             'id' => 2,
             'type_symbol' => 'C',
+            'label_atom_id' => 'CA',
+            'label_alt_id' => q{.},
+            'label_comp_id' => 'SER',
+            'label_asym_id' => 'A',
+            'label_entity_id' => 1,
+            'label_seq_id' => q{.},
+            'Cartn_x' => 0.009,
+            'Cartn_y' => 0.077,
+            'Cartn_z' => -0.688,
+        },
+        2 => {
+            'group_PDB' => 'ATOM',
+            'id' => 2,
+            'type_symbol' => 'C',
             'label_atom_id' => 'CB',
             'label_alt_id' => q{.},
             'label_comp_id' => 'SER',
-            'label_asym_id' => undef,
-            'label_entity_id' => undef,
-            'label_seq_id' => undef,
+            'label_asym_id' => 'A',
+            'label_entity_id' => 1,
+            'label_seq_id' => q{.},
             'Cartn_x' => -0.494,
             'Cartn_y' => 0.929,
             'Cartn_z' => 0.504,
-            'auth_seq_id' => undef,
-            'auth_comp_id' => 'SER',
-            'auth_asym_id' => undef,
-            'auth_atom_id' => 'CB',
-            'pdbx_PDB_model_num' => undef,
         },
-        2 => {
+        3 => {
             'group_PDB' => 'ATOM',
             'id' => 3,
             'type_symbol' => 'O',
             'label_atom_id' => 'OG',
             'label_alt_id' => q{.},
             'label_comp_id' => 'SER',
-            'label_asym_id' => undef,
-            'label_entity_id' => undef,
-            'label_seq_id' => undef,
+            'label_asym_id' => 'A',
+            'label_entity_id' => 1,
+            'label_seq_id' => q{.},
             'Cartn_x' => -0.029,
             'Cartn_y' => 0.446,
             'Cartn_z' => 1.769,
-            'auth_seq_id' => undef,
-            'auth_comp_id' => 'SER',
-            'auth_asym_id' => undef,
-            'auth_atom_id' => 'OG',
-            'pdbx_PDB_model_num' => undef,
         },
     },
 );
@@ -86,7 +91,14 @@ our %SIDECHAINS = (
 
 sub replace_with_moiety
 {
-    my ( $atom_site, $unique_residue_key, $moiety ) = @_;
+    my ( $atom_site, $unique_residue_key, $moiety, $options ) = @_;
+
+    my ( $isomer ) = $options->{'isomer'};
+    $isomer //= 'R';
+
+    my $residue_site =
+        filter_by_unique_residue_key( $atom_site, $unique_residue_key );
+
 }
 
 1;
