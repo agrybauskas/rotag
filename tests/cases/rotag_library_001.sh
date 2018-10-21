a3e282b00494f3b91493ed537dc11eac2e786ff9
algirdas <algirdas@lama>
2018-10-21 18:28:23 +0000

    Added strict parameter values to rotag_library test cases for the outputs to be reproduced.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1181 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_001.sh
+++ tests/cases/rotag_library_001.sh
@@ -2,4 +2,8 @@
 
 pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-001.cif
 
-rotag_library --cutoff-atom 10 --cutoff-residue 0 ${pdbx_file}
+rotag_library \
+    --parameters 'lj_epsilon=1.0,c_k=1.0,h_epsilon=1.0,r_sigma=2.0,cutoff_start=2.5,cutoff_end=5.0' \
+    --cutoff-atom 0.4 \
+    --cutoff-residue 1.0 \
+    ${pdbx_file}
--- tests/cases/rotag_library_011.sh
+++ tests/cases/rotag_library_011.sh
@@ -2,4 +2,8 @@
 
 pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-011.cif
 
-rotag_library --parameters 'lj_epsilon = 0, c_k = 0' --top-rank 1 ${pdbx_file}
+rotag_library \
+    -H \
+    --parameters 'lj_epsilon=0.0,c_k=0.0,h_epsilon=1.0,r_sigma=2.0,cutoff_start=2.5,cutoff_end=5.0' \
+    --top-rank 1 \
+    ${pdbx_file}
