4050be292f21e67680f0dcc3f721a845509ca7ae
algirdas <algirdas@lama>
2018-10-26 11:58:37 +0000

    Adding tests for generate_library to check usability when different alt ids are present.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1204 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/generate_library_010.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/arginine-002.dump
+conf_model="rotation_only"
+potential="composite"
+energy_cutoff_atom=70.0
+energy_cutoff_residue="Inf"
+residue_id="315"
+residue_chain="B"
+pdbx_model="1"
+residue_alt="B"
+small_angle="1*pi"
+parameters='lj_epsilon=1.0,c_k=1.0,h_epsilon=1.0,r_sigma=2.0,cutoff_atom=0.5,cutoff_residue=1.0,cutoff_start=2.5,cutoff_end=5.0'
+
+$(dirname "$0")/../scripts/generate_library ${residue_id} \
+                                            ${residue_chain} \
+                                            ${pdbx_model} \
+                                            ${residue_alt} \
+                                            ${conf_model} \
+	                                    ${small_angle} \
+	                                    ${potential} \
+					    ${energy_cutoff_atom} \
+					    ${energy_cutoff_residue} \
+					    ${pdbx_dump_file} \
+                                            ${parameters}
