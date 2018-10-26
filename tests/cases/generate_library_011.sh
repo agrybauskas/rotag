40501cbba70e3d2bc8a1098f388d6fb2e8af6d97
algirdas <algirdas@lama>
2018-10-26 13:26:34 +0000

    Adding more test cases for generate_library().
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1205 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/generate_library_011.sh
+++ tests/cases/generate_library_011.sh
@@ -2,16 +2,16 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/arginine-002.dump
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/glutamine-H-002.dump
 conf_model="rotation_only"
 potential="composite"
 energy_cutoff_atom=70.0
 energy_cutoff_residue="Inf"
-residue_id="315"
+residue_id="2"
 residue_chain="A"
 pdbx_model="1"
-residue_alt="B"
-small_angle="1*pi"
+residue_alt="."
+small_angle="0.25*pi"
 parameters='lj_epsilon=1.0,c_k=1.0,h_epsilon=1.0,r_sigma=2.0,cutoff_atom=0.5,cutoff_residue=1.0,cutoff_start=2.5,cutoff_end=5.0'
 
 $(dirname "$0")/../scripts/generate_library ${residue_id} \
