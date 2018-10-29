980e59707644724b8b0897ae084efacff26f8532
algirdas <algirdas@lama>
2018-10-29 08:24:25 +0000

    Patching generate_library() test case and output.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1215 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/generate_library_011.sh
+++ tests/cases/generate_library_011.sh
@@ -11,7 +11,7 @@ residue_id="2"
 residue_chain="A"
 pdbx_model="1"
 residue_alt="."
-small_angle="0.25*pi"
+small_angle="1*pi"
 parameters='lj_epsilon=1.0,c_k=1.0,h_epsilon=1.0,r_sigma=2.0,cutoff_atom=0.5,cutoff_residue=1.0,cutoff_start=2.5,cutoff_end=5.0'
 
 $(dirname "$0")/../scripts/generate_library ${residue_id} \
