3d4c5ccfb307d2f43390bf9703eb9e32851755bd
algirdas <algirdas@lama>
2018-10-29 14:46:25 +0000

    Added test cases that tests behaviour of rotamers when multiple targets are present.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1227 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/generate_library_012.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/synthetic/xaa-002.dump
+conf_model="rotation_only"
+potential="soft_sphere"
+energy_cutoff_atom=7
+energy_cutoff_residue="Inf"
+residue_id="18"
+residue_chain="A"
+residue_entity="1"
+residue_alt="."
+small_angle="0.1*pi"
+parameters='lj_epsilon=1.0,c_k=1.0,h_epsilon=1.0,r_sigma=2.0,cutoff_atom=0.5,cutoff_residue=1.0,cutoff_start=2.5,cutoff_end=5.0'
+
+$(dirname "$0")/../scripts/generate_library ${residue_id} \
+                                            ${residue_chain} \
+                                            ${residue_entity} \
+                                            ${residue_alt} \
+                                            ${conf_model} \
+	                                    ${small_angle} \
+	                                    ${potential} \
+					    ${energy_cutoff_atom} \
+					    ${energy_cutoff_residue} \
+					    ${pdbx_dump_file} \
+                                            ${parameters}
