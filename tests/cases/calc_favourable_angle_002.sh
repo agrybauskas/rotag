c9126b9b604cfbd4abe323b3e8bd0d6f81fad85d
algirdas <algirdas@lama>
2018-10-29 22:33:32 +0000

    Patched calc_favourable_angle{s} tests outputs and scripts.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1236 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/calc_favourable_angle_002.sh
+++ tests/cases/calc_favourable_angle_002.sh
@@ -6,7 +6,7 @@ pdbx_dump_file=$(dirname "$0")/../inputs/synthetic/xaa-rotation-only-002.dump
 atom_id="3"
 interaction_specifier="label_atom_id N,C,CA,O"
 energy_cutoff_atom=0
-checkable_angles="0.1*pi;0.2*pi;0.3*pi;0.4*pi;0.5*pi;0.6*pi;0.7*pi;0.8*pi;0.9*pi;1*pi"
+checkable_angles="0.1*pi;0.2*pi;0.3*pi;0.4*pi;0.5*pi;0.6*pi;0.7*pi;0.8*pi;0.9*pi;1*pi;1.1*pi;1.2*pi;1.3*pi;1.4*pi;1.5*pi;1.6*pi;1.7*pi;1.8*pi;1.9*pi;2*pi"
 
 $(dirname "$0")/../scripts/calc_favourable_angle \
                ${atom_id} \
