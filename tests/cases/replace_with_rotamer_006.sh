e5fe6455ce47fa6254485754a239bc714265b02c
algirdas <algirdas@lama>
2018-10-29 22:51:16 +0000

    Fixed issue with not generating library for xaa-002.cif. First, problem was in determine_residue_keys() - unique keys were not added due to 'last' statement. Secondly, xaa-002.dump lacked conformation. replace_with_rotamer() has to have conformation before generating the rotamer.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1237 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/replace_with_rotamer_006.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/synthetic/xaa-rotation-only-002.dump
+residue_unique_key="18,A,1,."
+angle_values="chi1 0.0"
+
+$(dirname "$0")/../scripts/replace_with_rotamer "${residue_unique_key}" \
+	                                        "${angle_values}" \
+					        ${pdbx_dump_file}
