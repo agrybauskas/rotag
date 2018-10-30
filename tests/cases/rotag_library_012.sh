ab69d9106eceb885c5bab9879cb195a3153d7787
algirdas <algirdas@lama>
2018-10-30 08:36:13 +0000

    At last, fixed rotag_library and how it deals with pdbx models and alt ids.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1240 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/rotag_library_012.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-012.cif
+
+rotag_library \
+    -H \
+    --parameters 'lj_epsilon=1.0,c_k=1.0,h_epsilon=1.0,r_sigma=2.0,cutoff_start=2.5,cutoff_end=5.0' \
+    --top-rank 1 \
+    ${pdbx_file}
