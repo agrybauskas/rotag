5a26fa2e65734675670acacc3a0647cba9dcbcb6
algirdas <algirdas@lama>
2018-11-02 11:04:13 +0000

    Removed chk files where they are obsolete.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1264 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_and_energy_001.sh
+++ tests/cases/rotag_library_and_energy_001.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-001.cif
-pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-library-001.cif
 
-rotag_library -c 'Inf' -C 'Inf' -t 1 ${pdbx_file}
-rotag_dihedral -r ${pdbx_file}
-rotag_energy -r ${pdbx_file}
+rotag_library -c 'Inf' -C 'Inf' -t 1 ${pdbx_file} \
+    | rotag_energy -r
