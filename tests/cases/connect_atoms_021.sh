e7284c3f060cb7c7ca85bfde4db7839381c036f9
algirdas <algirdas@lama>
2018-11-09 11:10:18 +0000

    It looks that rotag_add script does not adjust to alternative ids.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1296 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/connect_atoms_021.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/polipeptides/dipeptide-001.dump
+
+$(dirname "$0")/../scripts/connect_atoms ${pdbx_dump_file} | sort -k 1 -n
