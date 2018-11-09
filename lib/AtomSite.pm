4af69ca642df7b89783cbe0d286970a54ee35805
algirdas <algirdas@lama>
2018-11-09 16:09:56 +0000

    Added filter method to AtomSite object.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1302 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/AtomSite.pm
+++ lib/AtomSite.pm
@@ -3,6 +3,8 @@ package AtomSite;
 use strict;
 use warnings;
 
+use List::MoreUtils qw( any );
+
 use PDBxParser qw( pdbx_loop_unique
                    obtain_pdbx_loop );
 
@@ -25,11 +27,12 @@ sub destroy
 
 #
 # From PDBx file, obtains data only from _atom_site category and outputs special
-# data structure that represents atom data.
+# atom data structure that represents atom data.
 # Input:
 #     $pdbx_file - PDBx file.
 # Output:
-#     %atom_site - special data structure.
+#     none - atom data structure is created and stored in $self->{'atom_site'}.
+#
 #     Ex.: { 1 => { 'group_id' => 'ATOM',
 #                   'id'       => 1,
 #                   ... } }
@@ -45,11 +48,86 @@ sub open
     return;
 }
 
+#
+# Returns atom data structure.
+# Output:
+#     atom data structure.
+#
+
 sub get_atom_site
 {
     my ( $self ) = @_;
 
     return $self->{'atom_site'};
+}
+
+#
+# Filters atom data structure according to specified attributes with include,
+# exclude options.
+# Input:
+#     $options->{'include'} - attribute selector that includes atom data
+#     structure.
+#     Ex.:
+#         { 'label_atom_id' => [ 'N', 'CA', 'CB', 'CD' ],
+#           'label_comp_id' => [ 'A' ] };
+#     $options->{'exclude'} - attribute selector that excludes atom data
+#     structure.
+# Output:
+#     \%filtered_atoms- filtered atom data structure;
+#
+
+sub filter
+{
+    my ( $self, $options ) = @_;
+    my ( $include, $exclude ) =
+        ( $options->{'include'}, $options->{'exclude'} );
+
+    my $atom_site = $self->{'atom_site'};
+
+    if( ! defined $self->{'atom_site'} ) {
+        die 'No atom were loaded to the AtomSite data structure.';
+    }
+
+    # Iterates through each atom in $self->{'atom_site'} and checks if atom
+    # specifiers match up.
+    my %filtered_atom_site;
+
+    # First, filters atoms that are described in $self->{'include'} specifier.
+    if( defined $include ) {
+        for my $atom_id ( keys %{ $atom_site } ) {
+            my $match_counter = 0; # Tracks if all matches occured.
+            for my $attribute ( keys %{ $include } ) {
+                if( exists $atom_site->{$atom_id}{$attribute} &&
+                    any { $atom_site->{$atom_id}{$attribute} eq $_ }
+                       @{ $include->{$attribute} } ) {
+                    $match_counter += 1;
+                } else {
+                    last; # Terminates early if no match is found in specifier.
+                }
+            }
+            if( $match_counter == scalar keys %{ $include } ) {
+                $filtered_atom_site{$atom_id} = $atom_site->{$atom_id};
+            }
+        }
+    } else {
+        %filtered_atom_site = %{ $atom_site };
+    }
+
+    # Then filters out atoms that are in $self->{'exclude'} specifier.
+    if( defined $exclude ) {
+        for my $atom_id ( keys %filtered_atom_site ) {
+            for my $attribute ( keys %{ $exclude } ) {
+                if( exists $atom_site->{$atom_id}{$attribute} &&
+                    any { $atom_site->{$atom_id}{$attribute} eq $_ }
+                       @{ $exclude->{$attribute} } ) {
+                    delete $filtered_atom_site{$atom_id};
+                    last;
+                }
+            }
+        }
+    }
+
+    return \%filtered_atom_site;
 }
 
 1;
---
+++ tests/cases/AtomSite.pm_filter_001.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+export PDBX_FILE=$(dirname "$0")/../inputs/5svd.cif
+
+perl <<'END'
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use AtomSite;
+
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 1;
+
+my $atom_site = AtomSite->new();
+$atom_site->open( $ENV{PDBX_FILE} );
+
+print Dumper $atom_site->filter( { 'include' =>
+                                       { 'label_atom_id' => [ 'CA', 'CB' ] } } );
+
+END
