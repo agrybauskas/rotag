06aa3c57fbd6623da139dcded2cc30b26044fb6d
algirdas <algirdas@lama>
2018-11-18 09:13:57 +0000

    Changing '&' and '|' to standard '&&' and '||' tokens in SelectionParser.yp.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1321 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_select_004.sh
+++ tests/cases/rotag_select_004.sh
@@ -2,5 +2,5 @@
 
 pdbx_file=$(dirname "$0")/../inputs/2cj3.cif
 
-rotag_select -x 22 -t 'byres((atomname CA & resname TRP) rand 2)' ${pdbx_file} 2>&1 \
+rotag_select -x 22 -t 'byres((atomname CA && resname TRP) rand 2)' ${pdbx_file} 2>&1 \
     | sed 's/line\s*[0-9]*/line <row>/g'
--- tests/cases/selection_parser_001.sh
+++ tests/cases/selection_parser_001.sh
@@ -3,7 +3,7 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/5svd.dump
-selection_command="resname ASP & atomname CA"
+selection_command="resname ASP && atomname CA"
 
 "$(dirname "$0")"/../scripts/selection_parser "${selection_command}" \
 		                              "${pdbx_dump_file}"
