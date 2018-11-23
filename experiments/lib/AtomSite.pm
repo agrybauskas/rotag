d9b4051d0c21086da8ed6572c915e181d581841d
algirdas <algirdas@lama>
2018-11-23 08:31:18 +0000

    Moved old AtomSite.pm back to current library, because atom site objects and methods will be used.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1356 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- experiments/lib/AtomSite.pm
+++ experiments/lib/AtomSite.pm
@@ -3,382 +3,76 @@ package AtomSite;
 use strict;
 use warnings;
 
-use List::MoreUtils qw( any
-                        uniq );
-use List::Util qw( max );
+use Exporter qw( import );
+our @EXPORT_OK = qw( append_atom_site
+                     prepare_for_calc );
 
 use BondProperties qw( hybridization );
 use ConnectAtoms qw( connect_atoms );
-use PDBxParser qw( pdbx_loop_unique
-                   obtain_pdbx_loop
-                   to_pdbx );
+use SidechainModels qw( rotation_only );
 
-# ------------------------- Constructor and destructor ------------------------ #
+use Version qw( $VERSION );
 
-sub new
-{
-    my ( $class, $options ) = @_;
-    my ( $update_bonds ) = ( $options->{'update_bonds'} );
-    $update_bonds //= 0;
+our $VERSION = $VERSION;
 
-    my $self = { 'last_atom_id' => 0, 'update_bonds' => $update_bonds };
-
-    return bless $self, $class;
-}
-
-sub DESTROY
-{
-    my ( $self ) = @_;
-}
-
-# --------------------------------- Methods ----------------------------------- #
+# ------------------------ Manipulations with atom site ----------------------- #
 
 #
-# From PDBx file, obtains data only from _atom_site category and outputs special
-# atom data structure that represents atom data.
+# Appends atom site and checks if there are no duplicate ids.
 # Input:
-#     $pdbx_file - PDBx file.
+#     $atom_sites - array of atom site data structures (see PDBxParser.pm);
+#     $options->{'renumber_from'} - renumbers atoms starting from certain int.
 # Output:
-#     none - atom data structure is created and stored in $self->{'atoms'}.
-#
-#     Ex.: { 1 => { 'group_id' => 'ATOM',
-#                   'id'       => 1,
-#                   ... } }
+#     %atom_site - concatinated atom site.
 #
 
-sub open
+sub append_atom_site
 {
-    my ( $self, $pdbx_file ) = @_;
+    my ( $atom_sites, $options ) = @_;
+    my ( $renumber_from ) = $options->{'renumber_from'};
 
-    $self->{'atoms'} =
-        pdbx_loop_unique( obtain_pdbx_loop( $pdbx_file, [ '_atom_site' ] ) );
+    $renumber_from //= 0;
 
-    if( $self->{'update_bonds'} ) {
-        connect_atoms( $self->{'atoms'} );
-        hybridization( $self->{'atoms'} );
-    }
-
-    $self->{'last_atom_id'} = max( keys %{ $self->{'atoms'} } );
-
-    return;
-}
-
-#
-# Creates atom data structure item.
-# Input:
-#     $options - hash of all necessary attributes with corresponding values;
-# Output:
-#     atom data structure item.
-#
-
-sub create
-{
-    my ( $self, $options ) = @_;
-    my $atom_id = $options->{'id'};
-    $atom_id //= max( keys %{ $self->{'atoms'} } );
-    my $type_symbol = $options->{'type_symbol'};
-    my $label_atom_id = $options->{'label_atom_id'};
-    my $label_alt_id = $options->{'label_alt_id'};
-    $label_alt_id //= q{.};
-    my $label_comp_id = $options->{'label_comp_id'};
-    my $label_asym_id = $options->{'label_asym_id'};
-    my $label_entity_id = $options->{'label_entity_id'};
-    $label_entity_id //= q{?};
-    my $label_seq_id = $options->{'label_seq_id'};
-    my $cartn_x = $options->{'cartn_x'};
-    my $cartn_y = $options->{'cartn_y'};
-    my $cartn_z = $options->{'cartn_z'};
-    my $pdbx_model_num = $options->{'pdbx_PDB_model_num'};
-
-    if( ! exists $self->{'atoms'}{$atom_id} ) {
-        $self->{'atoms'}{$atom_id}{'group_PDB'} = 'ATOM';
-        $self->{'atoms'}{$atom_id}{'id'} = $atom_id;
-        $self->{'atoms'}{$atom_id}{'type_symbol'} = $type_symbol;
-        $self->{'atoms'}{$atom_id}{'label_atom_id'} = $label_atom_id;
-        $self->{'atoms'}{$atom_id}{'label_alt_id'} = $label_alt_id;
-        $self->{'atoms'}{$atom_id}{'label_comp_id'} = $label_comp_id;
-        $self->{'atoms'}{$atom_id}{'label_asym_id'} = $label_asym_id;
-        $self->{'atoms'}{$atom_id}{'label_entity_id'} = $label_entity_id;
-        $self->{'atoms'}{$atom_id}{'label_seq_id'} = $label_seq_id;
-        $self->{'atoms'}{$atom_id}{'Cartn_x'} = $cartn_x;
-        $self->{'atoms'}{$atom_id}{'Cartn_y'} = $cartn_y;
-        $self->{'atoms'}{$atom_id}{'Cartn_z'} = $cartn_z;
-        $self->{'atoms'}{$atom_id}{'pdbx_PDB_model_num'} = $pdbx_model_num;
-    } else {
-        die 'Specified atom id is already present in the atom site';
-    }
-
-    if( $self->{'update_bonds'} ) {
-        connect_atoms( $self->{'atoms'} );
-        hybridization( $self->{'atoms'} );
-    }
-
-    return;
-}
-
-#
-# Appends atom data structure to the current object.
-# Input:
-#     $atom_sites - list of appendable atom data structures.
-#     $renumber - renumbers the ids.
-# Output:
-#     none - atom sites are appended to the structure.
-#
-
-sub append
-{
-    my ( $self ) = shift;
-    my ( $atom_sites, $renumber ) = @_;
-    $renumber //= 0;
-
+    my %atom_site;
     for my $atom_site ( @{ $atom_sites } ) {
-        if( $renumber ) {
-            for my $atom_id ( sort keys %{ $atom_site } ) {
-                $self->{'atoms'}{$self->{'last_atom_id'}+1} =
-                    $atom_site->{$atom_id};
-                $self->{'atoms'}{$self->{'last_atom_id'}+1}{'id'} =
-                    $self->{'last_atom_id'}+1;
-                $self->{'last_atom_id'}++;
-            }
-        } else {
-            for my $atom_id ( sort keys %{ $atom_site } ) {
-                if( ! exists $self->{'atoms'}{$atom_id} ) {
-                    $self->{'atoms'}{$atom_id} = $atom_site->{$atom_id};
+        for my $atom_id ( sort keys %{ $atom_site } ) {
+            if( ! exists $atom_site{$atom_id} ) {
+                if( $renumber_from ) {
+                    $atom_site{$renumber_from} = $atom_site->{$atom_id};
+                    $atom_site{$renumber_from}{'id'} = $renumber_from;
+                    $renumber_from++;
                 } else {
-                    die "Atom with id $atom_id already exists";
+                    $atom_site{$atom_id} = $atom_site->{$atom_id};
                 }
-            }
-        }
-    }
-
-    if( $self->{'update_bonds'} ) {
-        connect_atoms( $self->{'atoms'} );
-        hybridization( $self->{'atoms'} );
-    }
-
-    $self->{'last_atom_id'} = max( keys %{ $self->{'atoms'} } );
-
-    return;
-}
-
-#
-# Creates an index table that stores atom ids according to the attribute data.
-# Output:
-#     creates an index table.
-# Ex.:
-#     { 'label_atom_id' => { 'CA' => [ 1, 4, 7 ],
-#                            'CB' => [ 2, 5, 8 ] } }
-#
-
-sub index
-{
-    my ( $self ) = @_;
-    my $atoms = $self->{'atoms'};
-
-    my %index_table;
-
-    for my $atom_id ( keys %{ $atoms } ) {
-        my $atom = $atoms->{$atom_id};
-
-        for my $attribute ( keys %{ $atom } ) {
-            my $value = $atom->{$attribute};
-            next if ref $value;
-            if( exists $index_table{"$attribute"} &&
-                exists $index_table{"$attribute"}{"$value"} ) {
-                push @{ $index_table{"$attribute"}{"$value"} }, $atom_id;
-                # $index_table{"$attribute"}{"$value"} =
-                #     [ sort { $a <=> $b }
-                #           @{ $index_table{"$attribute"}{"$value"} } ];
             } else {
-                $index_table{"$attribute"}{"$value"} = [ $atom_id ];
+                die "atom site data structure with identical ids are being " .
+                    "concatinated";
             }
         }
     }
 
-    $self->{'index_table'} = { %index_table };
+    return \%atom_site;
+}
+
+#
+# Prepares atom site data structure for calculations - adds connections,
+# assigns hybridizations and rotational bonds.
+# Input:
+#     $atom_site - atom site data structure (see PDBxParser.pm).
+# Output:
+#     hybridization, connections and conformation attributes are added to the
+#     $atom_site.
+#
+
+sub prepare_for_calc
+{
+    my ( $atom_site ) = @_;
+
+    connect_atoms( $atom_site );
+    hybridization( $atom_site );
+    rotation_only( $atom_site );
 
     return;
-}
-
-#
-# Adds T (target), S (surrounding or selected), I (ignored) char to
-# '[local]_selection_state' attribute data.
-# Input:
-#     $options{'target'} - list of ids of the target atom;
-#     $options{'select'} - list of ids of the selected atom;
-#     $options{'ignore'} - list of ids of the ignored atom.
-# Output:
-#     adds markers to specified attribute field.
-#
-
-sub mark_selection
-{
-    my ( $self, $options ) = @_;
-
-    my ( $target_atom_ids, $selected_atom_ids ) =
-        ( $options->{'target'}, $options->{'select'}, );
-
-    for my $atom_id ( keys %{ $self->{'atoms'} } ) {
-        if( any { $atom_id eq $_  } @{ $target_atom_ids } ) {
-            $self->{'atoms'}{$atom_id}{'[local]_selection_state'} = 'T';
-        } elsif( any { $atom_id eq $_  } @{ $selected_atom_ids } ) {
-            $self->{'atoms'}{$atom_id}{'[local]_selection_state'} = 'S';
-        } else {
-            $self->{'atoms'}{$atom_id}{'[local]_selection_state'} = 'I';
-        }
-    }
-
-    return;
-}
-
-#
-# Filters atom data structure according to specified attributes with include,
-# exclude options.
-# Input:
-#     $options->{'include'} - attribute selector that includes atom data
-#     structure.
-#     Ex.:
-#         { 'label_atom_id' => [ 'N', 'CA', 'CB', 'CD' ],
-#           'label_comp_id' => [ 'A' ] };
-#     $options->{'exclude'} - attribute selector that excludes atom data
-#     $options->{'return_ids'} - flag that make function return filtered atom
-#     ids instead of new AtomSite data structure.
-#     $options->{'group_id'} - assigns the value of described group id to
-#     '[local]_selection_group' attribute.
-#     structure.
-# Output:
-#     \%filtered_atoms - filtered atom data structure;
-#            or
-#     \@filtered_atom_ids - list of filtered atom ids.
-#
-
-sub filter
-{
-    my ( $self, $options ) = @_;
-    my ( $include, $exclude, $return_ids, $group_id ) =
-        ( $options->{'include'},    $options->{'exclude'},
-          $options->{'return_ids'}, $options->{'group_id'} );
-
-    $return_ids //= 0;
-
-    my $atoms = $self->{'atoms'};
-
-    if( ! defined $self->{'atoms'} ) {
-        die 'No atom were loaded to the AtomSite data structure';
-    }
-
-    # Iterates through each atom in $self->{'atoms'} and checks if atom
-    # specifiers match up.
-    my %filtered_atoms;
-
-    # First, filters atoms that are described in $self->{'include'} specifier.
-    if( defined $include ) {
-        for my $atom_id ( keys %{ $atoms } ) {
-            my $match_counter = 0; # Tracks if all matches occured.
-            for my $attribute ( keys %{ $include } ) {
-                if( exists $atoms->{$atom_id}{$attribute} &&
-                    any { $atoms->{$atom_id}{$attribute} eq $_ }
-                       @{ $include->{$attribute} } ) {
-                    $match_counter += 1;
-                } else {
-                    last; # Terminates early if no match is found in specifier.
-                }
-            }
-
-            if( $match_counter == scalar keys %{ $include } ) {
-                $filtered_atoms{$atom_id} = $atoms->{$atom_id};
-
-                # Assigns group id.
-                if( defined $group_id ) {
-                    $filtered_atoms{$atom_id}{'[local]_selection_group'} =
-                        $group_id;
-                }
-            }
-        }
-    } else {
-        %filtered_atoms = %{ $atoms };
-    }
-
-    # Then filters out atoms that are in $self->{'exclude'} specifier.
-    if( defined $exclude ) {
-        for my $atom_id ( keys %filtered_atoms ) {
-            for my $attribute ( keys %{ $exclude } ) {
-                if( exists $atoms->{$atom_id}{$attribute} &&
-                    any { $atoms->{$atom_id}{$attribute} eq $_ }
-                       @{ $exclude->{$attribute} } ) {
-                    delete $filtered_atoms{$atom_id};
-                    last;
-                }
-            }
-        }
-    }
-
-    # Return object handle or atom ids depending on the flag.
-    if( $return_ids ) {
-        return [ keys %filtered_atoms ]
-    } else {
-        my $filtered_atom_site = AtomSite->new();
-        $filtered_atom_site->append( [ \%filtered_atoms ] );
-
-        return $filtered_atom_site;
-    }
-}
-
-#
-# Extracts information from atom data structure.
-# Input:
-#     $options{'data'} -
-#     $options{'data_with_id'} -
-#     $options{'is_list'} -
-# Output:
-#
-
-sub extract
-{
-    my ( $self, $options ) = @_;
-    my ( $data, $data_with_id, $is_list ) =
-        ( $options->{'data'}, $options->{'data_with_id'}, $options->{'is_list'});
-
-    my @atom_data;
-    my $atom_site = $self->{'atoms'};
-
-    if( defined $data_with_id && $data_with_id ) {
-        my %atom_data_with_id;
-
-        # Simply iterates through $atom_site keys and extracts data using
-        # data specifier and is asigned to atom id.
-        for my $atom_id ( sort { $a <=> $b } keys %{ $atom_site } ) {
-            $atom_data_with_id{$atom_id} =
-                [ map { $atom_site->{$atom_id}{$_} } @{ $data } ];
-        }
-        return \%atom_data_with_id;
-    } else {
-        for my $atom_id ( sort { $a <=> $b } keys %{ $atom_site } ) {
-            if( defined $is_list && $is_list ) {
-                push @atom_data,
-                    map { $atom_site->{$atom_id}{$_} } @{ $data };
-            } else {
-                push @atom_data,
-                    [ map { $atom_site->{$atom_id}{$_} } @{ $data } ];
-            }
-        }
-        return \@atom_data;
-    }
-}
-
-#
-# Converts atom site data structure to PDBx.
-# Input:
-#     $options->{'atoms'} - atom site data structure.
-# Output:
-#     STDERR - PDBx format file.
-#
-
-sub pdbx
-{
-    my ( $self ) = @_;
-
-    return to_pdbx( { 'atom_site' => $self->{'atoms'}, 'data_name' => '' } );
 }
 
 1;
--- lib/AtomSite.pm
+++ lib/AtomSite.pm
@@ -3,76 +3,382 @@ package AtomSite;
 use strict;
 use warnings;
 
-use Exporter qw( import );
-our @EXPORT_OK = qw( append_atom_site
-                     prepare_for_calc );
+use List::MoreUtils qw( any
+                        uniq );
+use List::Util qw( max );
 
 use BondProperties qw( hybridization );
 use ConnectAtoms qw( connect_atoms );
-use SidechainModels qw( rotation_only );
+use PDBxParser qw( pdbx_loop_unique
+                   obtain_pdbx_loop
+                   to_pdbx );
 
-use Version qw( $VERSION );
+# ------------------------- Constructor and destructor ------------------------ #
 
-our $VERSION = $VERSION;
-
-# ------------------------ Manipulations with atom site ----------------------- #
-
-#
-# Appends atom site and checks if there are no duplicate ids.
-# Input:
-#     $atom_sites - array of atom site data structures (see PDBxParser.pm);
-#     $options->{'renumber_from'} - renumbers atoms starting from certain int.
-# Output:
-#     %atom_site - concatinated atom site.
-#
-
-sub append_atom_site
+sub new
 {
-    my ( $atom_sites, $options ) = @_;
-    my ( $renumber_from ) = $options->{'renumber_from'};
+    my ( $class, $options ) = @_;
+    my ( $update_bonds ) = ( $options->{'update_bonds'} );
+    $update_bonds //= 0;
 
-    $renumber_from //= 0;
+    my $self = { 'last_atom_id' => 0, 'update_bonds' => $update_bonds };
 
-    my %atom_site;
+    return bless $self, $class;
+}
+
+sub DESTROY
+{
+    my ( $self ) = @_;
+}
+
+# --------------------------------- Methods ----------------------------------- #
+
+#
+# From PDBx file, obtains data only from _atom_site category and outputs special
+# atom data structure that represents atom data.
+# Input:
+#     $pdbx_file - PDBx file.
+# Output:
+#     none - atom data structure is created and stored in $self->{'atoms'}.
+#
+#     Ex.: { 1 => { 'group_id' => 'ATOM',
+#                   'id'       => 1,
+#                   ... } }
+#
+
+sub open
+{
+    my ( $self, $pdbx_file ) = @_;
+
+    $self->{'atoms'} =
+        pdbx_loop_unique( obtain_pdbx_loop( $pdbx_file, [ '_atom_site' ] ) );
+
+    if( $self->{'update_bonds'} ) {
+        connect_atoms( $self->{'atoms'} );
+        hybridization( $self->{'atoms'} );
+    }
+
+    $self->{'last_atom_id'} = max( keys %{ $self->{'atoms'} } );
+
+    return;
+}
+
+#
+# Creates atom data structure item.
+# Input:
+#     $options - hash of all necessary attributes with corresponding values;
+# Output:
+#     atom data structure item.
+#
+
+sub create
+{
+    my ( $self, $options ) = @_;
+    my $atom_id = $options->{'id'};
+    $atom_id //= max( keys %{ $self->{'atoms'} } );
+    my $type_symbol = $options->{'type_symbol'};
+    my $label_atom_id = $options->{'label_atom_id'};
+    my $label_alt_id = $options->{'label_alt_id'};
+    $label_alt_id //= q{.};
+    my $label_comp_id = $options->{'label_comp_id'};
+    my $label_asym_id = $options->{'label_asym_id'};
+    my $label_entity_id = $options->{'label_entity_id'};
+    $label_entity_id //= q{?};
+    my $label_seq_id = $options->{'label_seq_id'};
+    my $cartn_x = $options->{'cartn_x'};
+    my $cartn_y = $options->{'cartn_y'};
+    my $cartn_z = $options->{'cartn_z'};
+    my $pdbx_model_num = $options->{'pdbx_PDB_model_num'};
+
+    if( ! exists $self->{'atoms'}{$atom_id} ) {
+        $self->{'atoms'}{$atom_id}{'group_PDB'} = 'ATOM';
+        $self->{'atoms'}{$atom_id}{'id'} = $atom_id;
+        $self->{'atoms'}{$atom_id}{'type_symbol'} = $type_symbol;
+        $self->{'atoms'}{$atom_id}{'label_atom_id'} = $label_atom_id;
+        $self->{'atoms'}{$atom_id}{'label_alt_id'} = $label_alt_id;
+        $self->{'atoms'}{$atom_id}{'label_comp_id'} = $label_comp_id;
+        $self->{'atoms'}{$atom_id}{'label_asym_id'} = $label_asym_id;
+        $self->{'atoms'}{$atom_id}{'label_entity_id'} = $label_entity_id;
+        $self->{'atoms'}{$atom_id}{'label_seq_id'} = $label_seq_id;
+        $self->{'atoms'}{$atom_id}{'Cartn_x'} = $cartn_x;
+        $self->{'atoms'}{$atom_id}{'Cartn_y'} = $cartn_y;
+        $self->{'atoms'}{$atom_id}{'Cartn_z'} = $cartn_z;
+        $self->{'atoms'}{$atom_id}{'pdbx_PDB_model_num'} = $pdbx_model_num;
+    } else {
+        die 'Specified atom id is already present in the atom site';
+    }
+
+    if( $self->{'update_bonds'} ) {
+        connect_atoms( $self->{'atoms'} );
+        hybridization( $self->{'atoms'} );
+    }
+
+    return;
+}
+
+#
+# Appends atom data structure to the current object.
+# Input:
+#     $atom_sites - list of appendable atom data structures.
+#     $renumber - renumbers the ids.
+# Output:
+#     none - atom sites are appended to the structure.
+#
+
+sub append
+{
+    my ( $self ) = shift;
+    my ( $atom_sites, $renumber ) = @_;
+    $renumber //= 0;
+
     for my $atom_site ( @{ $atom_sites } ) {
-        for my $atom_id ( sort keys %{ $atom_site } ) {
-            if( ! exists $atom_site{$atom_id} ) {
-                if( $renumber_from ) {
-                    $atom_site{$renumber_from} = $atom_site->{$atom_id};
-                    $atom_site{$renumber_from}{'id'} = $renumber_from;
-                    $renumber_from++;
+        if( $renumber ) {
+            for my $atom_id ( sort keys %{ $atom_site } ) {
+                $self->{'atoms'}{$self->{'last_atom_id'}+1} =
+                    $atom_site->{$atom_id};
+                $self->{'atoms'}{$self->{'last_atom_id'}+1}{'id'} =
+                    $self->{'last_atom_id'}+1;
+                $self->{'last_atom_id'}++;
+            }
+        } else {
+            for my $atom_id ( sort keys %{ $atom_site } ) {
+                if( ! exists $self->{'atoms'}{$atom_id} ) {
+                    $self->{'atoms'}{$atom_id} = $atom_site->{$atom_id};
                 } else {
-                    $atom_site{$atom_id} = $atom_site->{$atom_id};
+                    die "Atom with id $atom_id already exists";
                 }
-            } else {
-                die "atom site data structure with identical ids are being " .
-                    "concatinated";
             }
         }
     }
 
-    return \%atom_site;
+    if( $self->{'update_bonds'} ) {
+        connect_atoms( $self->{'atoms'} );
+        hybridization( $self->{'atoms'} );
+    }
+
+    $self->{'last_atom_id'} = max( keys %{ $self->{'atoms'} } );
+
+    return;
 }
 
 #
-# Prepares atom site data structure for calculations - adds connections,
-# assigns hybridizations and rotational bonds.
-# Input:
-#     $atom_site - atom site data structure (see PDBxParser.pm).
+# Creates an index table that stores atom ids according to the attribute data.
 # Output:
-#     hybridization, connections and conformation attributes are added to the
-#     $atom_site.
+#     creates an index table.
+# Ex.:
+#     { 'label_atom_id' => { 'CA' => [ 1, 4, 7 ],
+#                            'CB' => [ 2, 5, 8 ] } }
 #
 
-sub prepare_for_calc
+sub index
 {
-    my ( $atom_site ) = @_;
+    my ( $self ) = @_;
+    my $atoms = $self->{'atoms'};
 
-    connect_atoms( $atom_site );
-    hybridization( $atom_site );
-    rotation_only( $atom_site );
+    my %index_table;
+
+    for my $atom_id ( keys %{ $atoms } ) {
+        my $atom = $atoms->{$atom_id};
+
+        for my $attribute ( keys %{ $atom } ) {
+            my $value = $atom->{$attribute};
+            next if ref $value;
+            if( exists $index_table{"$attribute"} &&
+                exists $index_table{"$attribute"}{"$value"} ) {
+                push @{ $index_table{"$attribute"}{"$value"} }, $atom_id;
+                # $index_table{"$attribute"}{"$value"} =
+                #     [ sort { $a <=> $b }
+                #           @{ $index_table{"$attribute"}{"$value"} } ];
+            } else {
+                $index_table{"$attribute"}{"$value"} = [ $atom_id ];
+            }
+        }
+    }
+
+    $self->{'index_table'} = { %index_table };
 
     return;
+}
+
+#
+# Adds T (target), S (surrounding or selected), I (ignored) char to
+# '[local]_selection_state' attribute data.
+# Input:
+#     $options{'target'} - list of ids of the target atom;
+#     $options{'select'} - list of ids of the selected atom;
+#     $options{'ignore'} - list of ids of the ignored atom.
+# Output:
+#     adds markers to specified attribute field.
+#
+
+sub mark_selection
+{
+    my ( $self, $options ) = @_;
+
+    my ( $target_atom_ids, $selected_atom_ids ) =
+        ( $options->{'target'}, $options->{'select'}, );
+
+    for my $atom_id ( keys %{ $self->{'atoms'} } ) {
+        if( any { $atom_id eq $_  } @{ $target_atom_ids } ) {
+            $self->{'atoms'}{$atom_id}{'[local]_selection_state'} = 'T';
+        } elsif( any { $atom_id eq $_  } @{ $selected_atom_ids } ) {
+            $self->{'atoms'}{$atom_id}{'[local]_selection_state'} = 'S';
+        } else {
+            $self->{'atoms'}{$atom_id}{'[local]_selection_state'} = 'I';
+        }
+    }
+
+    return;
+}
+
+#
+# Filters atom data structure according to specified attributes with include,
+# exclude options.
+# Input:
+#     $options->{'include'} - attribute selector that includes atom data
+#     structure.
+#     Ex.:
+#         { 'label_atom_id' => [ 'N', 'CA', 'CB', 'CD' ],
+#           'label_comp_id' => [ 'A' ] };
+#     $options->{'exclude'} - attribute selector that excludes atom data
+#     $options->{'return_ids'} - flag that make function return filtered atom
+#     ids instead of new AtomSite data structure.
+#     $options->{'group_id'} - assigns the value of described group id to
+#     '[local]_selection_group' attribute.
+#     structure.
+# Output:
+#     \%filtered_atoms - filtered atom data structure;
+#            or
+#     \@filtered_atom_ids - list of filtered atom ids.
+#
+
+sub filter
+{
+    my ( $self, $options ) = @_;
+    my ( $include, $exclude, $return_ids, $group_id ) =
+        ( $options->{'include'},    $options->{'exclude'},
+          $options->{'return_ids'}, $options->{'group_id'} );
+
+    $return_ids //= 0;
+
+    my $atoms = $self->{'atoms'};
+
+    if( ! defined $self->{'atoms'} ) {
+        die 'No atom were loaded to the AtomSite data structure';
+    }
+
+    # Iterates through each atom in $self->{'atoms'} and checks if atom
+    # specifiers match up.
+    my %filtered_atoms;
+
+    # First, filters atoms that are described in $self->{'include'} specifier.
+    if( defined $include ) {
+        for my $atom_id ( keys %{ $atoms } ) {
+            my $match_counter = 0; # Tracks if all matches occured.
+            for my $attribute ( keys %{ $include } ) {
+                if( exists $atoms->{$atom_id}{$attribute} &&
+                    any { $atoms->{$atom_id}{$attribute} eq $_ }
+                       @{ $include->{$attribute} } ) {
+                    $match_counter += 1;
+                } else {
+                    last; # Terminates early if no match is found in specifier.
+                }
+            }
+
+            if( $match_counter == scalar keys %{ $include } ) {
+                $filtered_atoms{$atom_id} = $atoms->{$atom_id};
+
+                # Assigns group id.
+                if( defined $group_id ) {
+                    $filtered_atoms{$atom_id}{'[local]_selection_group'} =
+                        $group_id;
+                }
+            }
+        }
+    } else {
+        %filtered_atoms = %{ $atoms };
+    }
+
+    # Then filters out atoms that are in $self->{'exclude'} specifier.
+    if( defined $exclude ) {
+        for my $atom_id ( keys %filtered_atoms ) {
+            for my $attribute ( keys %{ $exclude } ) {
+                if( exists $atoms->{$atom_id}{$attribute} &&
+                    any { $atoms->{$atom_id}{$attribute} eq $_ }
+                       @{ $exclude->{$attribute} } ) {
+                    delete $filtered_atoms{$atom_id};
+                    last;
+                }
+            }
+        }
+    }
+
+    # Return object handle or atom ids depending on the flag.
+    if( $return_ids ) {
+        return [ keys %filtered_atoms ]
+    } else {
+        my $filtered_atom_site = AtomSite->new();
+        $filtered_atom_site->append( [ \%filtered_atoms ] );
+
+        return $filtered_atom_site;
+    }
+}
+
+#
+# Extracts information from atom data structure.
+# Input:
+#     $options{'data'} -
+#     $options{'data_with_id'} -
+#     $options{'is_list'} -
+# Output:
+#
+
+sub extract
+{
+    my ( $self, $options ) = @_;
+    my ( $data, $data_with_id, $is_list ) =
+        ( $options->{'data'}, $options->{'data_with_id'}, $options->{'is_list'});
+
+    my @atom_data;
+    my $atom_site = $self->{'atoms'};
+
+    if( defined $data_with_id && $data_with_id ) {
+        my %atom_data_with_id;
+
+        # Simply iterates through $atom_site keys and extracts data using
+        # data specifier and is asigned to atom id.
+        for my $atom_id ( sort { $a <=> $b } keys %{ $atom_site } ) {
+            $atom_data_with_id{$atom_id} =
+                [ map { $atom_site->{$atom_id}{$_} } @{ $data } ];
+        }
+        return \%atom_data_with_id;
+    } else {
+        for my $atom_id ( sort { $a <=> $b } keys %{ $atom_site } ) {
+            if( defined $is_list && $is_list ) {
+                push @atom_data,
+                    map { $atom_site->{$atom_id}{$_} } @{ $data };
+            } else {
+                push @atom_data,
+                    [ map { $atom_site->{$atom_id}{$_} } @{ $data } ];
+            }
+        }
+        return \@atom_data;
+    }
+}
+
+#
+# Converts atom site data structure to PDBx.
+# Input:
+#     $options->{'atoms'} - atom site data structure.
+# Output:
+#     STDERR - PDBx format file.
+#
+
+sub pdbx
+{
+    my ( $self ) = @_;
+
+    return to_pdbx( { 'atom_site' => $self->{'atoms'}, 'data_name' => '' } );
 }
 
 1;
