7e7cc5a7c69a5c575e829fe5dba0a9eda42b8688
algirdas <algirdas@lama>
2018-12-06 14:59:58 +0000

    Patched few rotag_library tests.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1452 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_015.sh
+++ tests/cases/rotag_library_015.sh
@@ -4,6 +4,8 @@ pdbx_file=$(dirname "$0")/../inputs/amino-acids/arginine-002.cif
 
 rotag_library \
     -H \
+    -c 40.0 \
+    -C 40.0 \
     --angle-step 1.0 \
     --parameters 'lj_epsilon=1.0,c_k=1.0,h_epsilon=1.0,r_sigma=2.0,cutoff_start=2.5,cutoff_end=5.0' \
     --top-rank 1 \
--- tests/cases/rotag_library_add_select_and_dihedral_001.sh
+++ tests/cases/rotag_library_add_select_and_dihedral_001.sh
@@ -5,4 +5,4 @@ pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-001.cif
 rotag_library -c 'Inf' -C 'Inf' -t 5 ${pdbx_file} \
     | rotag_add -r \
     | rotag_select \
-    | rotag_dihedral
+    | rotag_dihedral -r
