889047f06a88a1b5c0c628f3583d35d34f71a619
algirdas <algirdas@lama>
2018-12-21 18:25:20 +0000

    Removed deprecated test cases and outputs.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1554 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/prepare_for_calc_001.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PERL5LIB=$(dirname "$0")/../../lib
-
-pdbx_file_dump=$(dirname "$0")/../inputs/amino-acids/arginine-001.dump
-
-$(dirname "$0")/../scripts/prepare_for_calc ${pdbx_file_dump}
