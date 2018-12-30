35595315a10d057c088bea1bce287c6362179f78
algirdas <algirdas@lama>
2018-12-30 02:55:31 +0000

    Trying to learn reading files and using regex with boost library in obtain_pdbx_loop (C++).
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@1634 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/cpp/PDBxParser.cpp
+++ lib/cpp/PDBxParser.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/regex.hpp>
 #include "PDBxParser.h"
 
 /*
@@ -18,10 +21,42 @@ void obtain_pdbx_loop( std::string pdbx_file,
                        std::vector<std::string> categories,
                        bool read_until_end = false )
 {
-    std::vector<std::string> current_categories;
-    std::vector<std::string> attributes;
-    std::vector<std::string> data; /* Will be used for storing atom data temporarily.*/
+    std::vector< std::vector<std::string> > current_categories;
+    std::vector< std::vector< std::vector<std::string> > > attributes;
+    /* Will be used for storing atom data temporarily.*/
+    std::vector< std::vector< std::vector<std::string> > > data;
 
-    std::string category_regexp = boost::algorithm::join( categories , "|");
+    boost::regex category_regex(
+        "(" + boost::algorithm::join( categories , "|") + ")[.](.+)\n?$"
+    );
+    boost::regex data_regex( "^data_" );
+    boost::regex reading_end_regex( "^_|loop_|#" );
+
     bool is_reading_lines = false; /* Starts/stops reading lines at certain flags. */
+
+    std::ifstream fh( pdbx_file );
+    std::string line;
+    boost::smatch match;
+    if( fh.is_open() ) {
+        while( getline( fh, line ) ) {
+            if( boost::regex_search( line, match, data_regex ) ||
+                current_categories.empty() ) {
+                current_categories.push_back( std::vector<std::string>() );
+                attributes.push_back( std::vector< std::vector<std::string> >() );
+                data.push_back( std::vector< std::vector<std::string> >() );
+            } else if( boost::regex_search( line, match, category_regex ) ) {
+                if( current_categories.back().empty() ||
+                    current_categories.back().back() != match[1] ) {
+                    current_categories.back().push_back( match[1] );
+                    attributes.back().push_back( std::vector<std::string>() );
+                    data.back().push_back( std::vector<std::string>() );
+                }
+                // attributes.back().back().push_back( boost::algorithm::trim( match[2] ) );
+                // std::cout << boost::algorithm::trim( "  d  " ) << std::endl;
+                // boost::algorithm::trim( match[2] );
+                is_reading_lines = true;
+            }
+        }
+        fh.close();
+    }
 }
