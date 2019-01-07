0ba88d4a9c328778c97d6fc1a11fa69db680bb36
algirdas <algirdas@lama>
2019-01-07 00:29:36 +0000

    Refactored torsion().
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1652 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/AtomInteractions.pm
+++ lib/AtomInteractions.pm
@@ -244,57 +244,62 @@ sub coulomb
 
 sub torsion
 {
-    my ( $atom_site, $atom_i_id, $parameters ) = @_;
+    my ( $atom_i_id, $parameters ) = @_;
 
-    my ( $t_epsilon ) = ( $parameters->{'t_epsilon'}, );
+    my ( $t_epsilon, $reference_atom_site ) =
+        ( $parameters->{'t_epsilon'}, $parameters->{'atom_site'} );
 
     $t_epsilon //= 1.0;
 
     # Determines all dihedral angles by searching third neighbours following the
     # connections.
-    my @connection_ids = @{ $atom_site->{$atom_i_id}{'connections'} };
+    my @connection_ids = @{ $reference_atom_site->{$atom_i_id}{'connections'} };
 
     my $torsion_potential = 0;
     for my $neighbour_id ( @connection_ids ) {
         my @second_neighbour_ids =
             grep { $atom_i_id ne $_ }
-                @{ $atom_site->{$neighbour_id}{'connections'} };
+                @{ $reference_atom_site->{$neighbour_id}{'connections'} };
 
         next if ! @second_neighbour_ids;
 
         my @second_atom_names =
-            @{ sort_atom_names( [ map { $atom_site->{$_}{'label_atom_id'} }
-                                      @second_neighbour_ids ] ) };
+            @{ sort_atom_names(
+                   [ map { $reference_atom_site->{$_}{'label_atom_id'} }
+                         @second_neighbour_ids ] ) };
         my ( $second_neighbour_id ) =
-            grep { $atom_site->{$_}{'label_atom_id'} eq $second_atom_names[0] }
+            grep { $reference_atom_site->{$_}{'label_atom_id'} eq
+                   $second_atom_names[0] }
                  @second_neighbour_ids;
 
         my @third_neighbour_ids =
             grep { $neighbour_id ne $_ }
-                @{ $atom_site->{$second_neighbour_id}{'connections'} };
+                @{ $reference_atom_site->{$second_neighbour_id}{'connections'} };
 
         next if ! @third_neighbour_ids;
 
         my @third_atom_names =
-            @{ sort_atom_names( [ map { $atom_site->{$_}{'label_atom_id'} }
-                                      @third_neighbour_ids ] ) };
+            @{ sort_atom_names(
+                   [ map { $reference_atom_site->{$_}{'label_atom_id'} }
+                         @third_neighbour_ids ] ) };
         my ( $third_neighbour_id ) =
-            grep { $atom_site->{$_}{'label_atom_id'} eq $third_atom_names[0] }
+            grep { $reference_atom_site->{$_}{'label_atom_id'} eq
+                   $third_atom_names[0] }
                  @third_neighbour_ids;
 
         my $omega = dihedral_angle(
-            [ [ $atom_site->{$third_neighbour_id}{'Cartn_x'},
-                $atom_site->{$third_neighbour_id}{'Cartn_y'},
-                $atom_site->{$third_neighbour_id}{'Cartn_z'} ],
-              [ $atom_site->{$second_neighbour_id}{'Cartn_x'},
-                $atom_site->{$second_neighbour_id}{'Cartn_y'},
-                $atom_site->{$second_neighbour_id}{'Cartn_z'} ],
-              [ $atom_site->{$neighbour_id}{'Cartn_x'},
-                $atom_site->{$neighbour_id}{'Cartn_y'},
-                $atom_site->{$neighbour_id}{'Cartn_z'} ],
-              [ $atom_site->{$atom_i_id}{'Cartn_x'},
-                $atom_site->{$atom_i_id}{'Cartn_y'},
-                $atom_site->{$atom_i_id}{'Cartn_z'} ] ],
+            [ [ $reference_atom_site->{$third_neighbour_id}{'Cartn_x'},
+                $reference_atom_site->{$third_neighbour_id}{'Cartn_y'},
+                $reference_atom_site->{$third_neighbour_id}{'Cartn_z'} ],
+              [ $reference_atom_site->{$second_neighbour_id}{'Cartn_x'},
+                $reference_atom_site->{$second_neighbour_id}{'Cartn_y'},
+                $reference_atom_site->{$second_neighbour_id}{'Cartn_z'} ],
+              [ $reference_atom_site->{$neighbour_id}{'Cartn_x'},
+                $reference_atom_site->{$neighbour_id}{'Cartn_y'},
+                $reference_atom_site->{$neighbour_id}{'Cartn_z'} ],
+              [ $reference_atom_site->{$atom_i_id}{'Cartn_x'},
+                $reference_atom_site->{$atom_i_id}{'Cartn_y'},
+                $reference_atom_site->{$atom_i_id}{'Cartn_z'} ] ],
         );
 
         $torsion_potential += ( $t_epsilon / 2 ) * ( 1 + cos( 2 * $omega ) )
