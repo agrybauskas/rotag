a1398fb34bb2a01f0e0c96442ba1c0fe037af416
algirdas <algirdas@lama>
2019-01-09 14:43:47 +0000

    Adding GridNew.pm that should "absorb" Grid.pm functions as methods.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/atoms-in-grid-model@1673 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/Grid.pm_grid_box_001.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/alanine-001.dump
+
+$(dirname "$0")/../scripts/Grid.pm_grid_box ${pdbx_dump_file}
