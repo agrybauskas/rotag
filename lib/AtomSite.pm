0238fa91bce2798a4182b39fdc86869f9c11a9b6
algirdas <algirdas@lama>
2019-01-09 21:08:57 +0000

    Added notes on basic data structures for AtomSite variables.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/atoms-in-grid-model@1676 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/AtomSite.pm
+++ lib/AtomSite.pm
@@ -11,10 +11,18 @@ sub new
 {
     my ( $class, $args )  = @_;
 
-    my %self = ( 'atoms'        => $args->{'atoms'},
-                 'connections'  => $args->{'connections'},
-                 'interactions' => $args->{'interactions'},
-                 'grid_box'     => $args->{'grid_box'} );
+    my %self = (
+        # { <id> => { 'id' => <id> , 'Cartn_x' => <Cartn_x>, ... }, ... }
+        'atoms'          => $args->{'atoms'},
+        # { <atoms_id> => [ <atoms_id>, ... ], ... }
+        'connections'    => $args->{'connections'},
+        # { <atom_id> => <hybridization>, ... }
+        'hybridizations' => $args->{'interactions'},
+        # { <atom_id> => { <atom_id> => { <type> => <value> }, ... }, ... }
+        'interactions'   => $args->{'interactions'},
+        # { "<cell_x_id>,<cell_y_id>,<cell_z_ud>" => [ <atom_id>, ... ], ... }
+        'grid_box'       => $args->{'grid_box'}
+    );
 
     return bless \%self, $class;
 }
