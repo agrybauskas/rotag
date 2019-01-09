91d8e411b794cbcf50eaa46aef4551e045641cc0
algirdas <algirdas@lama>
2019-01-09 20:17:26 +0000

    Decided to use atom site data structure/object that stores information on connections, interactions, calculations and etc. Removed GridNew.pm.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/atoms-in-grid-model@1674 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/AtomSite.pm
@@ -0,0 +1,44 @@
+package AtomSite;
+
+use strict;
+use warnings;
+
+use Grid qw( grid_box );
+
+# ----------------------- Constructors and Destructors ------------------------ #
+
+sub new
+{
+    my ( $class, $args )  = @_;
+
+    my %self = ( 'atoms'        => $args->{'atoms'},
+                 'connections'  => $args->{'connections'},
+                 'interactions' => $args->{'interactions'},
+                 'grid_box'     => $args->{'grid_box'} );
+
+    return bless \%self, $class;
+}
+
+# ----------------------------- Setters/Getters ------------------------------- #
+
+sub set_grid_box
+{
+    my ( $self, $edge_length ) = @_;
+
+    ( $self->{'grid_box'} ) = grid_box( $self->{'atoms'}, $edge_length );
+
+    return $self->{'grid_box'};
+}
+
+sub get_grid_box
+{
+    my ( $self ) = @_;
+
+    return $self->{'grid_box'};
+}
+
+# --------------------------------- Methods ----------------------------------- #
+
+# ----------------------------- Static functions ------------------------------ #
+
+1;
--- tests/cases/AtomSite.pm_grid_box_001.sh
+++ tests/cases/AtomSite.pm_grid_box_001.sh
@@ -4,4 +4,4 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/alanine-001.dump
 
-$(dirname "$0")/../scripts/Grid.pm_grid_box ${pdbx_dump_file}
+$(dirname "$0")/../scripts/AtomSite.pm_grid_box ${pdbx_dump_file}
