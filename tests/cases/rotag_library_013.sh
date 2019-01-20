860ad201403903680a18ef60e5f1c3178a05b29b
algirdas <algirdas@lama>
2019-01-20 20:35:45 +0000

    Made parameters to be written into hash form.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1704 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_013.sh
+++ tests/cases/rotag_library_013.sh
@@ -4,6 +4,6 @@ pdbx_file=$(dirname "$0")/../inputs/surrounded/serine-001.cif
 
 rotag_library \
     --cutoff-atom 15.0 \
-    --parameters 'lj_k=1.0,c_k=1.0,h_k=1.0,cutoff_start=2.5,cutoff_end=5.0' \
+    --parameters lj_k=1.0 c_k=1.0 h_k=1.0 cutoff_start=2.5 cutoff_end=5.0 \
     --top-rank 1 \
     ${pdbx_file}
