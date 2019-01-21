0bbcb326c31a7421b91f24fe9854e7f31826963e
algirdas <algirdas@lama>
2019-01-21 09:33:14 +0000

    Changed rotag_energy --parameters behaviour in order to match rotag_library's.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1714 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_energy_001.sh
+++ tests/cases/rotag_energy_001.sh
@@ -4,4 +4,4 @@ pdbx_file=$(dirname "$0")/../inputs/surrounded/serine-H-bonding-001.cif
 
 rotag_energy \
     --potential composite ${pdbx_file} \
-    --parameters lj_k=1.0 c_k=1.0 h_k=1.0 cutoff_start=2.5 cutoff_end=5.0
+    --parameters 'lj_k=1.0, c_k=1.0, h_k=1.0, cutoff_start=2.5, cutoff_end=5.0'
--- tests/cases/rotag_select_008.sh
+++ tests/cases/rotag_select_008.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/amino-acids/lysine-001.cif
 
-rotag_select -t 'atomid 8642 group 3; atomid 8645 group 2' ${pdbx_file}
+rotag_select -t '3:atomid 8642; 2:atomid 8645' ${pdbx_file}
