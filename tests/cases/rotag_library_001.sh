2da391650c150d7fcb52c13d648dac13adbdaf22
algirdas <algirdas@lama>
2019-01-21 09:11:43 +0000

    Decided not to use multi-valued hash options that are built in getopts, because it is not very obvious how to separate options and file without using -i.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1711 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_001.sh
+++ tests/cases/rotag_library_001.sh
@@ -3,6 +3,6 @@
 pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-001.cif
 
 rotag_library \
-    --parameters lj_k=1.0 c_k=1.0 h_k=1.0 cutoff_start=2.5 cutoff_end=5.0 \
+    --parameters 'lj_k=1.0, c_k=1.0, h_k=1.0, cutoff_start=2.5, cutoff_end=5.0' \
     --cutoff-atom 0.4 \
     ${pdbx_file}
