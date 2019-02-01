f3927c5deab55a1b07e073714cc2ff056356d048
algirdas <algirdas@lama>
2019-02-01 09:59:13 +0000

    Patching 'rotag_library' and 'sample_angles' related test outputs.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1740 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_library_add_select_and_dihedral_001.sh
+++ tests/cases/rotag_library_add_select_and_dihedral_001.sh
@@ -2,7 +2,7 @@
 
 pdbx_file=$(dirname "$0")/../inputs/synthetic/xaa-001.cif
 
-rotag_library -c 'Inf' --top-rank 5 ${pdbx_file} \
+rotag_library -c Inf --top-rank 5 ${pdbx_file} \
     | rotag_add -S \
     | rotag_select \
     | rotag_dihedral -S -r
--- tests/cases/sample_angles_qs_parsing_001.sh
+++ tests/cases/sample_angles_qs_parsing_001.sh
@@ -2,7 +2,8 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-$(dirname "$0")/../scripts/sample_angles_qs_parsing 'chi1=0.0..36.0..360.0'
-$(dirname "$0")/../scripts/sample_angles_qs_parsing 'chi1=0.0..36.0..360.0,chi2=0.0..36.0..360.0'
-$(dirname "$0")/../scripts/sample_angles_qs_parsing '0.0..36.0..360.0'
-$(dirname "$0")/../scripts/sample_angles_qs_parsing '0.0..360.0'
+$(dirname "$0")/../scripts/sample_angles_qs_parsing 'chi1=-180.0..36.0..180.0'
+$(dirname "$0")/../scripts/sample_angles_qs_parsing 'chi1=-180.0..36.0..180.0,chi2=-180.0..36.0..180.0'
+$(dirname "$0")/../scripts/sample_angles_qs_parsing '-180.0..36.0..180.0'
+$(dirname "$0")/../scripts/sample_angles_qs_parsing '-180.0..180.0'
+$(dirname "$0")/../scripts/sample_angles_qs_parsing '36.0'
