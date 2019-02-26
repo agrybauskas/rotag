3b8e6874b213733e7162c72c2e299cda55bcee12
algirdas <algirdas@lama>
2019-02-26 19:36:25 +0000

    Started writing first function - create_ref_frame() for LinearAlgebra.cpp.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@1790 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/cpp/LinearAlgebra.cpp
@@ -0,0 +1,12 @@
+#include <LinearAlgebra.h>
+
+/* ------------------------- Numeric linear algebra -------------------------- */
+
+/*
+  Creates local reference frame for any three given atoms positions in cartesian
+  coordinate system.
+  Input:
+      {mid,up,side}_atom_coord - Cartesian coordinates of three atoms.
+  Output:
+      local_ref_frame - Cartesian coordinates of points on x, y and z axis.
+*/
---
+++ lib/cpp/LinearAlgebra.h
@@ -0,0 +1,6 @@
+#ifndef _LINEARALGEBRA_H_
+#define _LINEARALGEBRA_H_
+
+void create_ref_frame();
+
+#endif
