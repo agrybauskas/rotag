69230c134bbf0e63b4fffeac6f5010314c050f8f
algirdas <algirdas@lama>
2019-03-04 20:29:36 +0000

    In test case, the vectors are boundless.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@1822 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/src/create_ref_frame.cpp
+++ tests/src/create_ref_frame.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/split.hpp>
 
 int main( int argc, char* argv[] ) {
-    std::vector<double> mid_atom_coord(3);
-    std::vector<double> up_atom_coord(3);
-    std::vector<double> side_atom_coord(3);
+    std::vector<double> mid_atom_coord;
+    std::vector<double> up_atom_coord;
+    std::vector<double> side_atom_coord;
 
     std::vector<double> coords_xyz;
 
@@ -28,11 +29,12 @@ int main( int argc, char* argv[] ) {
     }
 
     for( int i = 0; i < coords_xyz.size(); i++ ) {
-        if( i / 3 == 0 ) { mid_atom_coord[i] = coords_xyz.at( i ); }
-        if( i / 3 == 1 ) { up_atom_coord[i-3] = coords_xyz.at( i ); }
-        if( i / 3 == 2 ) { side_atom_coord[i-6] = coords_xyz.at( i ); }
+        if( i / 3 == 0 ) { mid_atom_coord.push_back( coords_xyz.at( i ) ); }
+        if( i / 3 == 1 ) { up_atom_coord.push_back( coords_xyz.at( i ) ); }
+        if( i / 3 == 2 ) { side_atom_coord.push_back( coords_xyz.at( i ) ); }
     }
 
     create_ref_frame( mid_atom_coord, up_atom_coord, side_atom_coord );
+
     return 0;
 }
