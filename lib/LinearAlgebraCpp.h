2d8adb89d7c8287a91ab4ba094440242e356e3f1
algirdas <algirdas@lama>
2019-03-04 13:10:29 +0000

    Almost finished writing first test script for create_ref_frame() (C++ version).
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@1819 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/LinearAlgebraCpp.h
+++ lib/LinearAlgebraCpp.h
@@ -7,5 +7,4 @@ void create_ref_frame( std::vector<double> mid_atom_coord,
                        std::vector<double> up_atom_coord,
                        std::vector<double> side_atom_coord );
 
-
 #endif
--- tests/src/create_ref_frame.cpp
+++ tests/src/create_ref_frame.cpp
@@ -1,7 +1,38 @@
 #include "LinearAlgebraCpp.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
 
-int main() {
-    std::cout << "Haha\n";
+int main( int argc, char* argv[] ) {
+    std::vector<double> mid_atom_coord(3);
+    std::vector<double> up_atom_coord(3);
+    std::vector<double> side_atom_coord(3);
+
+    std::vector<double> coords_xyz;
+
+    std::string line;
+    std::ifstream fh( argv[1] );
+    if( fh.is_open() ) {
+        while( getline( fh, line ) ) {
+            std::vector<std::string> line_split;
+            boost::split( line_split, line, boost::is_any_of(" ")  );
+            for( int i = 0; i < line_split.size(); i++ ) {
+                if( line_split[i] != "" ) {
+                    coords_xyz.push_back( std::stod( line_split[i] ) );
+                }
+            }
+        }
+        fh.close();
+    }
+
+    for( int i = 0; i < coords_xyz.size(); i++ ) {
+        if( i / 3 == 0 ) { mid_atom_coord[i] = coords_xyz.at( i ); }
+        if( i / 3 == 1 ) { up_atom_coord[i-3] = coords_xyz.at( i ); }
+        if( i / 3 == 2 ) { side_atom_coord[i-6] = coords_xyz.at( i ); }
+    }
+
+    create_ref_frame( mid_atom_coord, up_atom_coord, side_atom_coord );
     return 0;
 }
