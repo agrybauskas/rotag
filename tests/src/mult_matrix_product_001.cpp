9ae32a096e32eff66bb3337625bd152a9e6f1145
algirdas <algirdas@lama>
2019-03-05 21:33:30 +0000

    Cleaned up unnecessary line of code.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@1844 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/src/mult_matrix_product_001.cpp
+++ tests/src/mult_matrix_product_001.cpp
@@ -25,18 +25,5 @@ int main( int argc, char* argv[] ) {
   algebraic_matrix2.set_symbols( symbols2 );
   algebraic_matrix2.set_matrix_function( matrix2_function );
 
-  // std::vector<std::string> symbols2 = { "x", "y", "z" };
-  // AlgebraicMatrix matrix2(
-  //     false,
-  //     symbols2,
-  //     std::function< std::vector< std::vector<double> >() >
-  //     {
-  //       std::vector< std::vector<double> > tmp_matrix1 = { { 1, 0, 0 },
-  //                                                          { 0, 2, 0 },
-  //                                                          { 0, 0, 3 } };
-  //       return tmp_matrix1;
-  //     }
-  // );
-
   return 0;
 }
