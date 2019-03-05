163e691343c2dfd3729bbb90066a64f3c990ba58
algirdas <algirdas@lama>
2019-03-05 16:29:58 +0000

    End of the session. Function is being tried to be captured in class.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@1835 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/src/mult_matrix_product_001.cpp
+++ tests/src/mult_matrix_product_001.cpp
@@ -1,7 +1,28 @@
 #include "AlgebraicMatrix.h"
 
+#include <functional>
+#include <string>
+#include <vector>
+
 int main( int argc, char* argv[] ) {
-  AlgebraicMatrix matrix1( true, std::vector<std::string> (),
-                           std::vector< std::vector<double> > () );
+  std::vector< std::vector<double> > tmp_matrix1 = { { 1, 0, 0 },
+                                                     { 0, 2, 0 },
+                                                     { 0, 0, 3 } };
+
+  AlgebraicMatrix matrix1( true, std::vector<std::string> (), tmp_matrix1 );
+
+  std::vector<std::string> symbols2 = { "x", "y", "z" };
+  // AlgebraicMatrix matrix2(
+  //     false,
+  //     symbols2,
+  //     std::function< std::vector< std::vector<double> >() >
+  //     {
+  //       std::vector< std::vector<double> > tmp_matrix1 = { { 1, 0, 0 },
+  //                                                          { 0, 2, 0 },
+  //                                                          { 0, 0, 3 } };
+  //       return tmp_matrix1;
+  //     }
+  // );
+
   return 0;
 }
