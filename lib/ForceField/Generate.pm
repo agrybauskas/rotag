394d3e7412d7f0ca933db387febc67461ab61c2e
algirdas <algirdas@lama>
2019-03-12 14:18:00 +0000

    Started Generate.pm that would encapsulate tools/generate-force-field into the separate function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1899 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/ForceField/Generate.pm
@@ -0,0 +1,15 @@
+package ForceField::Generate;
+
+use strict;
+use warnings;
+
+use Exporter qw( import );
+our @EXPORT_OK = qw();
+
+use Version qw( $VERSION );
+
+our $VERSION = $VERSION;
+
+# --------------------------- Potential functions ----------------------------- #
+
+1;
