79ed51c59584fcc0c07304a2d9f0d37156acaeb2
algirdas <algirdas@lama>
2019-03-13 16:00:33 +0000

    End of the session. obtain_force_field_data() currently determines both looped and unlooped data categories.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1910 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/ForceField/Generate.pm
+++ lib/ForceField/Generate.pm
@@ -6,13 +6,38 @@ use warnings;
 use Exporter qw( import );
 our @EXPORT_OK = qw( obtain_force_field_data );
 
+use List::Util qw( uniq );
+
 use Version qw( $VERSION );
 
 our $VERSION = $VERSION;
 
 sub obtain_force_field_data
 {
+    my ( $pdbx_file ) = @_;
 
+    my @lined_categories;
+    my @looped_categories;
+
+    my $is_looped = 0;
+
+    local @ARGV = ( $pdbx_file );
+    while( <> ) {
+        if( m/^loop_/ ) {
+            $is_looped = 1;
+        } elsif( $is_looped && m/^_(\S+)\./ ) {
+            my $category = $1;
+            push @looped_categories, $category;
+        } elsif( m/^_(\S+)\./ ) {
+            my $category = $1;
+            push @lined_categories, $category;
+        } else {
+            $is_looped = 0;
+        }
+    }
+
+    @lined_categories = uniq @lined_categories;
+    @looped_categories = uniq @looped_categories;
 }
 
 1;
