6405fb26b86ac79e9e263f7ccf37dc409c3b87f0
algirdas <algirdas@lama>
2019-03-18 09:13:36 +0000

    Fixed the passing of the arguments to test in calc_favourable_anlgle{s}().
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@1957 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/calc_favourable_angles_001.sh
+++ tests/cases/calc_favourable_angles_001.sh
@@ -5,7 +5,6 @@ export PERL5LIB=$(dirname "$0")/../../lib
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-rotation-only-001.dump
 potential="hard_sphere"
 interaction_specifier="label_atom_id N,C,CA,O"
-energy_cutoff_atom=0
 residue_id="18"
 residue_chain="A"
 pdbx_model_num="1"
@@ -20,5 +19,4 @@ $(dirname "$0")/../scripts/calc_favourable_angles \
 	           ${angles} \
 	           ${potential} \
                "${interaction_specifier}" \
-	           ${energy_cutoff_atom} \
 	           ${pdbx_dump_file}
