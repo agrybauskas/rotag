1aaa394f4346af42c52b91bc69945de63cee9f5d
algirdas <algirdas@lama>
2019-04-05 12:34:57 +0000

    Added second test case for Measure::energy().
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2037 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/energy_002.sh
+++ tests/cases/energy_002.sh
@@ -2,7 +2,7 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/arginine-002.dump
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/arginine-001.dump
 potential="composite"
 
 $(dirname "$0")/../scripts/energy ${potential} ${pdbx_dump_file}
