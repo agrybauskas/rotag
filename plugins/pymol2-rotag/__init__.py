eb874ec75a0c82a8b2fbbde8b05f72e09645460a
algirdas <algirdas@lama>
2019-04-19 14:01:32 +0000

    Adding notebooks in pymol plugin.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2088 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- plugins/pymol2-rotag/__init__.py
+++ plugins/pymol2-rotag/__init__.py
@@ -3,10 +3,27 @@ def __init_plugin__( app ):
                              command=lambda: rotag_tk_dialog( app.root ) )
 
 def rotag_tk_dialog( parent ):
-    try:
-        import tkMessageBox  # Python 2
-    except ImportError:
-        import tkinter.messagebox as tkMessageBox  # Python 3
+    import Tkinter as tk
+    import ttk
 
-    tkMessageBox.showinfo( parent=parent, title='rotag',
-                           message='Hello World' )
+    # Initialization.
+    root = tk.Tk()
+
+    # Main frame.
+    frame = tk.Frame( root )
+    frame.pack()
+
+    # Notebook.
+    notebook = ttk.Notebook( frame )
+    notebook.pack()
+
+    # Notebook tabs.
+    scan_tab          = ttk.Frame(notebook)
+    library_tab       = ttk.Frame(notebook)
+    configuration_tab = ttk.Frame(notebook)
+    about_tab         = ttk.Frame(notebook)
+
+    notebook.add( scan_tab, text = "Scan" )
+    notebook.add( library_tab, text = "Library" )
+    notebook.add( configuration_tab, text = "Configuration" )
+    notebook.add( about_tab, text = "About" )
