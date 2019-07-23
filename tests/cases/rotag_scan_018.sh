c26b269f6f3db6217ce149352e69d7e03d242b2a
algirdas <algirdas@lama>
2019-07-23 12:17:04 +0000

    Patching rotag_rmsd and rotag_scan test cases.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2359 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_scan_018.sh
+++ tests/cases/rotag_scan_018.sh
@@ -4,5 +4,5 @@ pdbx_file=$(dirname "$0")/../inputs/empty.cif
 
 rotag_scan \
     --parameters 'lj_k=0.0, c_k=0.0, h_k=1.0, t_k=0.0, cutoff_start=2.5, cutoff_end=5.0' \
-    --categories '_[local]_rotamer_angle' \
+    --tags '_[local]_rotamer_angle' \
     ${pdbx_file} 2>&1
