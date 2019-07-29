dd333ea10d4240a4cec574970570042e3fd9dcee
algirdas <algirdas@lama>
2019-07-29 10:28:13 +0000

    Patching test cases and outputs.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2390 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/sort_atom_names_003.sh
+++ tests/cases/sort_atom_names_003.sh
@@ -4,4 +4,6 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 atom_names_file=$(dirname "$0")/../inputs/atom-names-001.dat
 
-$(dirname "$0")/../scripts/sort_atom_names ${atom_names_file} 'n' 2>&1
+$(dirname "$0")/../scripts/sort_atom_names ${atom_names_file} 'n' 2>&1 \
+    | sed 's/line\s*[0-9]*.$/line <row>./g' \
+    | sed 's/0x[0-9a-f]\{12\}/<hex>/g'
