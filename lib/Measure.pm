e108c269f8ab0eacd14f6e9feaf2f852470c7f26
algirdas <algirdas@lama>
2019-08-11 21:35:47 +0000

    Now, comparison of symmetric atoms is correctly done - atom names correspond to correct rmsd.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2425 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Measure.pm
+++ lib/Measure.pm
@@ -631,32 +631,38 @@ sub rmsd_sidechains
                 my $symmetrical_atom_names =
                     $symmetrical_atom_names->{$residue_name}
                                              {$first_sidechain_data->[$i][2]};
-                my $rmsd;
                 # TODO: make sure that there are no situations were best case
                 # rmsd does not produce higher avarage rmsd.
                 if( defined $symmetrical_atom_names ) {
+                    my $rmsd;
+                    my $symmetric_atom_data;
                     my @second_atom_names = ( $first_sidechain_data->[$i][2],
                                               @{ $symmetrical_atom_names } );
                     for my $second_atom_name ( @second_atom_names ) {
-                        my $symmetric_atom_data =
+                        my $current_symmetric_atom_data =
                             $second_sidechain{$second_atom_name};
                         my $current_rmsd =
                             rmsd([[(@{$first_sidechain_data->[$i]})[8..10]]],
-                                 [[(@{$symmetric_atom_data})[8..10]]]);
+                                 [[(@{$current_symmetric_atom_data})[8..10]]]);
                         if( ! defined $rmsd || $current_rmsd < $rmsd ){
+                            $symmetric_atom_data = $current_symmetric_atom_data;
                             $rmsd = $current_rmsd;
                         }
                     }
-                } else {
-                    $rmsd = rmsd([[(@{$first_sidechain_data->[$i]})[8..10]]],
-                                 [[(@{$second_sidechain_data->[$i]})[8..10]]]);
-                }
 
-                push @current_sidechain_data,
-                    [ (@{$first_sidechain_data->[$i]})[0..7],
-                      (@{$second_sidechain_data->[$i]})[0..7],
-                      sprintf $sig_figs_max,
-                      $rmsd ];
+                    push @current_sidechain_data,
+                        [ (@{$first_sidechain_data->[$i]})[0..7],
+                          (@{$symmetric_atom_data})[0..7],
+                          sprintf $sig_figs_max, $rmsd ];
+                } else {
+                    my $rmsd =
+                        rmsd([[(@{$first_sidechain_data->[$i]})[8..10]]],
+                             [[(@{$second_sidechain_data->[$i]})[8..10]]]);
+                    push @current_sidechain_data,
+                        [ (@{$first_sidechain_data->[$i]})[0..7],
+                          (@{$second_sidechain_data->[$i]})[0..7],
+                          sprintf $sig_figs_max, $rmsd ];
+                }
             }
 
             if( $best_case ) {
