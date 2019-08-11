eb9642d946360ea0db085dbd3d687e0ee6e4e804
algirdas <algirdas@lama>
2019-08-11 19:44:00 +0000

    Added more test inputs for rmsd_sidechains() test case.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2420 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rmsd_sidechains_001.sh
+++ tests/cases/rmsd_sidechains_001.sh
@@ -2,7 +2,7 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file_1=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-002.dump
-pdbx_dump_file_2=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-003.dump
+pdbx_dump_file_1=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-003.dump
+pdbx_dump_file_2=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-004.dump
 
 $(dirname "$0")/../scripts/rmsd_sidechains ${pdbx_dump_file_1} ${pdbx_dump_file_2}
