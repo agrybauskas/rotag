415d53abe0dedeb863b60f576aa0b020f2006d7f
algirdas <algirdas@lama>
2019-11-14 13:47:20 +0000

    Added Graph and GraphViz libraries for having easier development of the side-chain prediction algorithms.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/side-chain-prediction@2471 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/PredictSidechains.pm
+++ lib/PredictSidechains.pm
@@ -1,14 +1,40 @@
-package PredictSidechains
+package PredictSidechains;
 
 use strict;
 use warnings;
 
 use Exporter qw( import );
-our @EXPORT_OK = qw();
+our @EXPORT_OK = qw( sidechain_positions );
+
+use Graph;
+use GraphViz;
+
+use PDBxParser qw( filter_new );
+use Grid qw( grid_box
+             identify_neighbour_cells );
 
 use Version qw( $VERSION );
 
 our $VERSION = $VERSION;
+
+sub sidechain_positions
+{
+    my ( $args ) = @_;
+    my ( $parameters, $atom_site ) =
+        ( $args->{'parameters'}, $args->{'atom_site'} );
+
+    my $edge_length_interaction =
+        $parameters->{'_[local]_constants'}{'edge_length_interaction'};
+
+    # Chooses only CA atoms, because from them, boundary interaction conditions
+    # are measured.
+    my $atom_site_cas = filter_new( $atom_site,
+                                    { 'include' =>
+                                          { 'label_atom_id' => [ 'CA' ] } } );
+    my ( $grid_box_cas ) =
+        grid_box( $parameters, $atom_site_cas, $edge_length_interaction );
+
+}
 
 # ----------------------------------------------------------------------------- #
 
