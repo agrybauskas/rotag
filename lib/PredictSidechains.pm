eac9257024a7fea2a1a54e13a72245ddbcf487c1
algirdas <algirdas@lama>
2019-11-14 12:05:08 +0000

    Started module where all prediction functions will reside.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/side-chain-prediction@2470 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/PredictSidechains.pm
@@ -0,0 +1,15 @@
+package PredictSidechains
+
+use strict;
+use warnings;
+
+use Exporter qw( import );
+our @EXPORT_OK = qw();
+
+use Version qw( $VERSION );
+
+our $VERSION = $VERSION;
+
+# ----------------------------------------------------------------------------- #
+
+1;
