f1128b317c33f84d9e09170d0df593e4ff14086d
algirdas <algirdas@lama>
2019-11-29 10:31:19 +0000

    Continuing on parameter's grammar - currently trying to figure out what tokens there should be.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2486 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/parameter_parser_001.sh
+++ tests/cases/parameter_parser_001.sh
@@ -2,6 +2,6 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-parameter_command="resname ASP && atomname CA"
+parameter_cmd="a=[{a=5,b=6},{c=7},{d=8}],b=5"
 
-"$(dirname "$0")"/../scripts/parameter_parser "${paramter_command}"
+"$(dirname "$0")"/../scripts/parameter_parser ${parameter_cmd}
