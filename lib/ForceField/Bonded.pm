f4a3dab250cbcf8030fc86f73c88b85b1da483b9
algirdas <algirdas@lama>
2019-12-04 07:59:52 +0000

    Removed old torsion potential function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2513 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/ForceField/Bonded.pm
+++ lib/ForceField/Bonded.pm
@@ -32,81 +32,13 @@ our $VERSION = $VERSION;
 #     n         - number of energy maxima;
 #     omega     - dihedral angle.
 # Input:
+#     $parameters - parameters' values.
 #     $atom_site - atom site data structure;
 #     $atom_i_id - target atom id;
-#     $parameters - parameters' values.
+#     $options->{atom_site} - reference atom site.
 # Output:
 #     $torsion_potential - value of torsion energy potential.
 #
-
-sub torsion
-{
-    my ( $parameters, $atom_i_id, $options ) = @_;
-
-    my ( $reference_atom_site ) = ( $options->{'atom_site'} );
-
-    my $t_k //= $parameters->{'_[local]_force_field'}{'t_k'};
-
-    my $t_epsilon = 1.0;
-    my $t_n = 3; # FIXME: here the number depends on the hybridization.
-
-    # Determines all dihedral angles by searching third neighbours following the
-    # connections.
-    my @connection_ids = @{ $reference_atom_site->{$atom_i_id}{'connections'} };
-
-    my $torsion_potential = 0;
-    for my $neighbour_id ( @connection_ids ) {
-        my @second_neighbour_ids =
-            grep { $atom_i_id ne $_ }
-                @{ $reference_atom_site->{$neighbour_id}{'connections'} };
-
-        next if ! @second_neighbour_ids;
-
-        my @second_atom_names =
-            @{ sort_atom_names(
-                   [ map { $reference_atom_site->{$_}{'label_atom_id'} }
-                         @second_neighbour_ids ] ) };
-        my ( $second_neighbour_id ) =
-            grep { $reference_atom_site->{$_}{'label_atom_id'} eq
-                   $second_atom_names[0] }
-                 @second_neighbour_ids;
-
-        my @third_neighbour_ids =
-            grep { $neighbour_id ne $_ }
-                @{ $reference_atom_site->{$second_neighbour_id}{'connections'} };
-
-        next if ! @third_neighbour_ids;
-
-        my @third_atom_names =
-            @{ sort_atom_names(
-                   [ map { $reference_atom_site->{$_}{'label_atom_id'} }
-                         @third_neighbour_ids ] ) };
-        my ( $third_neighbour_id ) =
-            grep { $reference_atom_site->{$_}{'label_atom_id'} eq
-                   $third_atom_names[0] }
-                 @third_neighbour_ids;
-
-        my $omega = dihedral_angle(
-            [ [ $reference_atom_site->{$third_neighbour_id}{'Cartn_x'},
-                $reference_atom_site->{$third_neighbour_id}{'Cartn_y'},
-                $reference_atom_site->{$third_neighbour_id}{'Cartn_z'} ],
-              [ $reference_atom_site->{$second_neighbour_id}{'Cartn_x'},
-                $reference_atom_site->{$second_neighbour_id}{'Cartn_y'},
-                $reference_atom_site->{$second_neighbour_id}{'Cartn_z'} ],
-              [ $reference_atom_site->{$neighbour_id}{'Cartn_x'},
-                $reference_atom_site->{$neighbour_id}{'Cartn_y'},
-                $reference_atom_site->{$neighbour_id}{'Cartn_z'} ],
-              [ $reference_atom_site->{$atom_i_id}{'Cartn_x'},
-                $reference_atom_site->{$atom_i_id}{'Cartn_y'},
-                $reference_atom_site->{$atom_i_id}{'Cartn_z'} ] ],
-        );
-
-        $torsion_potential +=
-            $t_k * ( $t_epsilon / 2 ) * ( 1 + cos( $t_n * $omega ) );
-    }
-
-    return $torsion_potential;
-}
 
 sub torsion_new
 {
