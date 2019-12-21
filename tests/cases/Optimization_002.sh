4bd9d7a38ecb7915c340552d51a6cae599b3b4d9
algirdas <algirdas@lama>
2019-12-21 20:20:20 +0000

    Decided to revert back the list to hash in Optimization object.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2543 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/Optimization_002.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use ForceField::Optimization;
+
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 1;
+
+my $optimization = Optimization->new();
+$optimization->parameters();
+
+print Dumper $optimization;
