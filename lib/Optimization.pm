4fe2186770bfe66ca621293f53e39e2283b8d1d3
algirdas <algirdas@lama>
2020-01-02 19:45:03 +0000

    Started Optimization module that will have particle swarm optimization algorithm.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2597 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/Optimization.pm
@@ -0,0 +1,17 @@
+package Optimization;
+
+use strict;
+use warnings;
+
+# ------------------------- Constructors/Destructors -------------------------- #
+
+sub new
+{
+    my ( $class, $args ) = @_;
+    my $self = {};
+    return bless $self, $class;
+}
+
+# ----------------------------- Setters/Getters ------------------------------- #
+
+1;
