beff9a1ddbc0cc30962f5a6aa7a7d05578478055
algirdas <algirdas@lama>
2020-01-02 20:48:51 +0000

    Wrote Optimization.pm constructor.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2598 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Optimization.pm
+++ lib/Optimization.pm
@@ -3,15 +3,36 @@ package Optimization;
 use strict;
 use warnings;
 
+use Optimization::Parameters;
+
 # ------------------------- Constructors/Destructors -------------------------- #
 
 sub new
 {
-    my ( $class, $args ) = @_;
-    my $self = {};
+    my ( $class, $particles ) = @_;
+    my $self = {
+        'particles' => undef,
+        'min_value' => undef
+    };
+
+    for my $name ( keys %{ $particles } ) {
+        my $particle = Parameters->new( {
+            'key' => $name,
+            'min_range' => $particles->{$name}{'min_range'},
+            'max_range' => $particles->{$name}{'max_range'}
+        } );
+        $self->{'particles'} = $particle;
+    }
+
     return bless $self, $class;
 }
 
-# ----------------------------- Setters/Getters ------------------------------- #
+# ------------------------- Optimization algorithms --------------------------- #
+
+sub particle_swarm
+{
+    my ( $particles ) = @_;
+}
+
 
 1;
