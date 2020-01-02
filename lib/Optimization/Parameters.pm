5ad14ce3fe28033e5b3ed20cd470d39961bd9316
algirdas <algirdas@lama>
2020-01-02 18:48:07 +0000

    Started creating struct for optimizing parameters.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2595 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ lib/Optimization/Parameters.pm
@@ -0,0 +1,19 @@
+package Parameters;
+
+use strict;
+use warnings;
+
+# ------------------------- Constructors/Destructors -------------------------- #
+
+sub new
+{
+    my ( $class, $args ) = @_;
+    my $self = {};
+    return bless $self, $class;
+}
+
+# ----------------------------- Member functions ------------------------------ #
+
+# ----------------------------- Setters/Getters ------------------------------- #
+
+1;
