0687aa60fffac16f3b8fcdf6f7832c6f00f6396c
algirdas <algirdas@lama>
2020-01-02 21:41:52 +0000

    Values are being generated randomly.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2602 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Optimization.pm
+++ lib/Optimization.pm
@@ -24,7 +24,7 @@ sub new
             'min_range' => $particles->{$name}{'min_range'},
             'max_range' => $particles->{$name}{'max_range'}
         } );
-        $self->{'particles'} = $particle;
+        $self->{'particles'}{$name} = $particle;
     }
 
     return bless $self, $class;
@@ -43,9 +43,26 @@ sub set_cost_function
 sub particle_swarm
 {
     my ( $particles, $options ) = @_;
+    my $cost_function = $particles->{'cost_function'};
     my ( $seed ) = ( $options->{'seed'} );
-
     $seed //= 23;
+
+    srand( $seed );
+
+    if( ! defined $cost_function ) {
+        die "Cost function is missing. It has to be set.\n";
+    }
+
+    for my $key ( keys %{ $particles->{'particles'} } ) {
+        my ( $particle ) = $particles->{'particles'}{$key};
+
+        if( ! defined $particle->value ) {
+            $particle->value(
+                $particle->min_range +
+                rand( $particle->max_range - $particle->min_range )
+            );
+        }
+    }
 }
 
 1;
--- tests/cases/particle_swarm_001.sh
+++ tests/cases/particle_swarm_001.sh
@@ -12,4 +12,6 @@ my $particles =
                                         'max_range' => 2.0 },
                          'epsilon' => { 'min_range' => 1.0,
                                         'max_range' => 5.0 } } );
+$particles->set_cost_function( \&{ sub { my ( $x ) = @_; return $x^2 } } );
+
 particle_swarm( $particles );
