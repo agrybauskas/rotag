1919f53d97c9cf25b29317a711126a1d838cd2c4
algirdas <algirdas@lama>
2020-01-03 23:07:44 +0000

    Made optimization module again without constructors.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2610 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/particle_swarm_001.sh
+++ tests/cases/particle_swarm_001.sh
@@ -5,14 +5,12 @@ use warnings;
 
 use Data::Dumper;
 
+use Optimization::Particle;
 use Optimization qw( particle_swarm );
 
-# $Data::Dumper::Sortkeys = 1;
-# $Data::Dumper::Indent = 1;
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 1;
 
-# my $particles =
-#     Optimization->new( { 'x'   => { 'min_range' => 1.0,
-#                                     'max_range' => 2.0 } }, 10 );
-# $particles->set_cost_function( \&{ sub { my ( $x ) = @_; return $x^2 } } );
+my $x = Particle->new( { 'x' => { 'min_range' => 5.0, 'max_range' => 10.0 } } );
 
-# particle_swarm( $particles );
+particle_swarm( [ $x ] );
