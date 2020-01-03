23923108a5444c86d08807e471c26f419b6d86f0
algirdas <algirdas@lama>
2020-01-03 16:27:03 +0000

    Bringing back constructors.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2607 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Optimization.pm
+++ lib/Optimization.pm
@@ -3,25 +3,48 @@ package Optimization;
 use strict;
 use warnings;
 
-use Exporter qw( import );
-our @EXPORT_OK = qw( particle_swarm );
-
 use Optimization::Parameter;
 
-# ------------------------- Optimization algorithms --------------------------- #
+# ------------------------- Constructors/Destructors -------------------------- #
 
-sub particle_swarm
+sub new
 {
-    my ( $particles, $options ) = @_;
-    my $cost_function = $particles->{'cost_function'};
+    my ( $class, $parameters, $particle_num, $options ) = @_;
+    my ( $seed ) = ( $options->{'seed'} );
+    $seed //= 23;
 
-    if( ! defined $cost_function ) {
-        die "Cost function is missing. It has to be set.\n";
+    my $self = {
+        'particles' => undef,
+        'cost_function' => undef,
+    };
+
+    srand( $seed );
+
+    for my $i ( 0..$particle_num-1 ) {
+        my $id = $i+1;
+        for my $name ( keys %{ $parameters } ) {
+            my $parameter = Parameters->new( {
+                'key' => $name,
+                'min_range' => $parameters->{$name}{'min_range'},
+                'max_range' => $parameters->{$name}{'max_range'},
+                'value' =>
+                    $parameters->{$name}{'min_range'} +
+                    rand( $parameters->{$name}{'max_range'} -
+                          $parameters->{$name}{'min_range'} )
+            } );
+            $self->{'particles'}{$id}{'parameters'}{$name} = $parameter;
+        }
     }
 
-    for my $id ( keys %{ $particles->{'particles'} } ) {
-        my $particle = $particles->{'particles'}{$id};
-    }
+    return bless $self, $class;
+}
+
+# ----------------------------- Setters/Getters ------------------------------- #
+
+sub set_cost_function
+{
+    my ( $self, $cost_function ) = @_;
+    $self->{'cost_function'} = $cost_function;
 }
 
 1;
