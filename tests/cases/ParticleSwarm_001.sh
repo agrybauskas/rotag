4e0d44c044970217301aa0d990574c8bd2a9746f
algirdas <algirdas@lama>
2020-01-06 13:30:20 +0000

    Now, best parameter is chosen for cost function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2622 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/ParticleSwarm_001.sh
+++ tests/cases/ParticleSwarm_001.sh
@@ -11,7 +11,7 @@ $Data::Dumper::Sortkeys = 1;
 $Data::Dumper::Indent = 1;
 
 my $particle_swarm =
-    ParticleSwarm->new( { 'x' => { 'min' => 4.0, 'max' => 6.0 } }, 10 );
+    ParticleSwarm->new( { 'x' => { 'min' => -4.0, 'max' => 4.0 } }, 10 );
 
 $particle_swarm->set_cost_function( \&{sub { my ( $x ) = @_; return $x**2 }} );
 $particle_swarm->optimize( 10 );
