e56b7f793396d0a6a1e36e7a927a9825821b17bc
algirdas <algirdas@lama>
2020-02-28 08:25:21 +0000

    Decided to use OOP for side-chain prediction, because the states have to be clearly tracked and graph package is also OOP.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/side-chain-prediction@2710 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/PredictSidechains.pm
+++ lib/PredictSidechains.pm
@@ -3,9 +3,6 @@ package PredictSidechains;
 use strict;
 use warnings;
 
-use Exporter qw( import );
-our @EXPORT_OK = qw( sidechain_positions );
-
 use Graph;
 use GraphViz;
 
@@ -18,63 +15,80 @@ use Version qw( $VERSION );
 
 our $VERSION = $VERSION;
 
-sub sidechain_positions
+# ------------------------- Constructors/Destructors -------------------------- #
+
+sub new
 {
-    my ( $args ) = @_;
-    my ( $rotamer_angles, $rotamer_energies, $parameters, $atom_site ) = (
-        $args->{'rotamer_angles'}, $args->{'rotamer_energies'},
-        $args->{'parameters'}, $args->{'atom_site'}
-    );
-
-    my $edge_length_interaction =
-        $parameters->{'_[local]_constants'}{'edge_length_interaction'};
-
-    # Chooses only CA atoms, because from them, boundary interaction conditions
-    # are measured.
-    my $atom_site_cas = filter_new( $atom_site,
-                                    { 'include' =>
-                                          { 'label_atom_id' => [ 'CA' ] } } );
-
-    my ( $grid_box_cas ) = grid_box( $parameters, $atom_site_cas,
-                                     $edge_length_interaction );
-    my $neighbouring_cells = identify_neighbour_cells( $grid_box_cas );
-
-    my $interaction_graph = Graph->new();
-    # my $graph_viz = GraphViz->new(); # NOTE: only for development purposes.
-
-    for my $cell ( keys %{ $grid_box_cas } ) {
-        my $neighbour_cell_atom_ids = $neighbouring_cells->{$cell};
-        for my $atom_id ( @{ $grid_box_cas->{$cell} } ) {
-            my $unique_residue_key =
-                unique_residue_key( $atom_site_cas->{$atom_id} );
-
-            $interaction_graph->add_vertex( $unique_residue_key );
-            # $graph_viz->add_node( $unique_residue_key );
-
-            my $neighbour_atom_ids =
-                [ grep { $atom_id ne $_ } @{ $neighbour_cell_atom_ids }  ];
-
-            for my $neighbour_atom_id ( @{ $neighbour_atom_ids } ) {
-                my $neighbour_residue_key =
-                    unique_residue_key( $atom_site_cas->{$neighbour_atom_id} );
-
-                $interaction_graph->add_vertex( $unique_residue_key );
-                # $graph_viz->add_node( $neighbour_residue_key );
-
-                $interaction_graph->add_edge( $unique_residue_key,
-                                              $neighbour_residue_key );
-                # $graph_viz->add_edge( $unique_residue_key =>
-                #                       $neighbour_residue_key );
-            }
-        }
-    }
-
-    # use Data::Dumper;
-    # print STDERR Dumper $interaction_graph;
-
-    # print $graph_viz->as_png;
+    my ( $class, $args ) = @_;
+    my $self = {
+        'rotamer_angles' => $args->{'rotamer_angles'},
+        'rotamer_energies' => $args->{'rotamer_energies'},
+        'parameters' => $args->{'parameters'}
+    };
+    return bless $self, $class;
 }
 
-# ----------------------------------------------------------------------------- #
+# ----------------------------- Setters/Getters ------------------------------- #
+
+# --------------------------------- Methods ----------------------------------- #
+
+# sub sidechain_positions
+# {
+#     my ( $args ) = @_;
+#     my ( $rotamer_angles, $rotamer_energies, $parameters, $atom_site ) = (
+#         $args->{'rotamer_angles'}, $args->{'rotamer_energies'},
+#         $args->{'parameters'}, $args->{'atom_site'}
+#     );
+
+#     my $edge_length_interaction =
+#         $parameters->{'_[local]_constants'}{'edge_length_interaction'};
+
+#     # Chooses only CA atoms, because from them, boundary interaction conditions
+#     # are measured.
+#     my $atom_site_cas = filter_new( $atom_site,
+#                                     { 'include' =>
+#                                           { 'label_atom_id' => [ 'CA' ] } } );
+
+#     my ( $grid_box_cas ) = grid_box( $parameters, $atom_site_cas,
+#                                      $edge_length_interaction );
+#     my $neighbouring_cells = identify_neighbour_cells( $grid_box_cas );
+
+#     my $interaction_graph = Graph->new();
+#     # my $graph_viz = GraphViz->new(); # NOTE: only for development purposes.
+
+#     for my $cell ( keys %{ $grid_box_cas } ) {
+#         my $neighbour_cell_atom_ids = $neighbouring_cells->{$cell};
+#         for my $atom_id ( @{ $grid_box_cas->{$cell} } ) {
+#             my $unique_residue_key =
+#                 unique_residue_key( $atom_site_cas->{$atom_id} );
+
+#             $interaction_graph->add_vertex( $unique_residue_key );
+#             # $graph_viz->add_node( $unique_residue_key );
+
+#             my $neighbour_atom_ids =
+#                 [ grep { $atom_id ne $_ } @{ $neighbour_cell_atom_ids }  ];
+
+#             for my $neighbour_atom_id ( @{ $neighbour_atom_ids } ) {
+#                 my $neighbour_residue_key =
+#                     unique_residue_key( $atom_site_cas->{$neighbour_atom_id} );
+
+#                 $interaction_graph->add_vertex( $unique_residue_key );
+#                 # $graph_viz->add_node( $neighbour_residue_key );
+
+#                 $interaction_graph->add_edge( $unique_residue_key,
+#                                               $neighbour_residue_key );
+#                 # $graph_viz->add_edge( $unique_residue_key =>
+#                 #                       $neighbour_residue_key );
+#             }
+#         }
+#     }
+
+#     use Data::Dumper;
+#     print STDERR Dumper $interaction_graph;
+
+#     # print $graph_viz->as_png;
+# }
+
+# # ----------------------------------------------------------------------------- #
 
 1;
