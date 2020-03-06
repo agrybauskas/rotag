2b2c917beb1b95d9df37147a3924c651edc4afef
algirdas <algirdas@lama>
2020-03-06 14:18:17 +0000

    Decided to use different tab collection in pymol plugin.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2745 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- plugins/pymol2-rotag/__init__.py
+++ plugins/pymol2-rotag/__init__.py
@@ -1,8 +1,8 @@
-def __init_plugin__( app ):
-    app.menuBar.addmenuitem( 'Plugin', 'command', label='rotag',
-                             command=lambda: rotag_tk_dialog( app.root ) )
+def __init_plugin__(app):
+    app.menuBar.addmenuitem('Plugin', 'command', label='rotag',
+                            command=lambda: rotag_tk_dialog(app.root))
 
-def rotag_tk_dialog( parent ): # TODO: find out how to use parent object.
+def rotag_tk_dialog(parent): # TODO: find out how to use parent object.
     import Tkinter as tk
     import ttk
     import os
@@ -13,21 +13,29 @@ def rotag_tk_dialog( parent ): # TODO: find out how to use parent object.
     # Initialization.
     root = tk.Tk()
     root.title("rotag")
-    # root.iconbitmap( os.path.dirname( __file__ ) + "/icon.ico" )
+    # root.iconbitmap(os.path.dirname(__file__) + "/icon.ico")
 
     # Canvas.
-    canvas = tk.Canvas( root, height=HEIGHT, width=WIDTH )
+    canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
     canvas.pack()
 
     # Notebook.
-    notebook = ttk.Notebook( root )
-    notebook.place( anchor='nw' )
+    notebook = ttk.Notebook(root)
+    notebook.place(anchor='nw')
 
     # Notebook tabs.
-    scan_tab          = ttk.Frame(notebook)
-    library_tab       = ttk.Frame(notebook)
-    about_tab         = ttk.Frame(notebook)
+    main_tab  = ttk.Frame(notebook)
+    about_tab = ttk.Frame(notebook)
 
-    notebook.add( scan_tab, text = "Scan" )
-    notebook.add( library_tab, text = "Library" )
-    notebook.add( about_tab, text = "About" )
+    # About.
+    about_license = ttk.LabelFrame(about_tab, text="License")
+
+    license_text = tk.Text(about_license)
+    license_text.insert(tk.INSERT,
+                        "GNU GENERAL PUBLIC LICENSE, Version 2, June 1991.",
+                        tk.END)
+
+    about_license.pack()
+
+    notebook.add(main_tab, text="Main")
+    notebook.add(about_tab, text="About")
