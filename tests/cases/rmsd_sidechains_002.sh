2d420d676cc14030332b64c4b6557e8d5347b8b8
algirdas <algirdas@lama>
2020-03-27 14:06:35 +0000

    Adding test case for rmsd_sidechains() with 'best_case' option on.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@2788 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/rmsd_sidechains_002.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file_1=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-003.dump
+pdbx_dump_file_2=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-004.dump
+is_best_case=1
+
+$(dirname "$0")/../scripts/rmsd_sidechains ${pdbx_dump_file_1} ${pdbx_dump_file_2} ${is_best_case}
