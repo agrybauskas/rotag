887c05dda8f66895e90b3346c6fe8a09c72dd5f6
algirdas <algirdas@lama>
2020-12-26 19:58:58 +0000

    Added variable names in the comments in order to try retrieve them first.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@2897 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/CPP/PseudoAtoms.cpp
+++ lib/CPP/PseudoAtoms.cpp
@@ -9,15 +9,24 @@
 #include <math.h>
 #include <string>
 #include <vector>
-/* --------------------- Contructors and/or destructors ---------------------- */
 
 void generate_library(SV* options_ptr)
 {
+    double edge_length_interation;
+    // interaction_atom_names;
+    // cutoff_atom;
+
+    // conf_model
+    // threads
+    // include_interactions
+
+    // potential_functions
+
+    // rotamer_library
+
+    // atom_site_groups
+
     // HV * options = (HV*) SvRV( options_ptr );
     // SV** value = hv_fetch(hash, "a", 1, 0);
     // std::cout << SvPV_nolen(*value) << std::endl;
 }
-
-/* -------------------------------- Methods ---------------------------------- */
-
-/* -------------------------- Setters and Getters ---------------------------- */
