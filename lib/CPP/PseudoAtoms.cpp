4db9faed2081a1340181d20eb098643673a211a8
algirdas <algirdas@lama>
2020-12-26 23:20:55 +0000

    Retrieving all arguments to generate_library() in CPP::PseudoAtoms.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@2898 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/CPP/PseudoAtoms.cpp
+++ lib/CPP/PseudoAtoms.cpp
@@ -12,7 +12,25 @@
 
 void generate_library(SV* options_ptr)
 {
-    double edge_length_interation;
+    HV* options = (HV*) SvRV(options_ptr);
+
+    HV* parameters = (HV*) SvRV((SV*) hv_fetch(options, "parameters", 10, 0));
+    HV* atom_site = (HV*) SvRV((SV*) hv_fetch(options, "atom_site", 9, 0));
+    HV* residue_unique_keys =
+        (HV*) SvRV((SV*) hv_fetch(options, "residue_unique_keys", 19, 0));
+    HV* include_interactions =
+        (HV*) SvRV((SV*) hv_fetch(options, "include_interactions", 20, 0));
+    HV* angles = (HV*) SvRV((SV*) hv_fetch(options, "angles", 6, 0));
+    // NOTE: True and false comes from key existance.
+    bool rmsd = hv_exists(options, "rmsd", 4);
+    HV* interactions = (HV*) SvRV((SV*) hv_fetch(options, "interactions", 12, 0));
+    // TODO: For now, threads are not included, but should be in the future.
+    HV* suboptions = (HV*) SvRV((SV*) hv_fetch(options, "options", 7, 0));
+
+    // HV* rmsd = (HV*) SvRV((SV*) hv_fetch(options, "rmsd", 4, 0));
+    // std::cout << (bool) SvRV((SV*) hv_fetch(options, "rmsd", 4, 0))  << std::endl;
+    // std::cout << SvNV(*edge_length_interation) << std::endl;
+
     // interaction_atom_names;
     // cutoff_atom;
 
