80ba10dcd8cc22964edcd81a0c82f3c076181dad
algirdas <algirdas@lama>
2020-12-27 21:50:11 +0000

    Almost all C++ version generate_library() is passed through function.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/adding-cpp-functions@2899 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/CPP/PseudoAtoms.cpp
+++ lib/CPP/PseudoAtoms.cpp
@@ -14,7 +14,6 @@ void generate_library(SV* options_ptr)
 {
     HV* options = (HV*) SvRV(options_ptr);
 
-    HV* parameters = (HV*) SvRV((SV*) hv_fetch(options, "parameters", 10, 0));
     HV* atom_site = (HV*) SvRV((SV*) hv_fetch(options, "atom_site", 9, 0));
     HV* residue_unique_keys =
         (HV*) SvRV((SV*) hv_fetch(options, "residue_unique_keys", 19, 0));
@@ -22,20 +21,19 @@ void generate_library(SV* options_ptr)
         (HV*) SvRV((SV*) hv_fetch(options, "include_interactions", 20, 0));
     HV* angles = (HV*) SvRV((SV*) hv_fetch(options, "angles", 6, 0));
     // NOTE: True and false comes from key existance.
-    bool rmsd = hv_exists(options, "rmsd", 4);
-    HV* interactions = (HV*) SvRV((SV*) hv_fetch(options, "interactions", 12, 0));
+    bool rmsd =
+        hv_exists(options,"rmsd",4) && SvUV(*hv_fetch(options, "rmsd",4,0)) != 0;
+    HV* interactions = (HV*) SvRV((SV*) hv_fetch(options, "interactions", 12,0));
+    AV* interaction_atom_names =
+        (AV*) SvRV((SV*) hv_fetch(options, "interaction_atom_names", 22,0));
+    double edge_length_interaction =
+        SvNV(*hv_fetch(options, "edge_length_interaction", 23,0));
     // TODO: For now, threads are not included, but should be in the future.
     HV* suboptions = (HV*) SvRV((SV*) hv_fetch(options, "options", 7, 0));
 
-    // HV* rmsd = (HV*) SvRV((SV*) hv_fetch(options, "rmsd", 4, 0));
-    // std::cout << (bool) SvRV((SV*) hv_fetch(options, "rmsd", 4, 0))  << std::endl;
-    // std::cout << SvNV(*edge_length_interation) << std::endl;
-
-    // interaction_atom_names;
-    // cutoff_atom;
-
-    // conf_model
-    // threads
+    // HACK: the variables should be changeable in the future.
+    std::string conf_model =  "rotation_only";
+    int threads = 1;
     // include_interactions
 
     // potential_functions
