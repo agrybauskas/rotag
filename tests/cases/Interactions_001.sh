2cf60872051aa2265aac90abb3dfe7cf255ff811
algirdas <algirdas@lama>
2021-03-24 06:21:45 +0000

    Continuing on Interaction object. Added a test case.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/optimizing-library-generation@2946 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/Interactions_001.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use Interactions;
+
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 1;
+
+my $interaction = Interactions->new();
