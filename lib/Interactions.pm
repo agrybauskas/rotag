febe996570b27f2290bf395d006bdfbf6262160c
algirdas <algirdas@lama>
2021-03-25 20:52:35 +0000

    Added a description about the future obj.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/optimizing-library-generation@2948 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/Interactions.pm
+++ lib/Interactions.pm
@@ -3,12 +3,46 @@ package Interactions;
 use strict;
 use warnings;
 
+use Digest::MD5;
+
+# # Example.
+# $ctx = Digest::MD5->new;
+# $ctx->add($data);
+# $ctx->addfile($file_handle);
+# $digest = $ctx->digest;
+# $digest = $ctx->hexdigest;
+# $digest = $ctx->b64digest;
+
 # ------------------------- Constructors/Destructors -------------------------- #
+
+#
+# Tracks overall interactions: their values, what is and not calculated, atom
+# list and etc. The overall structure is in this form:
+#
+# { interactions => {
+#     md5hex1 => {
+#       'atom_ids' => [ atom1_id, atom2_id, ... ],
+#       'unique_residue_ids' => [ unique_residue1_id, unique_residue2_id, ... ],
+#     },
+#     md5hex2 => {
+#       ...
+#     },
+#     ...
+#   }
+# }
+#
+#
+#
+# Input:
+#     ... - ...
+# Output:
+#     ... - ...
+#
 
 sub new
 {
     my ( $class, $args ) = @_;
-    my $self = {};
+    my $self = { 'interactions' => {} };
     return bless $self, $class;
 }
 
