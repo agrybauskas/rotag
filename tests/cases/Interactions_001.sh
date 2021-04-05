8ebfb17eee53b4627b2473e3ea33c0b121064c6a
algirdas <algirdas@lama>
2021-04-05 20:07:51 +0000

    Correctly passed PDBx file to the Interactions.pm test case.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/optimizing-library-generation@2952 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/Interactions_001.sh
+++ tests/cases/Interactions_001.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/perl
+#!/bin/bash
 
+export PDBx_file=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-001.cif
+
+perl <<'END'
 use strict;
 use warnings;
 
@@ -12,11 +15,11 @@ use Interactions;
 $Data::Dumper::Sortkeys = 1;
 $Data::Dumper::Indent = 1;
 
-my $pdbx_data = obtain_pdbx_data( 'inputs/amino-acids/aspartic-acid-001.cif',
-                                  [ '_atom_site' ] );
+my $pdbx_data = obtain_pdbx_data( "$ENV{PDBx_file}", [ '_atom_site' ] );
 raw2indexed( $pdbx_data,
              { 'attributes' => { '_atom_site' => [ 'id' ] } } );
 my $atom_site = $pdbx_data->{'_atom_site'}{'data'};
 
 my $interactions = Interactions->new;
 $interactions->add( $pdbx_data );
+END
