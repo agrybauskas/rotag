ebcf5119cde02f97aad92acdd0c11c5e7bf92f69
algirdas <algirdas@lama>
2021-09-17 12:52:46 +0000

    branches/rotation-translation-model For now, keeping same code for rotation_translation() as in rotation_only().
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/rotation-translation-model@2978 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/SidechainModels.pm
+++ lib/SidechainModels.pm
@@ -137,82 +137,82 @@ sub rotation_translation
     my @residue_unique_keys =
         @{ determine_residue_keys( $atom_site, { 'exclude_dot' => 1 } ) };
 
-    # # Iterates through target residues and their atom ids and assigns
-    # # conformational equations which can produce pseudo-atoms later.
-    # for my $residue_unique_key ( @residue_unique_keys ) {
-    #     my $residue_site =
-    #         filter_by_unique_residue_key( \%atom_site, $residue_unique_key, 1 );
+    # Iterates through target residues and their atom ids and assigns
+    # conformational equations which can produce pseudo-atoms later.
+    for my $residue_unique_key ( @residue_unique_keys ) {
+        my $residue_site =
+            filter_by_unique_residue_key( \%atom_site, $residue_unique_key, 1 );
 
-    #     my $rotatable_bonds = rotatable_bonds( $residue_site );
+        my $rotatable_bonds = rotatable_bonds( $residue_site );
 
-    #     if( ! %{ $rotatable_bonds } ) { next; }
+        if( ! %{ $rotatable_bonds } ) { next; }
 
-    #     for my $atom_id ( keys %{ $residue_site }  ) {
-    #         my @atom_coord = ( $atom_site{"$atom_id"}{'Cartn_x'},
-    #                            $atom_site{"$atom_id"}{'Cartn_y'},
-    #                            $atom_site{"$atom_id"}{'Cartn_z'}, );
+        for my $atom_id ( keys %{ $residue_site }  ) {
+            my @atom_coord = ( $atom_site{"$atom_id"}{'Cartn_x'},
+                               $atom_site{"$atom_id"}{'Cartn_y'},
+                               $atom_site{"$atom_id"}{'Cartn_z'}, );
 
-    #         if( ! exists $rotatable_bonds->{$atom_id} ) { next; }
+            if( ! exists $rotatable_bonds->{$atom_id} ) { next; }
 
-    #         my @transf_matrices; # Matrices for transforming atom coordinates.
+            my @transf_matrices; # Matrices for transforming atom coordinates.
 
-    #         for my $angle_name ( sort { $a cmp $b }
-    #                              keys %{ $rotatable_bonds->{$atom_id} } ) {
-    #             # First, checks if rotatable bond has fourth atom produce
-    #             # dihedral angle. It is done by looking at atom connections: if
-    #             # rotatable bond ends with terminal atom, then this bond is
-    #             # excluded.
-    #             my $up_atom_id = $rotatable_bonds->{$atom_id}{$angle_name}[1];
-    #             if( scalar( @{ $residue_site->{$up_atom_id}
-    #                                           {'connections'} } ) < 2 ){ next; }
+            for my $angle_name ( sort { $a cmp $b }
+                                 keys %{ $rotatable_bonds->{$atom_id} } ) {
+                # First, checks if rotatable bond has fourth atom produce
+                # dihedral angle. It is done by looking at atom connections: if
+                # rotatable bond ends with terminal atom, then this bond is
+                # excluded.
+                my $up_atom_id = $rotatable_bonds->{$atom_id}{$angle_name}[1];
+                if( scalar( @{ $residue_site->{$up_atom_id}
+                                              {'connections'} } ) < 2 ){ next; }
 
-    #             my $mid_atom_id = $rotatable_bonds->{$atom_id}{$angle_name}[0];
-    #             if( scalar( @{ $residue_site->{$mid_atom_id}
-    #                                           {'connections'} } ) < 2 ){ next; }
+                my $mid_atom_id = $rotatable_bonds->{$atom_id}{$angle_name}[0];
+                if( scalar( @{ $residue_site->{$mid_atom_id}
+                                              {'connections'} } ) < 2 ){ next; }
 
-    #             my @mid_connections = # Excludes up atom.
-    #                 grep { $_ ne $up_atom_id }
-    #                     @{ $residue_site->{$mid_atom_id}{'connections'} };
-    #             my @mid_connection_names = # Excludes up atom.
-    #                 map { $residue_site->{$_}{'label_atom_id'} }
-    #                     @mid_connections;
-    #             my $side_atom_name =
-    #                 sort_atom_names( \@mid_connection_names )->[0];
-    #             my $side_atom_id =
-    #                 filter_new( $residue_site,
-    #                         {  'include' =>
-    #                                { 'label_atom_id' => [ $side_atom_name ] },
-    #                            'return_data' => 'id' } )->[0];
+                my @mid_connections = # Excludes up atom.
+                    grep { $_ ne $up_atom_id }
+                        @{ $residue_site->{$mid_atom_id}{'connections'} };
+                my @mid_connection_names = # Excludes up atom.
+                    map { $residue_site->{$_}{'label_atom_id'} }
+                        @mid_connections;
+                my $side_atom_name =
+                    sort_atom_names( \@mid_connection_names )->[0];
+                my $side_atom_id =
+                    filter_new( $residue_site,
+                            {  'include' =>
+                                   { 'label_atom_id' => [ $side_atom_name ] },
+                               'return_data' => 'id' } )->[0];
 
-    #             my $mid_atom_coord =
-    #                 [ $residue_site->{$mid_atom_id}{'Cartn_x'},
-    #                   $residue_site->{$mid_atom_id}{'Cartn_y'},
-    #                   $residue_site->{$mid_atom_id}{'Cartn_z'} ];
-    #             my $up_atom_coord =
-    #                 [ $residue_site->{$up_atom_id}{'Cartn_x'},
-    #                   $residue_site->{$up_atom_id}{'Cartn_y'},
-    #                   $residue_site->{$up_atom_id}{'Cartn_z'} ];
-    #             my $side_atom_coord =
-    #                 [ $residue_site->{$side_atom_id}{'Cartn_x'},
-    #                   $residue_site->{$side_atom_id}{'Cartn_y'},
-    #                   $residue_site->{$side_atom_id}{'Cartn_z'} ];
+                my $mid_atom_coord =
+                    [ $residue_site->{$mid_atom_id}{'Cartn_x'},
+                      $residue_site->{$mid_atom_id}{'Cartn_y'},
+                      $residue_site->{$mid_atom_id}{'Cartn_z'} ];
+                my $up_atom_coord =
+                    [ $residue_site->{$up_atom_id}{'Cartn_x'},
+                      $residue_site->{$up_atom_id}{'Cartn_y'},
+                      $residue_site->{$up_atom_id}{'Cartn_z'} ];
+                my $side_atom_coord =
+                    [ $residue_site->{$side_atom_id}{'Cartn_x'},
+                      $residue_site->{$side_atom_id}{'Cartn_y'},
+                      $residue_site->{$side_atom_id}{'Cartn_z'} ];
 
-    #         # Creates and appends matrices to a list of matrices that later
-    #         # will be multiplied.
-    #         push @transf_matrices,
-    #              @{ bond_torsion( $parameters,
-    #                               $mid_atom_coord,
-    #                               $up_atom_coord,
-    #                               $side_atom_coord,
-    #                               $angle_name ) };
-    #         }
+            # Creates and appends matrices to a list of matrices that later
+            # will be multiplied.
+            push @transf_matrices,
+                 @{ bond_torsion( $parameters,
+                                  $mid_atom_coord,
+                                  $up_atom_coord,
+                                  $side_atom_coord,
+                                  $angle_name ) };
+            }
 
-    #         $atom_site->{$atom_id}{'conformation'} =
-    #             mult_matrix_product(
-    #                 [ @transf_matrices,
-    #                   @{ reshape( [ @atom_coord, 1 ], [ 4, 1 ] ) } ] );
-    #     }
-    # }
+            $atom_site->{$atom_id}{'conformation'} =
+                mult_matrix_product(
+                    [ @transf_matrices,
+                      @{ reshape( [ @atom_coord, 1 ], [ 4, 1 ] ) } ] );
+        }
+    }
 
     return;
 }
