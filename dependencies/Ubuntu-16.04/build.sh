3f2784b7dc2d72ffe7fb79535fcbea0fe865e423
algirdas <algirdas@lama>
2021-09-29 12:15:22 +0000

    trunk/ Adding missing dependencies. The problem occured when running after fresh installation.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/trunk@3000 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- dependencies/Ubuntu-16.04/build.sh
+++ dependencies/Ubuntu-16.04/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 sudo apt-get install -y \
+     libclone-perl \
+     liblist-moreutils-perl \
+     liblist-allutils-perl \
      libparse-yapp-perl \
+     libreadonly-perl \
      libsort-naturally-perl \
      libparallel-forkmanager-perl \
+     libjson-perl \
      zip
