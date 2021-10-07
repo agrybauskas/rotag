889352e130229348cd90756e913e5078c4bc9fbd
algirdas <algirdas@lama>
2021-10-07 15:10:25 +0000

    branches/rotation-translation-model Finished the rotation_translation(). However, not sure if the matrices are correctly placed. Will be tested later.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/rotation-translation-model@3005 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- lib/SidechainModels.pm
+++ lib/SidechainModels.pm
@@ -135,6 +135,10 @@ sub rotation_translation
         $options->{do_angle_bending},
     );
 
+    $do_bond_torsion //= 1;
+    $do_bond_stretching //= 1;
+    $do_angle_bending //= 1;
+
     my %atom_site = %{ $atom_site }; # Copy of $atom_site.
 
     # Determines all residue ids present in atom site.
@@ -160,59 +164,149 @@ sub rotation_translation
                                $atom_site{"$atom_id"}{'Cartn_y'},
                                $atom_site{"$atom_id"}{'Cartn_z'}, );
 
-            if( ! ( exists $bendable_angles->{$atom_id} &&
-                    exists $rotatable_bonds->{$atom_id} &&
-                    exists $stretchable_bonds->{$atom_id} ) ) { next; }
+            if( ! exists $rotatable_bonds->{$atom_id} &&
+                ! exists $stretchable_bonds->{$atom_id} &&
+                ! exists $bendable_angles->{$atom_id} ) { next; }
 
-            my @transf_matrices; # Matrices for transforming atom coordinates.
+            # Matrices for transforming atom coordinates.
+            my @bond_torsion_matrices;
+            my @bond_stretching_matrices;
+            my @angle_bending_matrices;
 
-            # Creates transformation matrices that describes the change of bond
-            # lengths.
-            for my $bond_name ( sort { $a cmp $b }
-                                keys %{ $stretchable_bonds->{$atom_id} } ) {
-                my $up_atom_id = $stretchable_bonds->{$atom_id}{$bond_name}[1];
-                my $mid_atom_id = $stretchable_bonds->{$atom_id}{$bond_name}[0];
+            if( $do_bond_torsion ) {
+                for my $angle_name ( sort { $a cmp $b }
+                                     keys %{ $rotatable_bonds->{$atom_id} } ) {
+                    # First, checks if rotatable bond has fourth atom produce
+                    # dihedral angle. It is done by looking at atom connections: if
+                    # rotatable bond ends with terminal atom, then this bond is
+                    # excluded.
+                    my $up_atom_id = $rotatable_bonds->{$atom_id}{$angle_name}[1];
+                    if( scalar( @{ $residue_site->{$up_atom_id}
+                                                  {'connections'} } ) < 2 ){ next; }
 
-                my @mid_connections = # Excludes up atom.
-                    grep { $_ ne $up_atom_id }
-                        @{ $residue_site->{$mid_atom_id}{'connections'} };
-                my @mid_connection_names = # Excludes up atom.
-                    map { $residue_site->{$_}{'label_atom_id'} }
-                        @mid_connections;
-                my $side_atom_name =
-                    sort_atom_names( \@mid_connection_names )->[0];
-                my $side_atom_id =
-                    filter_new( $residue_site,
-                            {  'include' =>
-                                   { 'label_atom_id' => [ $side_atom_name ] },
-                               'return_data' => 'id' } )->[0];
+                    my $mid_atom_id = $rotatable_bonds->{$atom_id}{$angle_name}[0];
+                    if( scalar( @{ $residue_site->{$mid_atom_id}
+                                                  {'connections'} } ) < 2 ){ next; }
 
-                my $mid_atom_coord =
-                    [ $residue_site->{$mid_atom_id}{'Cartn_x'},
-                      $residue_site->{$mid_atom_id}{'Cartn_y'},
-                      $residue_site->{$mid_atom_id}{'Cartn_z'} ];
-                my $up_atom_coord =
-                    [ $residue_site->{$up_atom_id}{'Cartn_x'},
-                      $residue_site->{$up_atom_id}{'Cartn_y'},
-                      $residue_site->{$up_atom_id}{'Cartn_z'} ];
-                my $side_atom_coord =
-                    [ $residue_site->{$side_atom_id}{'Cartn_x'},
-                      $residue_site->{$side_atom_id}{'Cartn_y'},
-                      $residue_site->{$side_atom_id}{'Cartn_z'} ];
+                    my @mid_connections = # Excludes up atom.
+                        grep { $_ ne $up_atom_id }
+                            @{ $residue_site->{$mid_atom_id}{'connections'} };
+                    my @mid_connection_names = # Excludes up atom.
+                        map { $residue_site->{$_}{'label_atom_id'} }
+                            @mid_connections;
+                    my $side_atom_name =
+                        sort_atom_names( \@mid_connection_names )->[0];
+                    my $side_atom_id =
+                        filter_new( $residue_site,
+                                {  'include' =>
+                                       { 'label_atom_id' => [ $side_atom_name ] },
+                                   'return_data' => 'id' } )->[0];
+
+                    my $mid_atom_coord =
+                        [ $residue_site->{$mid_atom_id}{'Cartn_x'},
+                          $residue_site->{$mid_atom_id}{'Cartn_y'},
+                          $residue_site->{$mid_atom_id}{'Cartn_z'} ];
+                    my $up_atom_coord =
+                        [ $residue_site->{$up_atom_id}{'Cartn_x'},
+                          $residue_site->{$up_atom_id}{'Cartn_y'},
+                          $residue_site->{$up_atom_id}{'Cartn_z'} ];
+                    my $side_atom_coord =
+                        [ $residue_site->{$side_atom_id}{'Cartn_x'},
+                          $residue_site->{$side_atom_id}{'Cartn_y'},
+                          $residue_site->{$side_atom_id}{'Cartn_z'} ];
 
                 # Creates and appends matrices to a list of matrices that later
                 # will be multiplied.
-                push @transf_matrices,
-                    @{ bond_stretching( $parameters,
-                                        $mid_atom_coord,
-                                        $up_atom_coord,
-                                        $side_atom_coord,
-                                        $bond_name ) };
+                push @bond_torsion_matrices,
+                     @{ bond_torsion( $parameters,
+                                      $mid_atom_coord,
+                                      $up_atom_coord,
+                                      $side_atom_coord,
+                                      $angle_name ) };
+                }
+            }
+
+            if( $do_bond_stretching ) {
+                for my $bond_name ( sort { $a cmp $b }
+                                     keys %{ $stretchable_bonds->{$atom_id} } ) {
+                    my $up_atom_id = $stretchable_bonds->{$atom_id}{$bond_name}[1];
+                    my $mid_atom_id = $stretchable_bonds->{$atom_id}{$bond_name}[0];
+
+                    my @mid_connections = # Excludes up atom.
+                        grep { $_ ne $up_atom_id }
+                            @{ $residue_site->{$mid_atom_id}{'connections'} };
+                    my @mid_connection_names = # Excludes up atom.
+                        map { $residue_site->{$_}{'label_atom_id'} }
+                            @mid_connections;
+                    my $side_atom_name =
+                        sort_atom_names( \@mid_connection_names )->[0];
+                    my $side_atom_id =
+                        filter_new( $residue_site,
+                                {  'include' =>
+                                       { 'label_atom_id' => [ $side_atom_name ] },
+                                   'return_data' => 'id' } )->[0];
+
+                    my $mid_atom_coord =
+                        [ $residue_site->{$mid_atom_id}{'Cartn_x'},
+                          $residue_site->{$mid_atom_id}{'Cartn_y'},
+                          $residue_site->{$mid_atom_id}{'Cartn_z'} ];
+                    my $up_atom_coord =
+                        [ $residue_site->{$up_atom_id}{'Cartn_x'},
+                          $residue_site->{$up_atom_id}{'Cartn_y'},
+                          $residue_site->{$up_atom_id}{'Cartn_z'} ];
+                    my $side_atom_coord =
+                        [ $residue_site->{$side_atom_id}{'Cartn_x'},
+                          $residue_site->{$side_atom_id}{'Cartn_y'},
+                          $residue_site->{$side_atom_id}{'Cartn_z'} ];
+
+                # Creates and appends matrices to a list of matrices that later
+                # will be multiplied.
+                push @bond_stretching_matrices,
+                     @{ bond_stretching( $parameters,
+                                         $mid_atom_coord,
+                                         $up_atom_coord,
+                                         $side_atom_coord,
+                                         $bond_name ) };
+                }
+            }
+
+            if( $do_angle_bending ) {
+                for my $angle_name ( sort { $a cmp $b }
+                                     keys %{ $bendable_angles->{$atom_id} } ) {
+                    my $up_atom_id = $bendable_angles->{$atom_id}{$angle_name}[2];
+                    my $mid_atom_id = $bendable_angles->{$atom_id}{$angle_name}[1];
+                    my $side_atom_id = $bendable_angles->{$atom_id}{$angle_name}[0];
+
+                    my $mid_atom_coord =
+                        [ $residue_site->{$mid_atom_id}{'Cartn_x'},
+                          $residue_site->{$mid_atom_id}{'Cartn_y'},
+                          $residue_site->{$mid_atom_id}{'Cartn_z'} ];
+                    my $up_atom_coord =
+                        [ $residue_site->{$up_atom_id}{'Cartn_x'},
+                          $residue_site->{$up_atom_id}{'Cartn_y'},
+                          $residue_site->{$up_atom_id}{'Cartn_z'} ];
+                    my $side_atom_coord =
+                        [ $residue_site->{$side_atom_id}{'Cartn_x'},
+                          $residue_site->{$side_atom_id}{'Cartn_y'},
+                          $residue_site->{$side_atom_id}{'Cartn_z'} ];
+
+                # Creates and appends matrices to a list of matrices that later
+                # will be multiplied.
+                push @angle_bending_matrices,
+                     @{ angle_bending( $parameters,
+                                       $mid_atom_coord,
+                                       $up_atom_coord,
+                                       $side_atom_coord,
+                                       $angle_name,
+                                       'eta' ) }; # 'eta' should equal to 0.
+                }
             }
 
             $atom_site->{$atom_id}{'conformation'} =
                 mult_matrix_product(
-                    [ @transf_matrices,
+                    [ @angle_bending_matrices,
+                      @bond_stretching_matrices,
+                      @bond_torsion_matrices,
                       @{ reshape( [ @atom_coord, 1 ], [ 4, 1 ] ) } ] );
         }
     }
