c63840b754c506ff918517a21cda69ed8c5bde54
algirdas <algirdas@lama>
2021-10-13 11:08:57 +0000

    branches/rotation-translation-model The output of changing two angle bending parameters makes sense.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/rotation-translation-model@3016 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/generate_pseudo_hetatom_003.sh
+++ tests/cases/generate_pseudo_hetatom_003.sh
@@ -4,8 +4,8 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-angle-bending-only-001.dump
 atom_id=152
-angle_and_length_ranges="theta1 -pi,pi & theta2 -pi,pi & eta -1*pi,1*pi"
-num_of_angles_and_lengths="theta1 20 & theta2 20 & eta 1"
+angle_and_length_ranges="theta1 -pi,pi & theta2 -pi,pi"
+num_of_angles_and_lengths="theta1 20 & theta2 20"
 no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
