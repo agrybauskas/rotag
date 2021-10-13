c67fc48b975c08357e057d67a281e7956fbac048
algirdas <algirdas@lama>
2021-10-13 10:37:22 +0000

    branches/rotation-translation-model Started to get meaningful pseudo atom positions from angle bending transformations.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/rotation-translation-model@3015 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/generate_pseudo_hetatom_002.sh
+++ tests/cases/generate_pseudo_hetatom_002.sh
@@ -4,10 +4,12 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-bond-stretching-only-001.dump
 atom_id=152
-angle_and_length_ranges="r1 -0.1,0.1 & r2 -0.1,0.1"
-num_of_angles_and_lengths="r1 2 & r2 2"
+angle_and_length_ranges="r1 -10,10 & r2 -10,10"
+num_of_angles_and_lengths="r1 30 & r2 30"
+no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
-                                                   ${pdbx_dump_file}
+                                                   ${pdbx_dump_file} \
+                                                   ${no_full_range}
--- tests/cases/generate_pseudo_hetatom_003.sh
+++ tests/cases/generate_pseudo_hetatom_003.sh
@@ -4,10 +4,12 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-angle-bending-only-001.dump
 atom_id=152
-angle_and_length_ranges="theta1 -0.1*pi,0.1*pi & theta2 -0.1*pi,0.1*pi"
-num_of_angles_and_lengths="theta1 2 & theta2 2"
+angle_and_length_ranges="theta1 -pi,pi & theta2 -pi,pi & eta -1*pi,1*pi"
+num_of_angles_and_lengths="theta1 20 & theta2 20 & eta 1"
+no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
-                                                   ${pdbx_dump_file}
+                                                   ${pdbx_dump_file} \
+                                                   ${no_full_range}
