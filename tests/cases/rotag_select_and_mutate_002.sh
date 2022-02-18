68a1b81373285aa0bb7d3d2cf1bf17fd2096aa77
algirdas <algirdas@lama>
2022-02-18 23:26:17 +0000

    branches/feature/algirdas-amino-acid-mutations Selected (not target) atoms are included in the output of rotag_mutate script.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-amino-acid-mutations@3086 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_select_and_mutate_002.sh
+++ tests/cases/rotag_select_and_mutate_002.sh
@@ -2,5 +2,5 @@
 
 pdbx_file=$(dirname "$0")/../inputs/5xjp.cif
 
-rotag_select -t '1:resid 20,21' -s 'byres(target around 5)' ${pdbx_file}  | \
+rotag_select -t '1:resid 20,21 && chain A' -s 'byres(target around 5) && ! hetatoms' ${pdbx_file}  | \
     rotag_mutate -m '1:TYR'
