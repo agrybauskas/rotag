783877d451c7fe69d8010811c24da264a6c48479
algirdas <algirdas@lama>
2022-03-11 00:02:52 +0000

    branches/feature/algirdas-adding-cpp-functions Started rotag_select script in CPP by first adding getopt_long.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-adding-cpp-functions@3114 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- src/scripts/rotag_select.cpp
+++ src/scripts/rotag_select.cpp
@@ -1,3 +1,25 @@
-int main() {
+#include <getopt.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  const struct option longopts[] = {
+    {"target",         0, 0, 't'},
+    {"select",         0, 0, 's'},
+    {"tags",           0, 0     },
+    {"related-data",   0, 0, 'r'},
+    {"pdb",            0, 0, 'p'},
+    {"keep-ignored",   0, 0, 'k'},
+    {"random-seed",    0, 0, 'x'},
+    {"help",           0, 0, 'h'},
+    {"version",        0, 0, 'v'},
+  };
+
+  int index;
+  int iarg = 0;
+
+  while(iarg != -1) {
+    iarg = getopt_long(argc, argv, "s:vh", longopts, &index);
+  }
+
   return 1;
 }
