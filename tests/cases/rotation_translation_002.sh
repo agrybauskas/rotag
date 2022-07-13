e4e1b6683d1079ce18ca745933a5f5a55dc84447
algirdas <algirdas@lama>
2022-07-13 06:27:57 +0000

    branches/feature/algirdas-sidechain-extension Changed test cases and scripts so rotation_translation parts could be turned off and on.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3181 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotation_translation_002.sh
+++ tests/cases/rotation_translation_002.sh
@@ -4,5 +4,12 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-002.dump
 atom_id=1925
+do_bond_torsion=0
+do_bond_stretching=1
+do_angle_bending=0
 
-$(dirname "$0")/../scripts/rotation_translation ${atom_id} ${pdbx_dump_file}
+$(dirname "$0")/../scripts/rotation_translation ${atom_id} \
+                                                ${pdbx_dump_file} \
+                                                ${do_bond_torsion} \
+                                                ${do_bond_stretching} \
+                                                ${do_angle_bending}
