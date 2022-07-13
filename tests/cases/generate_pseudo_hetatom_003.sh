1c9a3d950bfa1fe78c42bd6199c95fb72d47f1ed
algirdas <algirdas@lama>
2022-07-13 05:59:17 +0000

    branches/feature/algirdas-sidechain-extension Trying to correctly describe conformations to hetatom tests.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3180 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/generate_pseudo_hetatom_003.sh
+++ tests/cases/generate_pseudo_hetatom_003.sh
@@ -2,10 +2,10 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-angle-bending-only-001.dump
-atom_id=152
-angle_and_length_ranges="theta1 -pi,pi & theta2 -pi,pi"
-num_of_angles_and_lengths="theta1 20 & theta2 20"
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-002.dump
+atom_id=1925
+angle_and_length_ranges="r1 -0.5,0.5"
+num_of_angles_and_lengths="r1 20"
 no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
