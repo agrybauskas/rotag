ed7d4eb320518d675cb1d54878c48758548006a7
algirdas <algirdas@lama>
2022-07-25 14:06:39 +0000

    branches/feature/algirdas-sidechain-extension Trying to add more test cases. But they produce wrong results.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3191 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/rotation_translation_003.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-002.dump
+atom_id=1925
+do_hetatoms_only=1
+do_bond_torsion=0
+do_bond_stretching=0
+do_angle_bending=1
+
+$(dirname "$0")/../scripts/rotation_translation ${atom_id} \
+                                                ${pdbx_dump_file} \
+                                                ${do_hetatoms_only} \
+                                                ${do_bond_torsion} \
+                                                ${do_bond_stretching} \
+                                                ${do_angle_bending}
