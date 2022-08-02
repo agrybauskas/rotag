1269f8207301c6c1920214ffe9965975cf4654c9
algirdas <algirdas@lama>
2022-08-02 10:19:26 +0000

    branches/feature/algirdas-sidechain-extension/ (algirdas@lama)
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3210 03e66f16-22b2-49cf-b00b-e6796c25d2fd

---
+++ tests/cases/generate_pseudo_hetatom_005.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-007.dump
+atom_id=1925
+angle_and_length_ranges="chi1 0.5*pi,1.0*pi"
+num_of_angles_and_lengths="chi1 10"
+no_full_range=1
+
+$(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
+                                                   "${angle_and_length_ranges}" \
+                                                   "${num_of_angles_and_lengths}" \
+                                                   ${pdbx_dump_file} \
+                                                   ${no_full_range}
