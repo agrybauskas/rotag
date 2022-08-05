e2b0728b5974bf5d573299d65c8230013bccd95a
algirdas <algirdas@lama>
2022-08-05 22:44:32 +0000

    branches/feature/algirdas-sidechain-extension Started a test case for rotag_dihedral with heteroatoms option.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3227 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/rotag_dihedral_022.sh
+++ tests/cases/rotag_dihedral_022.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-with-bb-001.cif
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-001.cif
 
 rotag_dihedral -H -S -M ${pdbx_file}
