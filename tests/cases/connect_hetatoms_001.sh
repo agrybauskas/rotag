84cd58ab3ff2f1f216d2ba4de1bdec3f90a15499
algirdas <algirdas@lama>
2022-08-06 10:56:24 +0000

    branches/feature/algirdas-sidechain-extension It seems the differentiation between connecting ATOM and HETATM is needed.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3231 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/connect_hetatoms_001.sh
+++ tests/cases/connect_hetatoms_001.sh
@@ -4,8 +4,7 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-001.dump
 no_connection_list=0
-assign_hetatoms=1
 
-$(dirname "$0")/../scripts/connect_atoms \
-    ${pdbx_dump_file} ${no_connection_list} ${assign_hetatoms} \
+$(dirname "$0")/../scripts/connect_hetatoms \
+    ${pdbx_dump_file} ${no_connection_list} \
     | sort -k 1 -n
