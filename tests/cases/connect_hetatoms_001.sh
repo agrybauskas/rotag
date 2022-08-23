6a7a9de6dd88bedc38fd7b8c8a5ef997abca312b
algirdas <algirdas@lama>
2022-08-23 19:56:39 +0000

    branches/feature/algirdas-sidechain-extension Trying to add connection information for test input.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3270 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/connect_hetatoms_001.sh
+++ tests/cases/connect_hetatoms_001.sh
@@ -3,8 +3,9 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-001.dump
+struct_conn_dump_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-struct-conn-001.dump
 no_connection_list=0
 
 $(dirname "$0")/../scripts/connect_hetatoms \
-    ${pdbx_dump_file} ${no_connection_list} \
+    ${pdbx_dump_file} ${struct_conn_dump_file} ${no_connection_list} \
     | sort -k 1 -n
