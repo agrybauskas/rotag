fe885c688a50c78c1620b553bca0e8fc744b46e0
algirdas <algirdas@lama>
2022-08-24 08:51:13 +0000

    branches/feature/algirdas-sidechain-extension Fixed test case that would not retrieve structure information properly.
    git-svn-id: svn+ssh://pitonas/home/xray/svn-repositories/rotag/branches/feature/algirdas-sidechain-extension@3272 03e66f16-22b2-49cf-b00b-e6796c25d2fd

--- tests/cases/connect_hetatoms_001.sh
+++ tests/cases/connect_hetatoms_001.sh
@@ -2,10 +2,9 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-001.dump
-struct_conn_dump_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-struct-conn-001.dump
+pdbx_struct_conn_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-and-struct-conn-001.dump
 no_connection_list=0
 
 $(dirname "$0")/../scripts/connect_hetatoms \
-    ${pdbx_dump_file} ${struct_conn_dump_file} ${no_connection_list} \
+    ${pdbx_struct_conn_dump_file} ${no_connection_list} \
     | sort -k 1 -n
