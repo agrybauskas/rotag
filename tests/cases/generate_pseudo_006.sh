d9d4f5671d3794d7d9c43b435f6058dee53527bb
algirdas <algirdas.grybauskas@yahoo.com>
2022-10-02 10:57:25 +0300

    Adding test cases where only one of many dihedral angle values are supplied.

---
+++ tests/cases/generate_pseudo_006.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/aspartic-acid-rotation-only-001.dump
+atom_id=1414
+angle_ranges="chi2 -1*pi,1*pi"
+num_of_angles="chi2 4"
+
+$(dirname "$0")/../scripts/generate_pseudo "${atom_id}" \
+                                           "${angle_ranges}" \
+                                           "${num_of_angles}" \
+                                           ${pdbx_dump_file}
