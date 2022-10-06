8c46ad4eda893bb5e6131817ff612f6975545ed5
agrybauskas <algirdas@lama>
2022-10-06 16:47:30 +0300

    Changing input values to study more on generating pseudo atoms for heteroatoms.

--- tests/cases/generate_pseudo_hetatom_004.sh
+++ tests/cases/generate_pseudo_hetatom_004.sh
@@ -4,8 +4,8 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-006.dump
 atom_id=1925
-angle_and_length_ranges="tau1 0.5*pi,1.0*pi & r1 0.5,1.0"
-num_of_angles_and_lengths="tau1 5 & r1 5"
+angle_and_length_ranges="tau1 0.5*pi,1.0*pi & r1 0.0,2.0"
+num_of_angles_and_lengths="tau1 4 & r1 3"
 no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
