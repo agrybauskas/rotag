1c2c8a2b1ff79dee355649fd0ecde940f67a9f65
algirdas <algirdas.grybauskas@yahoo.com>
2022-10-15 13:19:07 +0300

    Trying to refactor and prepare variables for rotation_translation model.

--- lib/SidechainModels.pm
+++ lib/SidechainModels.pm
@@ -165,53 +165,18 @@ sub rotation_translation
 
         next if ! %{ $residue_site };
 
-        my $bendable_angles = {};
-        if( $do_angle_bending ) {
-            my $next_atom_ids =
-                $include_hetatoms ?
-                [ sort @{ filter_new( $residue_site,
-                                      { 'include' => {'group_PDB' => ['HETATM']},
-                                        'return_data' => 'id' } ) } ] : undef;
-            $bendable_angles =
-                bendable_angles( $residue_site, undef, $next_atom_ids, undef,
-                                 { 'include_hetatoms' => $include_hetatoms } );
-        }
-
-        my $rotatable_bonds = {};
-        if( $do_bond_torsion ) {
-            my $start_atom_id;
-            my $next_atom_ids;
-            my $ignore_atoms;
-            if( $include_hetatoms ) {
-                $start_atom_id =
-                    filter_new( $residue_site,
-                                { 'include' =>
-                                  { 'label_atom_id' => [ 'N' ] },
-                                    'return_data' => 'id' } )->[0];
-                $next_atom_ids =
-                    filter_new( $residue_site,
-                                { 'include' =>
-                                  { 'label_atom_id' => [ 'CA' ] },
-                                    'return_data' => 'id' } );
-                $ignore_atoms =
-                    filter_new( $residue_site,
-                                { 'include' =>
-                                  { 'label_atom_id' => [ 'C', 'CB' ] },
-                                    'return_data' => 'id' } );
-            }
-
-            $rotatable_bonds =
-                rotatable_bonds( $residue_site, $start_atom_id, $next_atom_ids,
-                                 { 'include_hetatoms' => $include_hetatoms,
-                                   'ignore_atoms' => $ignore_atoms } );
-        }
-
-        my $stretchable_bonds = {};
-        if( $do_bond_stretching ) {
-            $stretchable_bonds =
-                stretchable_bonds( $residue_site, undef, undef,
-                                   { 'include_hetatoms' => $include_hetatoms } );
-        }
+        my $bendable_angles =
+            $do_angle_bending ?
+            bendable_angles( $residue_site, undef, undef, undef,
+                             { 'include_hetatoms' => $include_hetatoms } ) : {};
+        my $rotatable_bonds =
+            $do_bond_torsion ?
+            rotatable_bonds( $residue_site, undef, undef,
+                             { 'include_hetatoms' => $include_hetatoms } ) : {};
+        my $stretchable_bonds =
+            $do_bond_stretching ?
+            stretchable_bonds( $residue_site, undef, undef,
+                               { 'include_hetatoms' => $include_hetatoms } ): {};
 
         if( ! %{ $rotatable_bonds } &&
             ! %{ $stretchable_bonds } &&
