eb6f456beff8729021a61d8b8a0c1f0c591d4a28
algirdas <algirdas.grybauskas@yahoo.com>
2022-12-29 15:43:16 +0200

    Adding a test case to test the current dihedral angle naming.

---
+++ tests/cases/all_dihedral_008.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/tryptophan-001.dump
+
+$(dirname "$0")/../scripts/all_dihedral ${pdbx_dump_file}
