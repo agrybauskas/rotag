1d6f6935c1ab8c2bcc7c4e52f52c933834921a84
algirdas <algirdas.grybauskas@yahoo.com>
2023-01-14 06:47:40 +0200

    Patching test cases and inputs due to the changed dihedral angle names.

--- tests/cases/generate_pseudo_hetatom_003.sh
+++ tests/cases/generate_pseudo_hetatom_003.sh
@@ -4,8 +4,8 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-004.dump
 atom_id=1925
-angle_and_length_ranges="N-CA -1.0*pi,1.0*pi"
-num_of_angles_and_lengths="N-CA 10"
+angle_and_length_ranges="C-N-CA-MG -1.0*pi,1.0*pi"
+num_of_angles_and_lengths="C-N-CA-MG 10"
 no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
