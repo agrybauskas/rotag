6f1a9f737c70a7cca9fa7c9d94c5bcb320147432
algirdas <algirdas.grybauskas@yahoo.com>
2023-01-19 01:54:12 +0200

    Adding an additional test input and output for generate_pseudo_hetatom().

--- tests/cases/generate_pseudo_hetatom_012.sh
+++ tests/cases/generate_pseudo_hetatom_012.sh
@@ -2,10 +2,10 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-006.dump
-atom_id=151
-angle_and_length_ranges="N-CA-CB 0,pi"
-num_of_angles_and_lengths="N-CA-CB 5"
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-003.dump
+atom_id=152
+angle_and_length_ranges="chi1 0,pi"
+num_of_angles_and_lengths="chi1 5"
 no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
