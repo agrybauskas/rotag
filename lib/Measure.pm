4d39c1e184d1ff9cfaf2b27fad50f2041c9b4c0e
agrybauskas <algirdas@lama>
2023-01-20 09:31:29 +0200

    Started bond parameter addition function.

--- lib/Measure.pm
+++ lib/Measure.pm
@@ -731,6 +731,14 @@ sub add_all_bond_lengths
     return;
 }
 
+sub add_all_bond_parameters
+{
+    my ( $parameters, $atom_site, $residue_unique_key, $options ) = @_;
+    my ( $include_hetatoms ) = ( $options->{'include_hetatoms'} );
+
+    $include_hetatoms //= 0;
+}
+
 sub unique_bond_parameters
 {
     my ( $bond_parameters ) = @_;
