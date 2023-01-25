e89ff63d3a08d608902d16bef340697a86fe7037
algirdas <algirdas.grybauskas@yahoo.com>
2023-01-25 10:01:26 +0200

    Starting BondParameters object in order to have more controled behaviour when retrieving bond parameter data.

---
+++ lib/BondParameters.pm
@@ -0,0 +1,19 @@
+package BondParameters;
+
+use strict;
+use warnings;
+
+# ------------------------- Constructors/Destructors -------------------------- #
+
+sub new
+{
+    my ( $class, $args ) = @_;
+    my $self = {};
+    return bless $self, $class;
+}
+
+# ----------------------------- Setters/Getters ------------------------------- #
+
+# --------------------------------- Methods ----------------------------------- #
+
+1;
