d3ce8fb69e33e8ea359b8ced6ba1c60e5e001639
algirdas <algirdas.grybauskas@yahoo.com>
2023-01-25 12:19:51 +0200

    Added empty methods that will be filled with moved static functions from other modules.

--- lib/BondParameters.pm
+++ lib/BondParameters.pm
@@ -8,12 +8,42 @@ use warnings;
 sub new
 {
     my ( $class, $args ) = @_;
-    my $self = {};
+    my $self = { 'parameters' => $args->{'parameters'} };
     return bless $self, $class;
 }
 
 # ----------------------------- Setters/Getters ------------------------------- #
 
+sub set_rotatable_bonds
+{
+
+}
+
+sub set_stretchable_bonds
+{
+
+}
+
+sub set_bendable_angles
+{
+
+}
+
 # --------------------------------- Methods ----------------------------------- #
+
+sub calculate_dihedral_angles
+{
+
+}
+
+sub calculate_bond_lengths
+{
+
+}
+
+sub calculate_bond_angles
+{
+
+}
 
 1;
