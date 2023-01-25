0a32b374d692c8d165cb96fea6aefb5b03bd48fd
algirdas <algirdas.grybauskas@yahoo.com>
2023-01-25 14:49:12 +0200

    Renaming test outputs from rotatable_bonds() to BondParameters_set_rotatable_bonds().

--- tests/cases/BondParameters_set_rotatable_bonds_001.sh
+++ tests/cases/BondParameters_set_rotatable_bonds_001.sh
@@ -2,6 +2,6 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/arginine-001.dump
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/alanine-001.dump
 
-$(dirname "$0")/../scripts/BondParameters_set_rotatable_bonds ${pdbx_dump_file}
+"$(dirname "$0")"/../scripts/rotatable_bonds ${pdbx_dump_file}
