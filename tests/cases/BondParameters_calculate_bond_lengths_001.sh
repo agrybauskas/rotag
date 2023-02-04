c131f0429a3493de79f740ca0f5eb75b9e177561
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-04 05:00:45 +0200

    Moved bond length calculations to a method. Also, patched appropriate test files.

--- tests/cases/BondParameters_calculate_bond_lengths_001.sh
+++ tests/cases/BondParameters_calculate_bond_lengths_001.sh
@@ -5,4 +5,4 @@ export PERL5LIB=$(dirname "$0")/../../lib
 pdbx_dump_file=$(dirname "$0")/../inputs/polipeptides/dipeptide-001.dump
 calc_mainchain=1
 
-$(dirname "$0")/../scripts/all_bond_lengths ${pdbx_dump_file} $calc_mainchain
+$(dirname "$0")/../scripts/BondParameters_calculate_bond_lengths ${pdbx_dump_file} $calc_mainchain
