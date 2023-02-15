324e207f140f8ee67d9d4bc59496987d7929d5a4
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-15 07:14:10 +0200

    Patching a test case for generate_pseudo() that is related to chained angle bending.

--- tests/cases/generate_pseudo_hetatom_011.sh
+++ tests/cases/generate_pseudo_hetatom_011.sh
@@ -4,7 +4,7 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-007.dump
 atom_id=152
-angle_and_length_ranges="N-CA-CB 0.5*pi,1.5*pi & CA-CB-OG 1.5*pi,2*pi"
+angle_and_length_ranges="N-CA-CB 0.5*pi,1.0*pi & CA-CB-OG 0.5*pi,1.0*pi"
 num_of_angles_and_lengths="N-CA-CB 5 & CA-CB-OG 5"
 no_full_range=1
 
