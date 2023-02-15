eed2d657a1a36e353cc9052b0ec4c9c802fbf2c2
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-15 11:02:41 +0200

    Adding a test case where all the transformations are in the conformation.

---
+++ tests/cases/generate_pseudo_hetatom_012.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-007.dump
+atom_id=152
+angle_and_length_ranges="chi1 0.0,0.5*pi"
+num_of_angles_and_lengths="chi1 5"
+no_full_range=1
+
+$(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
+                                                   "${angle_and_length_ranges}" \
+                                                   "${num_of_angles_and_lengths}" \
+                                                   ${pdbx_dump_file} \
+                                                   ${no_full_range}
