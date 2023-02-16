a1e5c05934511d810baf768f4c02b86bca1c71af
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-16 18:46:19 +0200

    Combining rotatable and stretchable bond parameters in test cases.

---
+++ tests/cases/generate_rotamer_051.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-008.dump
+residue_unique_key="18,A,1,."
+angle_values="CA-CB 1.0 & CB-OG 2.0 & chi1 0.25*pi"
+do_angle_bending=1
+do_bond_torsion=1
+do_bond_stretching=1
+
+$(dirname "$0")/../scripts/generate_rotamer "${residue_unique_key}" \
+	                                        "${angle_values}" \
+					                        "${pdbx_dump_file}" \
+                                            "${do_angle_bending}" \
+                                            "${do_bond_torsion}" \
+                                            "${do_bond_stretching}"
