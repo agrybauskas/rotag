e9fd83728b7312c253c970335b9b24996f775de9
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-16 17:27:04 +0200

    Using angle bond combinations to test generate_rotamer(). It seems it is working.

---
+++ tests/cases/generate_rotamer_048.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-008.dump
+residue_unique_key="18,A,1,."
+angle_values="N-CA-CB 0.25*pi & CA-CB-OG 0.75*pi"
+do_angle_bending=1
+do_bond_torsion=1
+do_bond_stretching=1
+
+$(dirname "$0")/../scripts/generate_rotamer "${residue_unique_key}" \
+	                                        "${angle_values}" \
+					                        "${pdbx_dump_file}" \
+                                            "${do_angle_bending}" \
+                                            "${do_bond_torsion}" \
+                                            "${do_bond_stretching}"
