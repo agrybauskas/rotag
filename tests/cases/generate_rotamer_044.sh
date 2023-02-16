5801212133aeaa416c74865ab3a4c3a1bbb890e0
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-16 17:04:02 +0200

    Rotamers are properly generated when using single or chained bond stretching parameters.

--- tests/cases/generate_rotamer_044.sh
+++ tests/cases/generate_rotamer_044.sh
@@ -4,9 +4,9 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-008.dump
 residue_unique_key="18,A,1,."
-angle_values="CA-CB 1.0"
+angle_values="CB-OG 2.0"
 do_angle_bending=1
-do_bond_torsion=1
+do_bond_torsion=0
 do_bond_stretching=1
 
 $(dirname "$0")/../scripts/generate_rotamer "${residue_unique_key}" \
