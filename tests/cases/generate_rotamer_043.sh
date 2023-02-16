53246b58b0fafdcde585b6eff30670e9bab8cc80
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-16 16:48:31 +0200

    It seems that first try on combining all bond parameters worked in a test case -- every parameter request was executed.

--- tests/cases/generate_rotamer_043.sh
+++ tests/cases/generate_rotamer_043.sh
@@ -4,7 +4,7 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-008.dump
 residue_unique_key="18,A,1,."
-angle_values="CB-OG 1.0 & CA-CB-OG pi & chi1 0"
+angle_values="CB-OG 1.0 & CA-CB-OG 0.5*pi & chi1 0"
 do_angle_bending=1
 do_bond_torsion=1
 do_bond_stretching=1
