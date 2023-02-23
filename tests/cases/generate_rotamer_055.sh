11321465fb3c260b531d73eaf5e1189386bc8ada
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-23 11:54:01 +0200

    Adding tryptophan test input atom site data scructure in order to test more complex rings when changing bond parameters.

---
+++ tests/cases/generate_rotamer_055.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/histidine-bond-length-only-001.dump
+residue_unique_key="18,A,1,."
+angle_values=""
+do_angle_bending=0
+do_bond_torsion=0
+do_bond_stretching=1
+
+$(dirname "$0")/../scripts/generate_rotamer "${residue_unique_key}" \
+	                                        "${angle_values}" \
+					                        "${pdbx_dump_file}" \
+                                            "${do_angle_bending}" \
+                                            "${do_bond_torsion}" \
+                                            "${do_bond_stretching}"
