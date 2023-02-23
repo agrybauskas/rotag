54e2a76796b46cc5c4a8123883b7b917529763c3
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-23 14:26:38 +0200

    Changing bond lengths was correct for tryptophan residue. It should be noted that by changing bond lengths in ring systems the bond angles also changes.

--- tests/cases/generate_rotamer_056.sh
+++ tests/cases/generate_rotamer_056.sh
@@ -4,7 +4,7 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/tryptophan-bond-length-only-001.dump
 residue_unique_key="276,A,1,."
-angle_values=""
+angle_values="CG-CD1 1.0 & CD2-CE3 2.0 & CZ2-CH2 3.0"
 do_angle_bending=0
 do_bond_torsion=0
 do_bond_stretching=1
