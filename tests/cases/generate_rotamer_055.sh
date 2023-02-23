ef4bcff74b038a0c19b3bf4a3cc56bb28efee90e
algirdas <algirdas.grybauskas@yahoo.com>
2023-02-23 12:01:06 +0200

    Fixing the arguments to select proper residues in test cases for generate_rotamer().

--- tests/cases/generate_rotamer_055.sh
+++ tests/cases/generate_rotamer_055.sh
@@ -3,7 +3,7 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/histidine-bond-length-only-001.dump
-residue_unique_key="18,A,1,."
+residue_unique_key="94,A,1,."
 angle_values=""
 do_angle_bending=0
 do_bond_torsion=0
