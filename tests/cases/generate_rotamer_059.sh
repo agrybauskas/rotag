cc546029ff52c8f1370d71c308224da6f88d7b9a
algirdas <algirdas.grybauskas@yahoo.com>
2023-04-06 07:00:12 +0300

    Updating conformational matrix that solved the problem of having both bendable and rotatable bonds.

--- tests/cases/generate_rotamer_059.sh
+++ tests/cases/generate_rotamer_059.sh
@@ -4,8 +4,7 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-009.dump
 residue_unique_key="18,A,1,."
-angle_values="chi1 0.25*pi"
-# angle_values="N-CA-CB 0.5*pi & chi1 0.25*pi"
+angle_values="CA-CB-OG 0.5*pi & chi1 0.25*pi"
 do_angle_bending=1
 do_bond_torsion=1
 do_bond_stretching=1
