d932a495f6c9a4290cc5234d5e82eab55234b36d
algirdas <algirdas.grybauskas@yahoo.com>
2023-07-17 19:27:36 +0300

    Updating test cases for sample_angles().

--- tests/cases/sample_angles_001.sh
+++ tests/cases/sample_angles_001.sh
@@ -6,3 +6,6 @@ $(dirname "$0")/../scripts/sample_angles "0..2" 0.2
 $(dirname "$0")/../scripts/sample_angles "4..5" 0.2
 $(dirname "$0")/../scripts/sample_angles "0..1" 0.1
 $(dirname "$0")/../scripts/sample_angles "-1..1" 0.1
+$(dirname "$0")/../scripts/sample_angles "0..2" 0.1 0 1
+$(dirname "$0")/../scripts/sample_angles "0..2" 0.1 1 0
+$(dirname "$0")/../scripts/sample_angles "0..2" 0.1 1 1
