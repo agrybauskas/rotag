d16a741df4072f88fa08aafbf351ce223e204084
agrybauskas <algirdas.grybauskas@yahoo.com>
2023-07-19 13:00:59 +0300

    Added corner cases for sample_bond_parameters().

--- tests/cases/sample_bond_parameters_001.sh
+++ tests/cases/sample_bond_parameters_001.sh
@@ -2,9 +2,15 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
+$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 0
+$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 1
+$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 9
 $(dirname "$0")/../scripts/sample_bond_parameters "0..2" 10
 $(dirname "$0")/../scripts/sample_bond_parameters "4..5" 4
+$(dirname "$0")/../scripts/sample_bond_parameters "4..5" 5
+$(dirname "$0")/../scripts/sample_bond_parameters "0..1" 9
 $(dirname "$0")/../scripts/sample_bond_parameters "0..1" 10
+$(dirname "$0")/../scripts/sample_bond_parameters "-1..1" 19
 $(dirname "$0")/../scripts/sample_bond_parameters "-1..1" 20
 $(dirname "$0")/../scripts/sample_bond_parameters "0..2" 10 0 0
 $(dirname "$0")/../scripts/sample_bond_parameters "0..2" 10 0 1
