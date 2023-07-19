34218657a94e950e964ad73002bb43d650423e8d
agrybauskas <algirdas@lama>
2023-07-19 11:35:12 +0300

    Adding sampling adjustments in sample_bond_parameters().

--- lib/Sampling.pm
+++ lib/Sampling.pm
@@ -47,22 +47,25 @@ sub sample_bond_parameters
     $inclusive_end //= 1;
 
     my @bond_parameter_values;
-    my $updated_sampling_count = $sampling_count;
-
     if( defined $rand_count ) {
         # TODO: add random sampling.
     } else {
         my $min_value = $bond_parameter_ranges->[0][0];
         my $max_value = $bond_parameter_ranges->[0][1];
-        my $small_change = ( $max_value - $min_value ) / $updated_sampling_count;
-        if( $inclusive_start ) {
 
-        }
-        if( $inclusive_end ) {
+        my $updated_sampling_count =
+            $sampling_count + ( $inclusive_start || $inclusive_end ? -1 : 0 );
+        my $small_change =
+            ( $max_value - $min_value ) / $updated_sampling_count;
 
-        }
+        my $sampling_adjustment =
+            ( ! $inclusive_start ? $small_change : 0 );
+        $sampling_adjustment =
+            ( ! $inclusive_start && ! $inclusive_end ? (1/2) * $small_change : 0 );
+
         @bond_parameter_values =
-            map { $min_value + $_ * $small_change + $bond_parameter_shift }
+            map { $min_value + $_ * $small_change + $sampling_adjustment +
+                  $bond_parameter_shift }
                 ( 0..$sampling_count - 1 );
     }
 
--- tests/cases/sample_bond_parameters_001.sh
+++ tests/cases/sample_bond_parameters_001.sh
@@ -6,6 +6,6 @@ $(dirname "$0")/../scripts/sample_bond_parameters "0..2" 10
 $(dirname "$0")/../scripts/sample_bond_parameters "4..5" 4
 $(dirname "$0")/../scripts/sample_bond_parameters "0..1" 10
 $(dirname "$0")/../scripts/sample_bond_parameters "-1..1" 20
-$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 20 0 1
-$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 20 1 0
-$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 20 1 1
+$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 10 0 1
+$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 10 1 0
+$(dirname "$0")/../scripts/sample_bond_parameters "0..2" 10 1 1
