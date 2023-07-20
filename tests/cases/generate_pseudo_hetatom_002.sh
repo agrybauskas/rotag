e2628d43292047f9bfc4f0a34492f3905a840254
algirdas <algirdas.grybauskas@yahoo.com>
2023-07-20 16:02:54 +0300

    Patching one of the test cases for generate_pseudo_hetatom().

--- tests/cases/generate_pseudo_hetatom_002.sh
+++ tests/cases/generate_pseudo_hetatom_002.sh
@@ -6,10 +6,8 @@ pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-con
 atom_id=1925
 angle_and_length_ranges="N-CA-MG 0.5*pi,1.0*pi"
 num_of_angles_and_lengths="N-CA-MG 5"
-no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
-                                                   ${pdbx_dump_file} \
-                                                   ${no_full_range}
+                                                   ${pdbx_dump_file}
