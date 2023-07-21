d227698bc1437a6edc5acb4780dceeb51524148b
algirdas <algirdas.grybauskas@yahoo.com>
2023-07-21 19:23:09 +0300

    Specifying the inclusion and exclusion of certain bond parameter ranges.

--- tests/cases/generate_pseudo_hetatom_003.sh
+++ tests/cases/generate_pseudo_hetatom_003.sh
@@ -6,10 +6,10 @@ pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-con
 atom_id=1925
 angle_and_length_ranges="C-N-CA-MG -1.0*pi,1.0*pi"
 num_of_angles_and_lengths="C-N-CA-MG 10"
-no_full_range=1
+full_range="C-N-CA-MG 0"
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
                                                    ${pdbx_dump_file} \
-                                                   ${no_full_range}
+                                                   "${full_range}"
