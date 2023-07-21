b848dcad9240d9fd22b9f8e63e45eb40b37a7e50
algirdas <algirdas.grybauskas@yahoo.com>
2023-07-21 19:26:29 +0300

    Specifying inclusion of sampling ranges for generate_pseudo_hetatom() test case.

--- tests/cases/generate_pseudo_hetatom_004.sh
+++ tests/cases/generate_pseudo_hetatom_004.sh
@@ -6,10 +6,10 @@ pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-con
 atom_id=1925
 angle_and_length_ranges="C-N-CA-MG 0.5*pi,1.0*pi & CA-MG 0.0,2.0"
 num_of_angles_and_lengths="C-N-CA-MG 4 & CA-MG 3"
-no_full_range=1
+full_range="C-N-CA-MG 0 & CA-MG 1"
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
                                                    ${pdbx_dump_file} \
-                                                   ${no_full_range}
+                                                   "${full_range}"
