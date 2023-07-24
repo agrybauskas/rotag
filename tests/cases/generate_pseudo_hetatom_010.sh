9eb151baba701742d237d731a03a53eb252a8f74
algirdas <algirdas.grybauskas@yahoo.com>
2023-07-24 11:27:51 +0300

    Patching the rest of the generate_pseudo_hetatom() test cases and outputs.

--- tests/cases/generate_pseudo_hetatom_010.sh
+++ tests/cases/generate_pseudo_hetatom_010.sh
@@ -6,10 +6,8 @@ pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-006.dump
 atom_id=151
 angle_and_length_ranges="N-CA-CB 0,pi"
 num_of_angles_and_lengths="N-CA-CB 5"
-no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
-                                                   ${pdbx_dump_file} \
-                                                   ${no_full_range}
+                                                   ${pdbx_dump_file}
--- tests/cases/generate_pseudo_hetatom_012.sh
+++ tests/cases/generate_pseudo_hetatom_012.sh
@@ -6,10 +6,10 @@ pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-008.dump
 atom_id=152
 angle_and_length_ranges="CA-CB-OG 0.5*pi,1.0*pi & CA-CB 1.0,1.5 & chi1 0.0,0.5*pi"
 num_of_angles_and_lengths="CA-CB-OG 3 & CA-CB 3 & chi1 3"
-no_full_range=1
+full_range="CA-CB-OG 1 & CA-CB 1 & chi1 0"
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
                                                    ${pdbx_dump_file} \
-                                                   ${no_full_range}
+                                                   "${full_range}"
