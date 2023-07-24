35069ea783184424d890fc5c7f49fe150e39efd2
algirdas <algirdas.grybauskas@yahoo.com>
2023-07-24 11:17:22 +0300

    Patching two test cases for generate_pseudo_hetatom().

--- tests/cases/generate_pseudo_hetatom_009.sh
+++ tests/cases/generate_pseudo_hetatom_009.sh
@@ -6,10 +6,8 @@ pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/serine-005.dump
 atom_id=152
 angle_and_length_ranges="CA-CB 1.0,2.0 & CB-OG 1.0,2.0"
 num_of_angles_and_lengths="CA-CB 5 & CB-OG 5"
-no_full_range=1
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
                                                    "${num_of_angles_and_lengths}" \
-                                                   ${pdbx_dump_file} \
-                                                   ${no_full_range}
+                                                   ${pdbx_dump_file}
