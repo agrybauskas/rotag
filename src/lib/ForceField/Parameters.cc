e69342ebfc39a58bec0786379366ae6a763095d7
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-02 08:07:48 +0300

    Refactoring header imports.

--- src/lib/ForceField/Parameters.cc
+++ src/lib/ForceField/Parameters.cc
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string/join.hpp>
 
--- src/lib/Version.cc
+++ src/lib/Version.cc
@@ -1,7 +1,7 @@
+#include "Version.h"
+
 #include <iostream>
 #include <string>
-
-#include "Version.h"
 
 std::string version() {
   return "rotag: 0.1.0";
