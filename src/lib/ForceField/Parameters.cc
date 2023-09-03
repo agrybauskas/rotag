43680d92be280a1c6e54f945b09add228434ff95
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-03 08:41:44 +0300

    Assigning parameter struct to a variable.

--- src/lib/ForceField/Parameters.cc
+++ src/lib/ForceField/Parameters.cc
@@ -3,5 +3,5 @@
 #include "cif_compiler.h"
 
 Parameters::Parameters(std::string parameter_file) {
-    new_cif_from_cif_file((char*) &parameter_file);
+    CIF* parameters = new_cif_from_cif_file((char*) &parameter_file);
 }
