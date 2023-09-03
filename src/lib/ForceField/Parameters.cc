0403ea2e8bfb106684be6a6887963514239d0bed
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-03 13:33:51 +0300

    Trying to figure out why the cif_option_default() function is not found.

--- src/lib/ForceField/Parameters.cc
+++ src/lib/ForceField/Parameters.cc
@@ -1,7 +1,11 @@
 #include "Parameters.h"
 
 #include "cif_compiler.h"
+#include "cif_options.h"
 
 Parameters::Parameters(std::string parameter_file) {
-    CIF* parameters = new_cif_from_cif_file((char*) &parameter_file);
+    cif_option_t compiler_options = cif_option_default();
+    cexception_t inner;
+    CIF* parameters =
+        new_cif_from_cif_file((char*) &parameter_file, compiler_options, &inner);
 }
