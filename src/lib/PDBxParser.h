2a01337decd5e328111fa290a296992b6b1832c7
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-04 16:13:09 +0300

    Removing all the source code from PDBxParser.cc, because they are not necessary anymore, because parsing will be done by "codcif".

--- src/lib/PDBxParser.h
+++ src/lib/PDBxParser.h
@@ -4,21 +4,5 @@
 #include <map>
 #include <string>
 #include <vector>
-#include <boost/any.hpp>
 
-struct pdbx_category {
-  std::vector<std::string>  attributes = {};
-  bool is_loop = false;
-  bool is_indexed = false;
-
-  boost::any data = {};
-};
-
-typedef std::map<std::string, pdbx_category> pdbx_data;
-
-void obtain_pdbx_data(std::string, std::vector<std::string>);
-
-pdbx_data obtain_pdbx_line(std::string, std::vector<std::string>);
-
-void obtain_pdbx_loop(std::string, std::vector<std::string>);
 #endif
