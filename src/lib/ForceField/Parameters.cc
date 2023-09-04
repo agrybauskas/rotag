a520752029d91341f017dd6125d0caf93af05e1c
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-04 12:30:55 +0300

    Uncommenting cexception_t type initialization.

--- src/lib/ForceField/Parameters.cc
+++ src/lib/ForceField/Parameters.cc
@@ -1,13 +1,13 @@
 #include "Parameters.h"
 
 extern "C" {
-  // #include "cif_compiler.h"
+  #include "cif_compiler.h"
   #include "cif_options.h"
 }
 
 Parameters::Parameters(std::string parameter_file) {
     cif_option_t compiler_options = cif_option_default();
-    // cexception_t inner;
+    cexception_t inner;
     // CIF* parameters =
     //     new_cif_from_cif_file((char*) &parameter_file, compiler_options, &inner);
 }
