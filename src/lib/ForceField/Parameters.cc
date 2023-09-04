2c6c72e4b69d8229606116f915777e0d49bf8196
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-04 16:29:30 +0300

    Another round of space refactoring in Parameters.cc.

--- src/lib/ForceField/Parameters.cc
+++ src/lib/ForceField/Parameters.cc
@@ -13,7 +13,7 @@ Parameters::Parameters(char* parameter_file) {
   cif_option_t compiler_options = cif_option_default();
   cexception_t inner;
   CIF* parameters =
-      new_cif_from_cif_file(parameter_file, compiler_options, &inner);
+    new_cif_from_cif_file(parameter_file, compiler_options, &inner);
 
   std::cout << parameters->minor_version << std::endl;
 }
