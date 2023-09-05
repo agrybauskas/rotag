4cf1cfc20bb794645e464d808fb6da3def57b06b
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-05 10:48:31 +0300

    Removed unnecessary header and also import.

--- src/lib/ForceField/CIF.cc
+++
@@ -1,1 +0,0 @@
-#include "CIF.h"
--- src/lib/ForceField/Parameters.cc
+++ src/lib/ForceField/Parameters.cc
@@ -6,18 +6,14 @@
 
 extern "C" {
   #include "cif_compiler.h"
-  #include "cif_options.h"
-  #include "datablock.h"
 }
-
-#include "CIF.h"
 
 Parameters::Parameters(char* parameter_file) {
   cif_option_t compiler_options = cif_option_default();
   cexception_t inner;
   CIF* parameters =
     new_cif_from_cif_file(parameter_file, compiler_options, &inner);
-  datablock_dump(parameters->datablock_list);
+  std::cout << cif_tag_index(parameters, "_[local]_atom_properties.type_symbol") << std::endl;
 }
 
 Parameters::~Parameters() {};
