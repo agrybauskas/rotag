da04f1cf03555c3519c208ae2674cfbc85b68b9a
algirdas <algirdas.grybauskas@yahoo.com>
2023-09-20 13:52:29 +0300

    Tracking which heteroatoms were already connected to.

--- lib/PseudoAtoms.pm
+++ lib/PseudoAtoms.pm
@@ -1081,14 +1081,15 @@ sub assign_hetatoms_to_residues
                                 ] } } );
             # Iteration has to be performed, because '_struct_conn' does not
             # have 'pdbx_PDB_model_num' and 'label_alt_id' entries.
-            my %visited_residues = ();
+            my %visited_residues_by_hetatom = ();
             for my $connected_atom_id ( keys %{ $connected_atom_site } ) {
                 my $connected_unique_residue_key = unique_residue_key(
                     $connected_atom_site->{$connected_atom_id}
                 );
 
-                next if $visited_residues{$connected_unique_residue_key};
-                $visited_residues{$connected_unique_residue_key} = 1;
+                next if defined $visited_residues_by_hetatom{$connected_unique_residue_key} &&
+                    $visited_residues_by_hetatom{$connected_unique_residue_key} == $hetatom_id;
+                $visited_residues_by_hetatom{$connected_unique_residue_key} = $hetatom_id;
 
                 connect_atoms_explicitly( $atom_site,
                                           [ $hetatom_id ],
