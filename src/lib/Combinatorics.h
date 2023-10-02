d4b6d32a454c5ee47d9b759f4a905cb8722277a1
algirdas <algirdas.grybauskas@yahoo.com>
2023-10-02 13:05:38 +0300

    Decided to make template for permutation as both string and double lists will be permuted.

--- src/lib/Combinatorics.h
+++ src/lib/Combinatorics.h
@@ -3,10 +3,23 @@
 
 #include <vector>
 
-std::vector<std::vector<double>>
+template <class T>
+std::vector<std::vector<T>>
   permutation(int size,
-              std::vector<std::vector<double>> base,
-              std::vector<double> list,
-              std::vector<std::vector<double>> permuted_list);
+              std::vector<std::vector<T>> base,
+              std::vector<T> list,
+              std::vector<std::vector<T>> permuted_list) {
+  const int base_size = base.size();
+  if(base_size == size) {
+    /* permuted_list.push_back(base); */
+  } else {
+    for(size_t i = 0; i < list.size(); i++) {
+      // const std::vector<std::vector<double>> updated_base =
+      //   base.push_back(list[base_size][i]);
+      // permutation(size, updated_base, list, permuted_list);
+    }
+  }
+  return permuted_list;
+}
 
 #endif
