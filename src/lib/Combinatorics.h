b262769953ea37badf50de972db0d04cb8d0ce01
algirdas <algirdas.grybauskas@yahoo.com>
2023-10-03 16:03:42 +0300

    Getting to the "truth". Output is still messy, but logical.

--- src/lib/Combinatorics.h
+++ src/lib/Combinatorics.h
@@ -5,22 +5,21 @@
 #include <iostream>
 
 template <class T>
-std::vector<std::vector<T>>
-  permutation(size_t size,
-              std::vector<std::vector<T>> non_permuted_list,
-              std::vector<std::vector<T>> permuted_list = {{}},
-              std::vector<T> base = {}) {
+void permutation(size_t size,
+                 std::vector<std::vector<T>> item_list,
+                 std::vector<std::vector<T>> *permuted_list,
+                 std::vector<T> base = {}) {
   const size_t base_size = base.size();
   if(base_size == size) {
-    permuted_list.push_back(base);
+    (*permuted_list).push_back(base);
   } else {
-    for(size_t i = 0; i < non_permuted_list[base_size].size(); i++) {
+    const size_t last_idx = item_list.size() > 0 ? item_list.size() - 1 : 0;
+    for(size_t i = 0; i <= item_list[last_idx].size(); i++) {
       std::vector<T> updated_base = base;
-      updated_base.push_back(non_permuted_list[base_size][i]);
-      permutation(size, non_permuted_list, permuted_list , updated_base);
+      updated_base.push_back(item_list[last_idx][i]);
+      permutation(size, item_list, permuted_list, updated_base);
     }
   }
-  return permuted_list;
 }
 
 #endif
--- src/rotag_select.cc
+++ src/rotag_select.cc
@@ -182,17 +182,18 @@ int main(int argc, char *argv[]) {
 
   Parameters parameters(argv[0]);
 
-  // std::vector<std::vector<double>> permuted_list = {{}};
-  std::vector<std::vector<double>> permuted_list =
+  std::vector<std::vector<double>> permuted_list = {{}};
   permutation<double>(
-    3,
-    std::vector<std::vector<double>>{{1.2, 3.4, 5.6}}
+    2,
+    std::vector<std::vector<double>>{{1.2, 3.4}},
+    &permuted_list
   );
 
   for(size_t i = 0; i < permuted_list.size(); i++) {
     for(size_t j = 0; j < permuted_list[i].size(); j++) {
-      std::cout << permuted_list[i][j] << std::endl;
+      std::cout << permuted_list[i][j] << "\t";
     }
+    std::cout << std::endl << "----------" << std::endl;
   }
 
   return 0;
