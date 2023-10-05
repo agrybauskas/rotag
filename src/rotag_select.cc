83f161c69dd32cf00edd48c10bb5d7c778c28027
agrybauskas <algirdas.grybauskas@yahoo.com>
2023-10-05 16:37:18 +0300

    Removed debug print.

--- src/rotag_select.cc
+++ src/rotag_select.cc
@@ -188,7 +188,6 @@ int main(int argc, char *argv[]) {
     if (is_pdb) {
     } else {
       atom_site = mmcif_to_atom_site(argv[index]);
-      std::cout << atom_site[147].id << std::endl;
     }
   }
 
