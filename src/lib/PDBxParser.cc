7db3c9a82bca67c1dd17401ec6154f5abc222c25
agrybauskas <algirdas.grybauskas@yahoo.com>
2023-10-05 15:57:05 +0300

    Lowercased cif tags as it lowercases by default in codcif.

--- src/lib/PDBxParser.cc
+++ src/lib/PDBxParser.cc
@@ -1,5 +1,7 @@
 #include "PDBxParser.h"
 
+#include <iostream>
+
 extern "C" {
   #include "cif_compiler.h"
 }
@@ -14,7 +16,7 @@ AtomSite mmcif_to_atom_site(char* mmcif_file_path) {
   CIF* mmcif = new_cif_from_cif_file(mmcif_file_path, compiler_options, &inner);
 
   const std::vector<std::string> cif_tags = {
-    "_atom_site.group_PDB",
+    "_atom_site.group_pdb",
     "_atom_site.id",
     "_atom_site.type_symbol",
     "_atom_site.label_atom_id",
@@ -23,16 +25,16 @@ AtomSite mmcif_to_atom_site(char* mmcif_file_path) {
     "_atom_site.label_asym_id",
     "_atom_site.label_entity_id",
     "_atom_site.label_seq_id",
-    "_atom_site.Cartn_x",
-    "_atom_site.Cartn_y",
-    "_atom_site.Cartn_z",
+    "_atom_site.cartn_x",
+    "_atom_site.cartn_y",
+    "_atom_site.cartn_z",
     "_atom_site.occupancy",
-    "_atom_site.B_iso_or_equiv",
+    "_atom_site.b_iso_or_equiv",
     "_atom_site.auth_seq_id",
     "_atom_site.auth_comp_id",
     "_atom_site.auth_asym_id",
     "_atom_site.auth_atom_id",
-    "_atom_site.pdbx_PDB_model_num",
+    "_atom_site.pdbx_pdb_model_num",
     "_atom_site.rotag_selection_state",
     "_atom_site.rotag_selection_group"
   };
@@ -45,10 +47,10 @@ AtomSite mmcif_to_atom_site(char* mmcif_file_path) {
       cif_value_length_lookup(datablock, cif_tags);
 
     // "_atom_site" category.
-    for (ssize_t i = 0; i <cif_value_length_lookup_table["_atom_site.id"]; i++) {
+    for (int i = 0; i < cif_value_length_lookup_table["_atom_site.id"]; i++) {
       Atom atom = {};
-      // atom.group_PDB = cifvalue_to_string(
-      //   datablock, cif_tag_index_lookup_table, "_atom_site.group_PDB", i);
+      atom.group_pdb = cifvalue_to_string(
+        datablock, cif_tag_index_lookup_table, "_atom_site.group_pdb", i);
       atom.id = cifvalue_to_long(
         datablock, cif_tag_index_lookup_table, "_atom_site.id", i);
       atom.type_symbol = cifvalue_to_string(
@@ -65,16 +67,16 @@ AtomSite mmcif_to_atom_site(char* mmcif_file_path) {
         datablock, cif_tag_index_lookup_table, "_atom_site.label_entity_id", i);
       atom.label_seq_id = cifvalue_to_long(
         datablock, cif_tag_index_lookup_table, "_atom_site.label_seq_id", i);
-      // atom.Cartn_x = cifvalue_to_double(
-      //   datablock, cif_tag_index_lookup_table, "_atom_site.Cartn_x", i);
-      // atom.Cartn_y = cifvalue_to_double(
-      //   datablock, cif_tag_index_lookup_table, "_atom_site.Cartn_y", i);
-      // atom.Cartn_z = cifvalue_to_double(
-      //   datablock, cif_tag_index_lookup_table, "_atom_site.Cartn_z", i);
+      atom.cartn_x = cifvalue_to_double(
+        datablock, cif_tag_index_lookup_table, "_atom_site.cartn_x", i);
+      atom.cartn_y = cifvalue_to_double(
+        datablock, cif_tag_index_lookup_table, "_atom_site.cartn_y", i);
+      atom.cartn_z = cifvalue_to_double(
+        datablock, cif_tag_index_lookup_table, "_atom_site.cartn_z", i);
       atom.occupancy = cifvalue_to_double(
         datablock, cif_tag_index_lookup_table, "_atom_site.occupancy", i);
-      // atom.B_iso_or_equiv = cifvalue_to_double(
-      //   datablock, cif_tag_index_lookup_table, "_atom_site.B_iso_or_equiv", i);
+      atom.b_iso_or_equiv = cifvalue_to_double(
+        datablock, cif_tag_index_lookup_table, "_atom_site.b_iso_or_equiv", i);
       atom.auth_seq_id = cifvalue_to_long(
         datablock, cif_tag_index_lookup_table, "_atom_site.auth_seq_id", i);
       atom.auth_comp_id = cifvalue_to_string(
@@ -83,11 +85,11 @@ AtomSite mmcif_to_atom_site(char* mmcif_file_path) {
         datablock, cif_tag_index_lookup_table, "_atom_site.auth_asym_id", i);
       atom.auth_atom_id = cifvalue_to_string(
         datablock, cif_tag_index_lookup_table, "_atom_site.auth_atom_id", i);
-      // atom.pdbx_PDB_model_num = cifvalue_to_long(
-      //   datablock,
-      //   cif_tag_index_lookup_table,
-      //   "_atom_site.pdbx_PDB_model_num",
-      //   i);
+      atom.pdbx_pdb_model_num = cifvalue_to_long(
+        datablock,
+        cif_tag_index_lookup_table,
+        "_atom_site.pdbx_pdb_model_num",
+        i);
       // atom.selection_state = cifvalue_to_string(
       //   datablock,
       //   cif_tag_index_lookup_table,
--- src/lib/PDBxParser.h
+++ src/lib/PDBxParser.h
@@ -7,7 +7,7 @@
 
 struct Atom {
   // "_atom_site" category-related.
-  std::string group_PDB;                // "ATOM" or "HETATM".
+  std::string group_pdb;                // "ATOM" or "HETATM".
   unsigned long int id;                 // Atom id.
   std::string type_symbol;              // Chemical element.
   std::string label_atom_id;            // Atom label.
@@ -16,14 +16,14 @@ struct Atom {
   std::string label_asym_id;            // Chain name.
   unsigned long int label_entity_id;    // Molecular entity.
   unsigned long int label_seq_id;       // Residue id.
-  double Cartn_x, Cartn_y, Cartn_z;     // Cartesian coordinates of the atom.
+  double cartn_x, cartn_y, cartn_z;     // Cartesian coordinates of the atom.
   double occupancy;                     // The fraction present in the site.
-  double B_iso_or_equiv;                // Isotropic displacement.
+  double b_iso_or_equiv;                // Isotropic displacement.
   unsigned long int auth_seq_id;        // Author's residue id.
   std::string auth_comp_id;             // Author's residue name.
   std::string auth_asym_id;             // Author's chain name.
   std::string auth_atom_id;             // Author's atom label.
-  unsigned long int pdbx_PDB_model_num; // Model id.
+  unsigned long int pdbx_pdb_model_num; // Model id.
 
   // Selection.
   std::string selection_state;          // Marks selection state: T, S ir H.
