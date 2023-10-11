1b129921e2256bf2e9d77959fbeb627760d27442
algirdas <algirdas.grybauskas@yahoo.com>
2023-10-11 12:36:59 +0300

    Refactored AlgebraicMatrix module.

--- src/lib/AlgebraicMatrix.cc
+++ src/lib/AlgebraicMatrix.cc
@@ -1,58 +1,48 @@
 #include "AlgebraicMatrix.h"
 
-/* --------------------- Contructors and/or destructors ---------------------- */
+AlgebraicMatrix::AlgebraicMatrix() {}
 
-AlgebraicMatrix::AlgebraicMatrix(){};
-
-/* -------------------------------- Methods ---------------------------------- */
-
-void AlgebraicMatrix::evaluate( std::map<std::string, double> symbol_values )
-{
+void AlgebraicMatrix::evaluate(std::map<std::string, double> symbol_values) {
   std::vector<std::string> symbols = this->symbols;
 
-  /* Checks if all symbol values are present. */
+  // Checks if all symbol values are present.
   try {
-    for( std::string symbol : symbols ) {
-      if( symbol_values.count( symbol ) == 0 ) {
+    for (std::string symbol : symbols) {
+      if (symbol_values.count(symbol) == 0) {
         throw "'" + symbol + "'" + " value is not passed";
       }
     }
-  } catch( std::string error_message ) {
+  } catch (std::string error_message) {
     std::cout << error_message << std::endl;
-    exit( EXIT_FAILURE );
+    exit(EXIT_FAILURE);
   }
 
-  /* Prepares array for passing to matrix function. */
+  // Prepares array for passing to matrix function.
   int symbols_size = symbols.size();
   double args[symbols_size];
-  for( int i = 0; i < symbols_size; i++ ) {
+  for (int i = 0; i < symbols_size; i++) {
     args[i] = symbol_values[symbols[i]];
   }
 
-  this->matrix = this->matrix_function( args );
+  this->matrix = this->matrix_function(args);
   this->is_evaluated = true;
 
   return;
 }
 
-/* -------------------------- Setters and Getters ---------------------------- */
-
-void AlgebraicMatrix::set_symbols( std::vector<std::string> symbols )
-{
+void AlgebraicMatrix::set_symbols(std::vector<std::string> symbols) {
   this->symbols = symbols;
   return;
 }
 
-void AlgebraicMatrix::set_matrix( std::vector< std::vector<double > > matrix )
-{
+void AlgebraicMatrix::set_matrix(std::vector<std::vector<double>> matrix) {
   this->matrix = matrix;
   this->is_evaluated = true;
   return;
 }
 
 void AlgebraicMatrix::set_matrix_function(
-    std::vector< std::vector<double> > ( *matrix_function )( double args[] ) )
-{
+  std::vector<std::vector<double>>(*matrix_function)(double args[])) {
   this->matrix_function = matrix_function;
   return;
 }
@@ -61,12 +51,10 @@ std::vector<std::string> AlgebraicMatrix::get_symbols() {
   return this->symbols;
 }
 
-bool AlgebraicMatrix::get_is_evaluated()
-{
+bool AlgebraicMatrix::get_is_evaluated() {
   return this->is_evaluated;
 }
 
-std::vector< std::vector<double > > AlgebraicMatrix::get_matrix()
-{
+std::vector<std::vector<double>> AlgebraicMatrix::get_matrix() {
   return this->matrix;
 }
--- src/lib/AlgebraicMatrix.h
+++ src/lib/AlgebraicMatrix.h
@@ -1,5 +1,5 @@
-#ifndef _ALGEBRAICMATRIX_H_
-#define _ALGEBRAICMATRIX_H_
+#ifndef SRC_LIB_ALGEBRAICMATRIX_H_
+#define SRC_LIB_ALGEBRAICMATRIX_H_
 
 #include <iostream>
 #include <string>
@@ -10,20 +10,21 @@ class AlgebraicMatrix {
  private:
   bool is_evaluated = false;
   std::vector<std::string> symbols;
-  std::vector< std::vector<double> > matrix;
-  std::vector< std::vector<double> > ( *matrix_function )( double args[] );
+  std::vector< std::vector<double>> matrix;
+  std::vector< std::vector<double>>(*matrix_function)(double args[]);
 
  public:
   AlgebraicMatrix();
 
-  void set_symbols( std::vector<std::string> symbols );
-  void set_matrix( std::vector< std::vector<double > > matrix );
-  void set_matrix_function( std::vector< std::vector<double> > ( *matrix_function )( double args[] ) );
+  void set_symbols(std::vector<std::string> symbols);
+  void set_matrix(std::vector<std::vector<double>> matrix);
+  void set_matrix_function(std::vector<std::vector<double>>
+                           (*matrix_function)(double args[]));
 
-  void evaluate( std::map<std::string, double> symbol_values );
+  void evaluate(std::map<std::string, double> symbol_values);
   std::vector<std::string> get_symbols();
   bool get_is_evaluated();
   std::vector< std::vector<double > > get_matrix();
 };
 
-#endif
+#endif  // SRC_LIB_ALGEBRAICMATRIX_H_
