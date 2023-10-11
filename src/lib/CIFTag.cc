c47995f991fec294f8e526661186432d222f40a7
algirdas <algirdas.grybauskas@yahoo.com>
2023-10-11 12:38:21 +0300

    Refactored code-style for CIFTag.

--- src/lib/CIFTag.cc
+++ src/lib/CIFTag.cc
@@ -17,7 +17,8 @@ std::map<std::string, ssize_t> cif_value_length_lookup(
   std::map<std::string, ssize_t> cif_value_length_lookup;
   for (const std::string &cif_tag : cif_tags) {
     cif_value_length_lookup[cif_tag] =
-      datablock_value_lengths(datablock)[datablock_tag_index(datablock, (char*) cif_tag.c_str())];
+      datablock_value_lengths(datablock)
+        [datablock_tag_index(datablock, (char*) cif_tag.c_str())];
   }
   return cif_value_length_lookup;
 }
