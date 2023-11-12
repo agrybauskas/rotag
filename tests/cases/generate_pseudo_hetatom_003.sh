3fc22592cad20a9dd2c72c6cfa9ea2cb6669d5da
algirdas <algirdas.grybauskas@yahoo.com>
2023-11-12 21:30:05 +0200

    Modifying test case as the hetatoms will not be connected to CA rather to CNOPS atoms.

--- tests/cases/generate_pseudo_hetatom_003.sh
+++ tests/cases/generate_pseudo_hetatom_003.sh
@@ -4,9 +4,9 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-004.dump
 atom_id=1925
-angle_and_length_ranges="C-N-CA-MG -1.0*pi,1.0*pi"
-num_of_angles_and_lengths="C-N-CA-MG 10"
-full_range="C-N-CA-MG 0"
+angle_and_length_ranges="CB-CG-OD1-MG -1.0*pi,1.0*pi"
+num_of_angles_and_lengths="CB-CG-OD1-MG 10"
+full_range="CB-CG-OD1-MG 0"
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
