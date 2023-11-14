8886733b36b425c9f8d7f4cd41971cf13b354e7d
algirdas <algirdas.grybauskas@yahoo.com>
2023-11-14 23:20:52 +0200

    Patched the rest of generate_pseudo_hetatom test cases.

--- tests/cases/generate_pseudo_hetatom_005.sh
+++ tests/cases/generate_pseudo_hetatom_005.sh
@@ -4,9 +4,9 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-007.dump
 atom_id=1925
-angle_and_length_ranges="C-N-CA-MG 0.5*pi,1.0*pi & N-CA-MG 0.5*pi,1.0*pi"
-num_of_angles_and_lengths="C-N-CA-MG 5 & N-CA-MG 5"
-full_range="C-N-CA-MG 0 & N-CA-MG 1"
+angle_and_length_ranges="CB-CG-OD1-MG 0.5*pi,1.0*pi & CG-OD1-MG 0.5*pi,1.0*pi"
+num_of_angles_and_lengths="CB-CG-OD1-MG 5 & CG-OD1-MG 5"
+full_range="CB-CG-OD1-MG 0 & CG-OD1-MG 1"
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
--- tests/cases/generate_pseudo_hetatom_007.sh
+++ tests/cases/generate_pseudo_hetatom_007.sh
@@ -4,9 +4,9 @@ export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-009.dump
 atom_id=1925
-angle_and_length_ranges="C-N-CA-MG 0.5*pi,1.0*pi & CA-MG 0.5,2.5 & N-CA-MG 0.5*pi,1.0*pi"
-num_of_angles_and_lengths="C-N-CA-MG 4 & CA-MG 4 & N-CA-MG 4"
-full_range="C-N-CA-MG 0 & CA-MG 1 & N-CA-MG 1"
+angle_and_length_ranges="CB-CG-OD1-MG 0.5*pi,1.0*pi & OD1-MG 0.5,2.5 & CG-OD1-MG 0.5*pi,1.0*pi"
+num_of_angles_and_lengths="CB-CG-OD1-MG 4 & OD1-MG 4 & CG-OD1-MG 4"
+full_range="CB-CG-OD1-MG 0 & OD1-MG 1 & CG-OD1-MG 1"
 
 $(dirname "$0")/../scripts/generate_pseudo_hetatom "${atom_id}" \
                                                    "${angle_and_length_ranges}" \
