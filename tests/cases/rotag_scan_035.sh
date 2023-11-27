6d634a8859370f0849336b203902af538fed4ee2
algirdas <algirdas.grybauskas@yahoo.com>
2023-11-27 17:08:02 +0200

    Added test cases for checking the beahviour of scannning bond angle and bond length changes.

--- tests/cases/rotag_scan_035.sh
+++ tests/cases/rotag_scan_035.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-001.cif
 
-rotag_scan -H -a "72.0,OD1-MG=0.5..0.5..1.5" ${pdbx_file}
+rotag_scan -H -a "72.0,OD1-MG=0.5..0.5..1.5,CG-OD1-MG=72.0" ${pdbx_file}
