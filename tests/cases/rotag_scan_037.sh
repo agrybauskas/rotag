213c6fc1f58db16f02b21b97ac82b74406f2350c
algirdas <algirdas.grybauskas@yahoo.com>
2024-01-20 10:13:40 +0200

    Adding differentiating angle changes for dihedral and bond angle parameters.

--- tests/cases/rotag_scan_037.sh
+++ tests/cases/rotag_scan_037.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-001.cif
 
-rotag_scan -H -a "120.0,CB-CG-OD1-MG=90.0;CG-OD1-MG=90.0" ${pdbx_file}
+rotag_scan -H -a "120.0,CB-CG-OD1-MG=90.0;CG-OD1-MG=30.0" ${pdbx_file}
--- tests/cases/rotag_scan_038.sh
+++ tests/cases/rotag_scan_038.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-001.cif
 
-rotag_scan -H -a "120.0,CB-CG-OD1-MG=90.0;OD1-MG=0.5..0.5..1.5;CG-OD1-MG=90.0" ${pdbx_file}
+rotag_scan -H -a "120.0,CB-CG-OD1-MG=90.0;OD1-MG=0.5..0.5..1.5;CG-OD1-MG=30.0" ${pdbx_file}
