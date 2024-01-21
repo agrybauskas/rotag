74e836e139392bcf806474c45ca052775a003391
algirdas <algirdas.grybauskas@yahoo.com>
2024-01-21 13:56:43 +0200

    Fixed variable passing to single-threaded function.

--- lib/PseudoAtoms.pm
+++ lib/PseudoAtoms.pm
@@ -569,7 +569,7 @@ sub generate_library
                 #                  $potential_functions{$interactions}{'bonded'},
                 #              ( $rmsd ? ( 'rmsd' => 1 ): ()  ),
                 #              'options' => $options },
-                #            [ @allowed_angles ] ) };
+                #            [ \@allowed_angles ] ) };
 
                 for( my $i = 0; $i <= $#{ $allowed_angles }; $i++  ) {
                     my %angles =
