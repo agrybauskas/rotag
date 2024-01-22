658609018e93c73c1e969ca9335d47a438f89469
algirdas <algirdas.grybauskas@yahoo.com>
2024-01-22 20:41:38 +0200

    Adding output of rotag_scan script in order to check how rotag_energy script performs. They should yield identical energies.

--- tests/cases/rotag_energy_018.sh
+++ tests/cases/rotag_energy_018.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-001.cif
+pdbx_file=$(dirname "$0")/../inputs/libraries/mg-with-sidechains-with-connections-library-001.cif
 
 rotag_energy -S ${pdbx_file}
