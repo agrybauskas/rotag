eedcc04ec3af77fccf98a85c234626fb03248b1f
algirdas <algirdas.grybauskas@yahoo.com>
2024-01-27 17:50:51 +0200

    Removing unnecessary flags.

--- tests/cases/assign_hetatoms_001.sh
+++ tests/cases/assign_hetatoms_001.sh
@@ -3,10 +3,8 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_struct_conn_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-without-connections-and-struct-conn-002.dump
-do_bond_stretching=1
-do_angle_bending=0
-do_bond_rotation=0
+assume_connections=1
 
 $(dirname "$0")/../scripts/assign_hetatoms \
-    ${pdbx_struct_conn_dump_file} ${do_bond_stretching} ${do_angle_bending} ${do_bond_rotation} \
+    ${pdbx_struct_conn_dump_file} ${assume_connections} \
     | sort -k 1 -n
--- tests/cases/assign_hetatoms_004.sh
+++ tests/cases/assign_hetatoms_004.sh
@@ -3,10 +3,7 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_struct_conn_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-and-struct-conn-001.dump
-do_bond_stretching=0
-do_angle_bending=0
-do_bond_rotation=0
 
 $(dirname "$0")/../scripts/assign_hetatoms \
-    ${pdbx_struct_conn_dump_file} ${do_bond_stretching} ${do_angle_bending} ${do_bond_rotation} \
+    ${pdbx_struct_conn_dump_file} \
     | sort -k 1 -n
