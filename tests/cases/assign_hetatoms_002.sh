9dcd9013244056bf64c6d514de40f736cd4a59d5
algirdas <algirdas.grybauskas@yahoo.com>
2024-01-27 18:51:29 +0200

    Removed redundant test cases.

--- tests/cases/assign_hetatoms_002.sh
+++ tests/cases/assign_hetatoms_002.sh
@@ -2,9 +2,8 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-pdbx_struct_conn_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-without-connections-and-struct-conn-002.dump
-assume_connections=1
+pdbx_struct_conn_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-and-struct-conn-001.dump
 
 $(dirname "$0")/../scripts/assign_hetatoms \
-    ${pdbx_struct_conn_dump_file} ${assume_connections} \
+    ${pdbx_struct_conn_dump_file} \
     | sort -k 1 -n
--- tests/cases/assign_hetatoms_004.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PERL5LIB=$(dirname "$0")/../../lib
-
-pdbx_struct_conn_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-with-connections-and-struct-conn-001.dump
-
-$(dirname "$0")/../scripts/assign_hetatoms \
-    ${pdbx_struct_conn_dump_file} \
-    | sort -k 1 -n
