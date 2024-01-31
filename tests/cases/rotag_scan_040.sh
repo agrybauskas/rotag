6d9d87b6a823c2e8fc7fdcd876fbd3819519a4bc
algirdas <algirdas.grybauskas@yahoo.com>
2024-01-31 13:17:04 +0200

    Added additional test files in order to test secondary pseudo connections -- interactions.

---
+++ tests/cases/rotag_scan_040.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-and-h2o-with-sidechains-with-connections-001.cif
+
+rotag_scan -H -a '120.0,CG-OD1-MG-O=90.0' ${pdbx_file}
---
+++ tests/cases/rotag_scan_041.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/h2o-and-mg-with-sidechains-with-connections-001.cif
+
+rotag_scan -H -a '120.0,OD1-H1-O-MG=90.0' ${pdbx_file}
