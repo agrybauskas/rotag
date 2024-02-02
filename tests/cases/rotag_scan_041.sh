b5501b70093f277689be7464ead7e7dbd80409e4
algirdas <algirdas.grybauskas@yahoo.com>
2024-02-02 04:31:07 +0200

    Fixed bond parameter names in three rotag_scan test cases.

--- tests/cases/rotag_scan_041.sh
+++ tests/cases/rotag_scan_041.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/h2o-and-mg-with-sidechains-with-connections-001.cif
 
-rotag_scan -H -a '120.0,OD1-H1-O.MG=90.0' ${pdbx_file}
+rotag_scan -H -a '120.0,OD1.H1-O.MG=90.0' ${pdbx_file}
