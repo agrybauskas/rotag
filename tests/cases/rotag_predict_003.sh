b7e0873d9c0e413231bd0f84ffeb08049c884bcf
algirdas <algirdas.grybauskas@yahoo.com>
2024-02-08 00:00:19 +0200

    Changed test input for rotag_predict test case.

--- tests/cases/rotag_predict_003.sh
+++ tests/cases/rotag_predict_003.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/libraries/trimer-library-001.cif
+pdbx_file=$(dirname "$0")/../inputs/libraries/asparagine-dimer-and-aspartic-acid-library-001.cif
 
 rotag_predict -S ${pdbx_file}
