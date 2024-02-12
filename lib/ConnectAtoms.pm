a730fbefbba45d6045d95d3c26644dce080eae18
algirdas <algirdas.grybauskas@yahoo.com>
2024-02-12 16:30:35 +0200

    Trying to track the renaming in the function block.

--- lib/ConnectAtoms.pm
+++ lib/ConnectAtoms.pm
@@ -380,6 +380,16 @@ sub assign_hetatoms
 
         my ( $connected_atom_id_1 ) = keys %{ $connected_atom_site_1 };
         my ( $connected_atom_id_2 ) = keys %{ $connected_atom_site_2 };
+        # Especially important when there are heteroatoms connected to another
+        # heteroatoms
+        if( exists $track_renamed_atom_ids{$connected_atom_id_1} ) {
+            $connected_atom_id_1 =
+                $track_renamed_atom_ids{$connected_atom_id_1};
+        }
+        if( exists $track_renamed_atom_ids{$connected_atom_id_2} ) {
+            $connected_atom_id_2 =
+                $track_renamed_atom_ids{$connected_atom_id_2};
+        }
 
         for my $related_atom_id_1 ( sort keys %{ $related_atom_site_1 } ) {
             replace_atom_site_ids( $atom_site,
