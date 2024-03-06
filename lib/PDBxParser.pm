214fbafe8c102703bb6b0c453bbbc2535d8c296a
algirdas <algirdas.grybauskas@yahoo.com>
2024-03-06 20:21:57 +0200

    Moving code that was wrongly moved up in to_pdbx().

--- lib/PDBxParser.pm
+++ lib/PDBxParser.pm
@@ -1437,8 +1437,9 @@ sub to_pdbx
                                       $category => $category_attribute_order}});
                 }
 
-                my $attribute_array_length = $#{ $attributes };
-                my $data_array_length = $#{ $data_array };
+                my $attribute_array_length =
+                    $#{ $pdbx_data->{$category}{'metadata'}{'attributes'} };
+                my $data_array_length = $#{ $pdbx_data->{$category}{'data'} };
 
                 for( my $i = 0;
                      $i <= $data_array_length;
