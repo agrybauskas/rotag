4efe52e8d3dccde52061423729ab41a58fc653c4
algirdas <algirdas.grybauskas@yahoo.com>
2024-03-30 15:11:09 +0200

    Added the example to function description about the output data structure.

--- lib/PDBxParser.pm
+++ lib/PDBxParser.pm
@@ -1226,6 +1226,12 @@ sub extract
 #     corresponding groups.
 # Output:
 #     %split_groups - hash of atom site data structures.
+#     Data structure example:
+#     { '12,A,1,.' => [
+#           1 => { 'label_seq_id' => 12,
+#                  'label_asym_id' => 'A',
+#                  'pdbx_PDB_model_num' => 1,
+#                  'label_alt_id' => '.' } ] }
 #
 
 sub split_by
