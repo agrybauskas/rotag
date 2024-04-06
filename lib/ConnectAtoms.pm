73b6561fd560d2a4a0160fd7250668d77d0c1991
algirdas <algirdas.grybauskas@yahoo.com>
2024-04-06 18:32:49 +0300

    Changing to reference atom site object in assign_hetatoms() step by step.

--- lib/ConnectAtoms.pm
+++ lib/ConnectAtoms.pm
@@ -424,7 +424,7 @@ sub assign_hetatoms
             for my $attribute ( 'label_seq_id', 'label_asym_id', 'label_alt_id',
                                 'pdbx_PDB_model_num'  ) {
                 $atom_site->{$last_atom_id}{$attribute} =
-                    $atom_site->{$connected_atom_id_2}{$attribute};
+                    $ref_atom_site->{$connected_atom_id_2}{$attribute};
             }
 
             $last_atom_id++;
