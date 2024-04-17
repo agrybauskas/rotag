ab23bb78a7dcabb91a2c375704fe64d08f1ff3c3
algirdas <algirdas.grybauskas@yahoo.com>
2024-04-17 15:56:29 +0300

    Adding additional test files for bendable_angles() and stretchable_bonds() for testing heteroatom usage.

---
+++ tests/cases/bendable_angles_003.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_dump_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-001.dump
+include_mainchain=0
+include_hetatoms=1
+
+"$(dirname "$0")"/../scripts/bendable_angles ${pdbx_dump_file} ${include_mainchain} ${include_hetatoms}
