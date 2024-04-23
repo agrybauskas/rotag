0b4f050d8f06656c482215fbbdbcdab068b4aed8
algirdas <algirdas.grybauskas@yahoo.com>
2024-04-23 18:22:01 +0300

    Bringing back the commented out code in rotag_add. Also, incorporating reference atom site into temporary residue site.

--- lib/ConnectAtoms.pm
+++ lib/ConnectAtoms.pm
@@ -411,6 +411,9 @@ sub assign_hetatoms
         }
 
         for my $related_atom_id_1 ( sort keys %{ $related_atom_site_1 } ) {
+            $atom_site->{$related_atom_id_1} =
+                $ref_atom_site->{$related_atom_id_1};
+
             replace_atom_site_ids( $atom_site,
                                    [ { 'from' => $related_atom_id_1,
                                        'to' => $last_atom_id } ],
