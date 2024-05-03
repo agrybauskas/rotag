611c95dd9176010991c5e67bf9b1b71ed7702a72
agrybauskas <algirdas.grybauskas@yahoo.com>
2024-05-03 12:38:21 +0300

    Fixed the wrong regex match in determine_residue_keys().

--- lib/PDBxParser.pm
+++ lib/PDBxParser.pm
@@ -1169,7 +1169,8 @@ sub determine_residue_keys
         my $residue_unique_keys = $residue_key_tree{$reduced_unique_key};
         if( scalar @{ $residue_unique_keys } > 1 ) {
             for my $i ( 0..$#{ $residue_unique_keys } ) {
-                if( $exclude_dot && $residue_unique_keys->[$i] =~ m/\.$/ ) {
+                if( $exclude_dot &&
+                    $residue_unique_keys->[$i]{'unique_residue_key'} =~ m/\.$/ ) {
                     splice @{ $residue_unique_keys }, $i, 1;
                     last;
                 }
