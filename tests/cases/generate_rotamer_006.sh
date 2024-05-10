12f9f2f23c5a1622b6589d1900f276ca13b6698e
agrybauskas <algirdas.grybauskas@yahoo.com>
2024-05-10 16:59:50 +0300

    Patched some generate_rotamer script's cases.

--- tests/cases/generate_rotamer_006.sh
+++ tests/cases/generate_rotamer_006.sh
@@ -3,7 +3,7 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/amino-acids/glutamic-acid-rotation-only-001.dump
-residue_unique_key="14,A,1,."
+residue_unique_key="14,A,1,.,59,A"
 angle_values="chi1 0 & chi2 pi & chi3 0"
 
 $(dirname "$0")/../scripts/generate_rotamer "${residue_unique_key}" \
