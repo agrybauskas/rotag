13791058ac14f14da81260feda931e3e15a83aa0
algirdas <algirdas.grybauskas@yahoo.com>
2024-06-09 12:55:48 +0300

    Storing assigned residue keys in generate_library() output.

--- lib/PseudoAtoms.pm
+++ lib/PseudoAtoms.pm
@@ -600,6 +600,9 @@ sub generate_library
                             { 'angles' => \%angles,
                               'potential' => $interactions,
                               'potential_energy_value' => $energy_sums->[$i],
+                              'terminal_residue_unique_key' => [
+                                  $residue_unique_key, @assigned_unique_keys,
+                              ],
                               ( $rmsd ? ( 'rmsd' => $rmsds->[$i][-1] ) : () ) };
                     }
                 }
