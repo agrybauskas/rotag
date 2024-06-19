2b7f2295def544316c28585a1433fa3f1b839f08
algirdas <algirdas.grybauskas@yahoo.com>
2024-06-19 21:29:28 +0300

    Added a test case to check if the heteroatom alternative positions are generated if this heteroatom is connected to mainchain.

--- tests/cases/rotag_scan_044.sh
+++ tests/cases/rotag_scan_044.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-002.cif
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-003.cif
 
-rotag_scan -H -a '120.0,CB-CG-OD1.MG=90.0' ${pdbx_file}
+rotag_scan -H -a '120.0,N.MG=1.0..0.5..1.5,CB-CG-OD1.MG=90.0' ${pdbx_file}
