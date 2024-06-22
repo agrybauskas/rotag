af731296eb4649f5bc5d18fecefb5a914235b2e8
algirdas <algirdas.grybauskas@yahoo.com>
2024-06-22 17:35:22 +0300

    Cleaned code looking at cpplint.

--- src/lib/PDBx.cc
+++ src/lib/PDBx.cc
@@ -2,11 +2,11 @@
 
 PDBx::PDBx(CIF* cif, std::vector<std::string> select_tags) {
   std::vector<std::string> cif_tags = {};
-  if(select_tags.size() > 0) {
+  if (select_tags.size() > 0) {
     cif_tags = select_tags;
   } else {
     char** tags = datablock_tags(cif_datablock_list(cif));
-    for(int i = 0; i < (int) sizeof(tags); i++ ) {
+    for (int i = 0; i < (int) sizeof(tags); i++) {
       std::string cif_tag = tags[i];
       cif_tags.push_back(cif_tag);
     }
@@ -19,15 +19,14 @@ PDBx::PDBx(CIF* cif, std::vector<std::string> select_tags) {
       const ssize_t cif_tag_index =
         datablock_tag_index(datablock, (char*) cif_tag.c_str());
 
-      for(ssize_t i = 0; i < cif_value_lengths[cif_tag_index]; i++) {
-        std::cout << datablock_cifvalue(datablock, cif_tag_index, i) << std::endl;
+      for (ssize_t i = 0; i < cif_value_lengths[cif_tag_index]; i++) {
+        datablock_cifvalue(datablock, cif_tag_index, i);
       }
     }
   }
 }
 
-PDBx::~PDBx(){}
+PDBx::~PDBx() {}
 
-void PDBx::values(std::string cif_tag){
-
+void PDBx::values(std::string cif_tag) {
 }
