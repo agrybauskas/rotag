0879d75a24a177b6bce8f31053c8c94f5488aeef
algirdas <algirdas.grybauskas@yahoo.com>
2024-06-22 22:21:58 +0300

    Retrieving loop markers from datablock.

--- src/lib/PDBx.cc
+++ src/lib/PDBx.cc
@@ -15,14 +15,18 @@ PDBx::PDBx(CIF* cif, std::vector<std::string> select_tags) {
     DATABLOCK* datablock;
     foreach_datablock(datablock, cif_datablock_list(cif)) {
         const ssize_t* cif_value_lengths = datablock_value_lengths(datablock);
+        const int* are_in_loops = datablock_in_loop(datablock);
         for (const std::string &cif_tag : cif_tags) {
             const ssize_t cif_tag_index =
                 datablock_tag_index(datablock,
                                     const_cast<char*>(cif_tag.c_str()));
+            const int is_in_loop = are_in_loops[cif_tag_index];
+            std::cout << cif_tag << std::endl;
+            std::cout << "    is_in_loop: " << is_in_loop << std::endl;
             for (ssize_t i = 0; i < cif_value_lengths[cif_tag_index]; i++) {
-                PDBXVALUE pdbx_value(datablock_cifvalue(datablock,
-                                                        cif_tag_index, i));
-                this->data[cif_tag].push_back(pdbx_value);
+                // PDBXVALUE pdbx_value(datablock_cifvalue(datablock,
+                //                                         cif_tag_index, i));
+                // this->data[cif_tag].push_back(pdbx_value);
             }
         }
     }
