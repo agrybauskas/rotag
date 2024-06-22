6a248f47d90fcb909f8e3e7ecfda2a26b0000ecb
algirdas <algirdas.grybauskas@yahoo.com>
2024-06-22 20:41:59 +0300

    Applied 4-space indentation to PDBx.

--- src/lib/PDBx.cc
+++ src/lib/PDBx.cc
@@ -1,29 +1,30 @@
 #include "PDBx.h"
 
 PDBx::PDBx(CIF* cif, std::vector<std::string> select_tags) {
-  std::vector<std::string> cif_tags = {};
-  if (select_tags.size() > 0) {
-    cif_tags = select_tags;
-  } else {
-    char** tags = datablock_tags(cif_datablock_list(cif));
-    for (int i = 0; i < static_cast<int>(sizeof(tags)); i++) {
-      std::string cif_tag = tags[i];
-      cif_tags.push_back(cif_tag);
+    std::vector<std::string> cif_tags = {};
+    if (select_tags.size() > 0) {
+        cif_tags = select_tags;
+    } else {
+        char** tags = datablock_tags(cif_datablock_list(cif));
+        for (int i = 0; i < static_cast<int>(sizeof(tags)); i++) {
+            std::string cif_tag = tags[i];
+            cif_tags.push_back(cif_tag);
+        }
     }
-  }
 
-  DATABLOCK* datablock;
-  foreach_datablock(datablock, cif_datablock_list(cif)) {
-    const ssize_t* cif_value_lengths = datablock_value_lengths(datablock);
-    for (const std::string &cif_tag : cif_tags) {
-      const ssize_t cif_tag_index =
-        datablock_tag_index(datablock, const_cast<char*>(cif_tag.c_str()));
-
-      for (ssize_t i = 0; i < cif_value_lengths[cif_tag_index]; i++) {
-        PDBXVALUE pdbx_value(datablock_cifvalue(datablock, cif_tag_index, i));
-      }
+    DATABLOCK* datablock;
+    foreach_datablock(datablock, cif_datablock_list(cif)) {
+        const ssize_t* cif_value_lengths = datablock_value_lengths(datablock);
+        for (const std::string &cif_tag : cif_tags) {
+            const ssize_t cif_tag_index =
+                datablock_tag_index(datablock,
+                                    const_cast<char*>(cif_tag.c_str()));
+            for (ssize_t i = 0; i < cif_value_lengths[cif_tag_index]; i++) {
+                PDBXVALUE pdbx_value(datablock_cifvalue(datablock,
+                                                        cif_tag_index, i));
+            }
+        }
     }
-  }
 }
 
 PDBx::~PDBx() {}
--- src/lib/PDBx.h
+++ src/lib/PDBx.h
@@ -7,42 +7,41 @@
 #include <map>
 
 extern "C" {
-  #include "cif.h"
-  #include "datablock.h"
+    #include "cif.h"
+    #include "datablock.h"
 }
 
 #include "PDBxParser.h"
 
 struct PDBXVALUE {
-  std::string value_str;
-  double value_float;
-  int64_t value_int;
+    std::string value_str;
+    double value_float;
+    int64_t value_int;
 
-  operator std::string () const { return value_str; }
-  operator double () const { return value_float; }
-  operator int64_t () const { return value_int; }
+    operator std::string () const { return value_str; }
+    operator double () const { return value_float; }
+    operator int64_t () const { return value_int; }
 
-  explicit PDBXVALUE(CIFVALUE* x) {
+    explicit PDBXVALUE(CIFVALUE* x) {
+        this->value_str = "A";
+        this->value_float = 1.0;
+        this->value_int = 2;
+    }
 
-    this->value_str = "A";
-    this->value_float = 1.0;
-    this->value_int = 2;
-  }
-
-  ~PDBXVALUE() {};
+    ~PDBXVALUE() {};
 };
 
 class PDBx {
  private:
-  std::map<std::string, std::vector<PDBXVALUE>> data;
-  std::map<std::string, std::vector<std::string>> categories;
-  std::map<std::string, std::vector<double>> category_order;
-  std::map<std::string, bool> loops;
+    std::map<std::string, std::vector<PDBXVALUE>> data;
+    std::map<std::string, std::vector<std::string>> categories;
+    std::map<std::string, std::vector<double>> category_order;
+    std::map<std::string, bool> loops;
 
  public:
-  explicit PDBx(CIF* cif, std::vector<std::string> select_tags = {});
-  ~PDBx();
-  void values(std::string cif_tag);
+    explicit PDBx(CIF* cif, std::vector<std::string> select_tags = {});
+    ~PDBx();
+    void values(std::string cif_tag);
 };
 
 #endif  // SRC_LIB_PDBX_H_
