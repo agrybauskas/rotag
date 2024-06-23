0cb3db79a5ac85c4c3a0c95799dcef9b87fc795b
algirdas <algirdas.grybauskas@yahoo.com>
2024-06-23 13:26:17 +0300

    Uncommented out that reads "_atom_site" and it does not produce errors for now.

--- src/lib/PDBxParser.cc
+++ src/lib/PDBxParser.cc
@@ -7,19 +7,19 @@ AtomSite pdbx_to_atom_site(char* pdbx_file_path) {
 
     cif_option_t compiler_options = cif_option_default();
     cexception_t inner;
-    // PDBx pdbx(new_cif_from_cif_file(pdbx_file_path, compiler_options, &inner),
-    //           ATOM_SITE_TAGS);
+    PDBx pdbx(new_cif_from_cif_file(pdbx_file_path, compiler_options, &inner),
+              ATOM_SITE_TAGS);
 
-    // for (size_t i = 0; i < pdbx.values("_atom_site.id").size(); i++) {
-    //     std::string id = pdbx.values("_atom_site.id")[i];
-    //     for (const std::string &cif_tag : ATOM_SITE_TAGS) {
-    //         // if (pdbx.values(cif_tag).size() > 0) {
-    //         //     std::cout << cif_tag << std::endl;
-    //         // //     atom_site[id][cif_tag] = value_scalar(datablock_cifvalue(
-    //         // //         datablock, cif_tag_index_lookup_table[cif_tag], i));
-    //         // }
-    //     }
-    // }
+    for (size_t i = 0; i < pdbx.values("_atom_site.id").size(); i++) {
+        std::string id = pdbx.values("_atom_site.id")[i];
+        for (const std::string &cif_tag : ATOM_SITE_TAGS) {
+            if (pdbx.values(cif_tag).size() > 0) {
+                // std::cout << cif_tag << std::endl;
+                // atom_site[id][cif_tag] = value_scalar(datablock_cifvalue(
+                //     datablock, cif_tag_index_lookup_table[cif_tag], i));
+            }
+        }
+    }
 
     return atom_site;
 }
