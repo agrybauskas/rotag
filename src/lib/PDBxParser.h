fe1ce8382a265b88237964c592c97a617de30444
algirdas <algirdas.grybauskas@yahoo.com>
2024-07-04 14:04:45 +0300

    Transfered comments to enumerators.

--- src/lib/PDBxParser.h
+++ src/lib/PDBxParser.h
@@ -14,53 +14,56 @@ extern "C" {
 
 #include "PDBx.h"
 
+struct ATOM_SITE_TAGS {
+
+};
+
 enum ATOM_SITE_TAG {
-    GROUP_PDB,
-    ID,
-    TYPE_SYMBOL,
-    LABEL_ATOM_ID,
-    LABEL_ALT_ID,
-    LABEL_COMP_ID,
-    LABEL_ASYM_ID,
-    LABEL_ENTITY_ID,
-    LABEL_SEQ_ID,
-    CARTN_X,
-    CARTN_Y,
-    CARTN_Z,
-    OCCUPANCY,
-    B_ISO_OR_EQUIV,
-    AUTH_SEQ_ID,
-    AUTH_COMP_ID,
-    AUTH_ASYM_ID,
-    AUTH_ATOM_ID,
-    PDBX_PDB_MODEL_NUM
+    GROUP_PDB,             // "ATOM" or "HETATM".
+    ID,                    // Atom id.
+    TYPE_SYMBOL,           // Chemical element.
+    LABEL_ATOM_ID,         // Atom label.
+    LABEL_ALT_ID,          // Related to alt. atom position.
+    LABEL_COMP_ID,         // Residue name.
+    LABEL_ASYM_ID,         // Chain name.
+    LABEL_ENTITY_ID,       // Molecular entity.
+    LABEL_SEQ_ID,          // Residue id.
+    CARTN_X,               // Cartesian x coordinates of the atom.
+    CARTN_Y,               // Cartesian y coordinates of the atom.
+    CARTN_Z,               // Cartesian z coordinates of the atom.
+    OCCUPANCY,             // The fraction present in the site.
+    B_ISO_OR_EQUIV,        // Isotropic displacement.
+    AUTH_SEQ_ID,           // Author's residue id.
+    AUTH_COMP_ID,          // Author's residue name.
+    AUTH_ASYM_ID,          // Author's chain name.
+    AUTH_ATOM_ID,          // Author's atom label.
+    PDBX_PDB_MODEL_NUM,    // Model id.
+    ROTAG_SELECTION_STATE, // Marks selection state: T, S ir H.
+    ROTAG_SELECTION_GROUP  // Selection group id.
 };
 
 const std::vector<std::string> ATOM_SITE_TAGS = {
-    // "_atom_site" category-related.
-    "_atom_site.group_pdb",              // "ATOM" or "HETATM".
-    "_atom_site.id",                     // Atom id.
-    "_atom_site.type_symbol",            // Chemical element.
-    "_atom_site.label_atom_id",          // Atom label.
-    "_atom_site.label_alt_id",           // Related to alt. atom position.
-    "_atom_site.label_comp_id",          // Residue name.
-    "_atom_site.label_asym_id",          // Chain name.
-    "_atom_site.label_entity_id",        // Molecular entity.
-    "_atom_site.label_seq_id",           // Residue id.
-    "_atom_site.cartn_x",                // Cartesian x coordinates of the atom.
-    "_atom_site.cartn_y",                // Cartesian y coordinates of the atom.
-    "_atom_site.cartn_z",                // Cartesian z coordinates of the atom.
-    "_atom_site.occupancy",              // The fraction present in the site.
-    "_atom_site.b_iso_or_equiv",         // Isotropic displacement.
-    "_atom_site.auth_seq_id",            // Author's residue id.
-    "_atom_site.auth_comp_id",           // Author's residue name.
-    "_atom_site.auth_asym_id",           // Author's chain name.
-    "_atom_site.auth_atom_id",           // Author's atom label.
-    "_atom_site.pdbx_pdb_model_num",     // Model id.
-
-    // Selection that is specific to rotag.
-    "_atom_site.rotag_selection_state",  // Marks selection state: T, S ir H.
-    "_atom_site.rotag_selection_group"   // Selection group id.
+    "_atom_site.group_pdb",
+    "_atom_site.id",
+    "_atom_site.type_symbol",
+    "_atom_site.label_atom_id",
+    "_atom_site.label_alt_id",
+    "_atom_site.label_comp_id",
+    "_atom_site.label_asym_id",
+    "_atom_site.label_entity_id",
+    "_atom_site.label_seq_id",
+    "_atom_site.cartn_x",
+    "_atom_site.cartn_y",
+    "_atom_site.cartn_z",
+    "_atom_site.occupancy",
+    "_atom_site.b_iso_or_equiv",
+    "_atom_site.auth_seq_id",
+    "_atom_site.auth_comp_id",
+    "_atom_site.auth_asym_id",
+    "_atom_site.auth_atom_id",
+    "_atom_site.pdbx_pdb_model_num",
+    "_atom_site.rotag_selection_state",
+    "_atom_site.rotag_selection_group"
 };
 
 typedef std::map<std::string, PDBXVALUE> Atom;
