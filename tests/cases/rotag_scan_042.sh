79d59d97b26b4609307221875501421c6271f9fd
agrybauskas <algirdas.grybauskas@yahoo.com>
2024-07-23 14:51:57 +0300

    Noticed empty test case. Removed it and renumbered the test scripts.

--- tests/cases/rotag_scan_042.sh
+++ tests/cases/rotag_scan_042.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/hetatoms/h2o-and-h2o-with-sidechains-with-connections-001.cif
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-004.cif
 
-rotag_scan -H -a '120.0,OD1.H1-O.H1=90.0' ${pdbx_file}
+rotag_scan -H -a 'N.MG=1.0..0.5..1.5' ${pdbx_file}
