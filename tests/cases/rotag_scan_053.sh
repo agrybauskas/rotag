50d08b151af99a9fd69ff3927d1120e56e42a9da
algirdas <algirdas.grybauskas@yahoo.com>
2024-07-23 22:46:47 +0300

    Changed the scanned bond parameters for few test cases.

--- tests/cases/rotag_scan_053.sh
+++ tests/cases/rotag_scan_053.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-007.cif
 
-rotag_scan -H -a 'C-CA-N.MG=90.0,CA-C-O.MG=90.0' ${pdbx_file}
+rotag_scan -H -a 'C-CA-N.MG=90.0,CB-CG-OD1.MG=90.0,CA-C-O.MG=90.0' ${pdbx_file}
