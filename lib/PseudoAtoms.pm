da9a38cfc48382cdef9b212981227fd4ba7f4425
algirdas <algirdas.grybauskas@yahoo.com>
2024-07-23 23:41:47 +0300

    Removed unnecessary connection retrievals in calc_favourable_angles().

--- lib/PseudoAtoms.pm
+++ lib/PseudoAtoms.pm
@@ -710,11 +710,7 @@ sub calc_favourable_angles
     my @visited_atom_ids = ( $ca_atom_id, $cb_atom_id );
     my @next_atom_ids =
         grep { $_ ne $ca_atom_id }
-             ( @{ $residue_site->{$cb_atom_id}{'connections'} },
-               @{ $include_hetatoms &&
-                  defined $residue_site->{$cb_atom_id}{'connections_hetatom'} ?
-                  $residue_site->{$cb_atom_id}{'connections_hetatom'} :
-                  [] } );
+             ( @{ $residue_site->{$cb_atom_id}{'connections'} } );
 
     my @allowed_bond_parameters;
     my @allowed_energies;
