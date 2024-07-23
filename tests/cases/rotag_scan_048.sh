e0f9a8928b2d6b6e233cd2292bf19435cf71609a
algirdas <algirdas.grybauskas@yahoo.com>
2024-07-23 21:40:23 +0300

    Added a test cases for scanning multiple MG positions around two different in the same residue.

---
+++ tests/cases/rotag_scan_048.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-006.cif
+
+rotag_scan -H -a 'N.MG=1.0..0.5..1.5,O.MG=1.0..0.5..1.5' ${pdbx_file}
---
+++ tests/cases/rotag_scan_049.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-006.cif
+
+rotag_scan -H -a 'CA-N.MG=90.0,C-O.MG=90.0' ${pdbx_file}
