226c4931315d67a9eb17b287acf49b43c3578ab3
algirdas <algirdas.grybauskas@yahoo.com>
2024-08-02 23:47:17 +0300

    Uncommented code parts in PDBx lib that probably will be kept.

--- src/lib/PDBx.h
+++ src/lib/PDBx.h
@@ -12,68 +12,68 @@ extern "C" {
     #include "datablock.h"
 }
 
-// struct PDBXVALUE {
-//     std::string value_str;
-//     double value_float;
-//     int64_t value_int;
+struct PDBXVALUE {
+    std::string value_str;
+    double value_float;
+    int64_t value_int;
 
-//     operator std::string () const { return value_str; }
-//     operator double () const { return value_float; }
-//     operator int64_t () const { return value_int; }
+    operator std::string () const { return value_str; }
+    operator double () const { return value_float; }
+    operator int64_t () const { return value_int; }
 
-//     explicit PDBXVALUE(std::string value) {
-//         this->value_str = value;
-//     }
+    explicit PDBXVALUE(std::string value) {
+        this->value_str = value;
+    }
 
-//     explicit PDBXVALUE(double value) {
-//         this->value_float = value;
-//         this->value_str = std::to_string(value);
-//     }
+    explicit PDBXVALUE(double value) {
+        this->value_float = value;
+        this->value_str = std::to_string(value);
+    }
 
-//     explicit PDBXVALUE(int64_t value) {
-//         this->value_int = value;
-//         this->value_str = std::to_string(value);
-//     }
+    explicit PDBXVALUE(int64_t value) {
+        this->value_int = value;
+        this->value_str = std::to_string(value);
+    }
 
-//     explicit PDBXVALUE(CIFVALUE* cif_value) {
-//         cif_value_type_t type = value_type(cif_value);
-//         switch (type) {
-//             case CIF_INT:
-//                 this->value_int = atoi(value_scalar(cif_value));
-//                 this->value_str = value_scalar(cif_value);
-//                 break;
-//             case CIF_FLOAT:
-//                 this->value_float = atof(value_scalar(cif_value));
-//                 this->value_str = value_scalar(cif_value);
-//                 break;
-//             case CIF_UQSTRING:
-//             case CIF_SQSTRING:
-//             case CIF_DQSTRING:
-//             case CIF_SQ3STRING:
-//             case CIF_DQ3STRING:
-//             case CIF_TEXT:
-//                 this->value_str = value_scalar(cif_value);
-//                 break;
-//             // HACK: look at these cases: CIF_UNKNOWN, CIF_NON_EXISTANT,
-//             // CIF_LIST, CIF_TABLE, last_CIF_VALUE.
-//             default:
-//                 this->value_str = "";
-//                 break;
-//         }
-//     }
-// };
+    explicit PDBXVALUE(CIFVALUE* cif_value) {
+        cif_value_type_t type = value_type(cif_value);
+        switch (type) {
+            case CIF_INT:
+                this->value_int = atoi(value_scalar(cif_value));
+                this->value_str = value_scalar(cif_value);
+                break;
+            case CIF_FLOAT:
+                this->value_float = atof(value_scalar(cif_value));
+                this->value_str = value_scalar(cif_value);
+                break;
+            case CIF_UQSTRING:
+            case CIF_SQSTRING:
+            case CIF_DQSTRING:
+            case CIF_SQ3STRING:
+            case CIF_DQ3STRING:
+            case CIF_TEXT:
+                this->value_str = value_scalar(cif_value);
+                break;
+            // HACK: look at these cases: CIF_UNKNOWN, CIF_NON_EXISTANT,
+            // CIF_LIST, CIF_TABLE, last_CIF_VALUE.
+            default:
+                this->value_str = "";
+                break;
+        }
+    }
+};
 
 class PDBx {
  private:
-    // std::map<std::string, std::vector<PDBXVALUE>> data;
-    // std::map<std::string, int64_t> cif_tag_order;
-    // std::map<std::string, int64_t> in_loop;
+    std::map<std::string, std::vector<PDBXVALUE>> data;
+    std::map<std::string, int64_t> cif_tag_order;
+    std::map<std::string, int64_t> in_loop;
 
  public:
     explicit PDBx(CIF* cif, std::vector<std::string> select_tags);
 
-    // std::vector<PDBXVALUE> values(std::string cif_tag);
-    // PDBXVALUE value(std::string, size_t);
+    std::vector<PDBXVALUE> values(std::string cif_tag);
+    PDBXVALUE value(std::string, size_t);
 };
 
 #endif  // SRC_LIB_PDBX_H_
