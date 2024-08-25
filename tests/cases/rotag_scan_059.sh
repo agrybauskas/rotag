e1b322e7c6fae7719fec45ff19936c27f2ac41da
algirdas <algirdas.grybauskas@yahoo.com>
2024-08-25 08:55:43 +0300

    Fixed inputs of rotag_scan parameters.

--- tests/cases/rotag_scan_059.sh
+++ tests/cases/rotag_scan_059.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/2bama-active-site-with-ca.cif
 
-rotag_scan -H -a '120.0,OD1.H1=3.0..1.0..4.0' ${pdbx_file}
+rotag_scan -H -a '120.0,OH.CA=3.0..1.0..4.0' ${pdbx_file}
