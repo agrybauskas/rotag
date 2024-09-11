f5d228964457633abf708ff5752ed4be15c04765
agrybauskas <algirdas.grybauskas@yahoo.com>
2024-09-11 11:46:17 +0300

    I will be trying to implement fixed values for bond parameters in CLI rotag_scan option.

--- tests/cases/rotag_scan_054.sh
+++ tests/cases/rotag_scan_054.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/ca-with-sidechains-with-connections-001.cif
 
-rotag_scan -H -a 'CB-CG-OD1.CA=90.0' ${pdbx_file}
+rotag_scan -H -a '*:*=!; CB-CG-OD1.CA=90.0' ${pdbx_file}
