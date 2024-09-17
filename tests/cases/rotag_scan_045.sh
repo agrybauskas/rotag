c14080254727927168c5a9624328ac1d391889cc
algirdas <algirdas.grybauskas@yahoo.com>
2024-09-17 23:07:28 +0300

    Updated test case query for rotag_scan in order to test the usage of bond parameters for heteroatoms that interact with mainchain atoms.

--- tests/cases/rotag_scan_045.sh
+++ tests/cases/rotag_scan_045.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-005.cif
 
-rotag_scan -H -a 'O.MG=1.0..0.5..1.5' ${pdbx_file}
+rotag_scan -H -a '!; O.MG=1.0..0.5..1.5' ${pdbx_file}
