27ef146df60fc1a1bf8dba92bfef26b0ef671e37
algirdas <algirdas.grybauskas@yahoo.com>
2024-09-22 10:32:04 +0300

    Patched test cases and outputs after sampling function change.

--- tests/cases/sample_bond_parameters_qs_parsing_010.sh
+++ tests/cases/sample_bond_parameters_qs_parsing_010.sh
@@ -2,4 +2,4 @@
 
 export PERL5LIB=$(dirname "$0")/../../lib
 
-$(dirname "$0")/../scripts/sample_bond_parameters_qs_parsing 'GLN:36.0'
+$(dirname "$0")/../scripts/sample_bond_parameters_qs_parsing 'GLN:..36.0..'
