e85a444aceb7cbdc61ba17d43ca8e82f16bf9408
algirdas <algirdas.grybauskas@yahoo.com>
2024-09-26 11:28:33 +0300

    Changed abstract dihedral angle naming from * to *-*-*-*. Not all the test cases now get passed.

--- lib/Sampling.pm
+++ lib/Sampling.pm
@@ -122,9 +122,6 @@ sub sample_bond_parameters_qs_parsing
     for my $residue_name ( sort keys %{ $dihedral_angle_restraints } ) {
         for my $dihedral_angle_name (
             sort keys %{ $dihedral_angle_restraints->{$residue_name} } ) {
-            if( $dihedral_angle_name eq '*' ) {
-                $dihedral_angle_name = '*-*-*-*';
-            }
             my ( $dihedral_angle_start,
                  $dihedral_angle_step,
                  $dihedral_angle_end ) =
@@ -312,9 +309,9 @@ sub retrieve_dihedral_angle_params
         my $residue_specific =
             $dihedral_angle_restraints->{$residue_name}{'.'}{$param};
         my $nonspecific =
-            $dihedral_angle_restraints->{'*'}{'*'}{$param};
+            $dihedral_angle_restraints->{'*'}{'*-*-*-*'}{$param};
 
-        if( defined $angle_specific && $angle_specific ne '*' ) {
+        if( defined $angle_specific && $angle_specific ne '*-*-*-*' ) {
             $params{$param} = $angle_specific;
         } elsif( defined $residue_specific && $residue_specific ne '*' ) {
             $params{$param} = $residue_specific;
