1325c1c8325b10690da2f01136fd5ae440273a10
algirdas <algirdas.grybauskas@yahoo.com>
2024-10-16 22:20:17 +0300

    Now, only defined by user bond parameters are overwritten.

--- lib/Sampling.pm
+++ lib/Sampling.pm
@@ -361,13 +361,26 @@ sub resolve_bond_parameters
                 for my $alt_bond_parameter_name (
                     uniq( $bond_parameter_name, @{ $alt_bond_parameter_names } ) ) {
                     for my $parameter_key ( 'from', 'step', 'to' ) {
-                        next if ! exists $bond_parameters->{$_} ||
-                                ! exists $bond_parameters->{$_}{$alt_bond_parameter_name} ||
-                                ! exists $bond_parameters->{$_}{$alt_bond_parameter_name}{$parameter_key} ||
-                                $bond_parameters->{$residue_name}{$bond_parameter_name}{$parameter_key} ne '*' ||
-                                $bond_parameters->{$_}{$alt_bond_parameter_name}{$parameter_key} eq '*';
-                        $bond_parameters->{$residue_name}{$bond_parameter_name}{$parameter_key} =
-                            $bond_parameters->{$_}{$alt_bond_parameter_name}{$parameter_key};
+                        if( exists $bond_parameters->{$_} &&
+                            exists $bond_parameters->{$_}{$alt_bond_parameter_name} &&
+                            exists $bond_parameters->{$_}{$alt_bond_parameter_name}{$parameter_key} &&
+                            $bond_parameters->{$_}{$alt_bond_parameter_name}{$parameter_key} ne '*' &&
+                            $bond_parameters->{$_}{$bond_parameter_name}{$parameter_key} eq '*' ) {
+                            $bond_parameters->{$_}{$bond_parameter_name}{$parameter_key} =
+                                $bond_parameters->{$_}{$alt_bond_parameter_name}{$parameter_key};
+                            next;
+                        }
+
+                        if( exists $default_bond_parameters{$_} &&
+                            exists $default_bond_parameters{$_}{$alt_bond_parameter_name} &&
+                            exists $default_bond_parameters{$_}{$alt_bond_parameter_name}{$parameter_key} &&
+                            $default_bond_parameters{$_}{$alt_bond_parameter_name}{$parameter_key} ne '*' &&
+                            $bond_parameters->{$_}{$bond_parameter_name}{$parameter_key} eq '*' ) {
+                            $bond_parameters->{$_}{$bond_parameter_name}{$parameter_key} =
+                                $default_bond_parameters{$_}{$alt_bond_parameter_name}{$parameter_key};
+                            next;
+                        }
+
                     }
 
                     if( exists $bond_parameters->{$_} &&
