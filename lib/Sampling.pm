fbb4fa721e6fc40c9920c6521c29ba3407ed16ff
algirdas <algirdas.grybauskas@yahoo.com>
2024-10-16 16:26:54 +0300

    Moved step variable check to determine_bond_parameter_values().

--- lib/Sampling.pm
+++ lib/Sampling.pm
@@ -237,9 +237,7 @@ sub sample_bond_parameters_qs_parsing
                     'from' => $bond_parameter_start,
                     'step' => $bond_parameter_step,
                     'to' => $bond_parameter_end,
-                    'values' => (
-                        $bond_parameter_step < 0 ? [] : $bond_parameter_values
-                    ),
+                    'values' => $bond_parameter_values,
                     'type' => $bond_parameter_type,
                     'units' => (
                         $bond_parameter_type eq 'bond_length' ?
@@ -284,6 +282,8 @@ sub determine_bond_parameter_values
 {
     my ( $parameters, $bond_parameter_name, $bond_parameter_start,
          $bond_parameter_step, $bond_parameter_end, $options ) = @_;
+
+    return [] if $bond_parameter_step =~ m/^-\d+$/g;
 
     return if $bond_parameter_start eq '*' ||
               $bond_parameter_step eq '*' ||
