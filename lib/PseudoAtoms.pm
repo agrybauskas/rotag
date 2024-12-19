9cb906377db06632d46dfd2cb423655366996214
algirdas <algirdas.grybauskas@yahoo.com>
2024-12-19 16:04:32 +0200

    Made small refactor change.

--- lib/PseudoAtoms.pm
+++ lib/PseudoAtoms.pm
@@ -397,8 +397,8 @@ sub generate_library
         my $current_atom_site =
             filter_new( $ref_atom_site,
                     { 'include' =>
-                          {'id' => $atom_site_groups->{$atom_site_identifier}
-                                                      {'atom_ids'} } } );
+                          { 'id' => $atom_site_groups->{$atom_site_identifier}
+                                                       {'atom_ids'} } } );
 
         connect_atoms( $parameters, $current_atom_site );
         hybridization( $parameters, $current_atom_site );
