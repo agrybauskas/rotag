d8d6d53fb45e58d4e1ac93360aa9e6fc44e79bee
algirdas <algirdas.grybauskas@yahoo.com>
2024-12-27 22:46:27 +0200

    Added code to make sure that rotamer extensions do not go back to the other residues in assign_hetatoms().

--- lib/ConnectAtoms.pm
+++ lib/ConnectAtoms.pm
@@ -392,12 +392,13 @@ sub assign_hetatoms
                 my $connection_unique_key =
                     unique_residue_key( $ref_atom_site->{$connection_atom_id} );
 
-                next if $ref_atom_site->{$connection_atom_id}{'group_PDB'} eq 'ATOM' &&
-                    ! exists $unique_residue_keys->{$connection_unique_key};
+                next if $ref_atom_site->{$connection_atom_id}{'group_PDB'} eq 'ATOM';
 
                 # Clones, assigns proper and next atom ids.
                 for my $connection_related_atom_id (
                     sort @{ $all_unique_residue_keys->{$connection_unique_key} } ) {
+                    next if $ref_atom_site->{$connection_related_atom_id}{'group_PDB'} eq 'ATOM';
+
                     $atom_site->{$connection_related_atom_id} =
                         clone $ref_atom_site->{$connection_related_atom_id};
 
