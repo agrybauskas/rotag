c9f50139b2107e5b5bd0d92db6852cae9c69828f
algirdas <algirdas.grybauskas@yahoo.com>
2025-01-06 02:27:02 +0200

    Refactored and simplified determine_bond_parameter_values().

--- lib/Sampling.pm
+++ lib/Sampling.pm
@@ -269,38 +269,29 @@ sub determine_bond_parameter_values
         $bond_parameter_count++;
     }
 
-    my $values;
-    if( $bond_parameter_type eq 'dihedral_angle' && $in_radians ) {
-        $values = sample_bond_parameters(
-            [ [ $bond_parameter_start,
-                $bond_parameter_end ] ],
-            $bond_parameter_count, 1, 0
-        );
-    } elsif( $bond_parameter_type eq 'bond_angle' && $in_radians ) {
-        $values = sample_bond_parameters(
-            [ [ $bond_parameter_start,
-                $bond_parameter_end ] ],
-            $bond_parameter_count, 1, 1
-        );
-    } elsif( $bond_parameter_type eq 'dihedral_angle' ) {
-        $values = sample_bond_parameters(
-            [ [ $bond_parameter_start * $pi / 180.0,
-                $bond_parameter_end * $pi / 180.0 ] ],
-            $bond_parameter_count, 1, 0
-        );
-    } elsif( $bond_parameter_type eq 'bond_angle' ) {
-        $values = sample_bond_parameters(
-            [ [ $bond_parameter_start * $pi / 180.0,
-                $bond_parameter_end * $pi / 180.0 ] ],
-            $bond_parameter_count, 1, 1
-        );
-    } else {
-        $values = sample_bond_parameters(
-            [ [ $bond_parameter_start,
-                $bond_parameter_end ] ],
-            $bond_parameter_count, 1, 1
-        );
+    my $inclusive_start = 1;
+    my $inclusive_end = 1;
+
+    if( $bond_parameter_type eq 'dihedral_angle' ) {
+        $inclusive_end = 0;
     }
+
+    my $start_value;
+    my $end_value;
+    if( ( $bond_parameter_type eq 'dihedral_angle' ||
+          $bond_parameter_type eq 'bond_angle' ) &&
+        ! $in_radians ) {
+        $start_value = $bond_parameter_start * $pi / 180.0;
+        $end_value = $bond_parameter_end * $pi / 180.0;
+    } else {
+        $start_value = $bond_parameter_start;
+        $end_value = $bond_parameter_end;
+    }
+
+    my $values = sample_bond_parameters(
+        [ [ $start_value, $end_value ] ],
+        $bond_parameter_count, $inclusive_start, $inclusive_end
+    );
 
     return $values;
 }
