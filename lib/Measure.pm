d87a22e76400eb0ec8360f965fc2547a01884cb5
algirdas <algirdas.grybauskas@yahoo.com>
2025-01-14 11:34:02 +0200

    Added "group_PDB" for filtering in rmsd_sidechains() as heteroatoms will be also added.

--- lib/Measure.pm
+++ lib/Measure.pm
@@ -358,8 +358,8 @@ sub rmsd_sidechains
     $average //= 0;
     $best_case //= 0;
     $include_atoms //= $parameters->{'_[local]_sidechain_atom_names'};
-    $include_heteroatoms //= [];
-        # [ keys %{ $parameters->{'_[local]_sidechain_hetatom_extension'} } ];
+    $include_heteroatoms //=
+        [ keys %{ $parameters->{'_[local]_sidechain_hetatom_extension'} } ];
     # HACK: grep'ing by first symbol is not robust, because the first symbol of
     # atom name sometimes can differ from the type symbol.
     $exclude_atoms //=
@@ -374,7 +374,9 @@ sub rmsd_sidechains
 
     my @atom_names_include = [];
     push @atom_names_include, @{ $include_atoms } if $include_atoms;
-    push @atom_names_include, @{ $include_heteroatoms } if $include_heteroatoms;
+
+    my @hetatom_names_include = [];
+    push @hetatom_names_include, @{ $include_heteroatoms } if $include_heteroatoms;
 
     my @atom_names_exclude;
     push @atom_names_exclude, @{ $exclude_atoms } if $exclude_atoms;
@@ -390,7 +392,8 @@ sub rmsd_sidechains
         my $first_sidechain_data =
             filter( { 'atom_site' => $first_atom_site,
                       'include' =>
-                          { 'label_seq_id' => [ $first_residue_id ],
+                          { 'group_PDB' => [ 'ATOM' ],
+                            'label_seq_id' => [ $first_residue_id ],
                             'label_asym_id' => [ $first_chain ],
                             'pdbx_PDB_model_num' => [ $first_pdbx_model_num ],
                             'label_alt_id' => [ $first_alt_id ],
@@ -425,7 +428,8 @@ sub rmsd_sidechains
             my $second_sidechain_data =
                 filter( { 'atom_site' => $second_atom_site,
                           'include' =>
-                              { 'label_seq_id' => [ $second_residue_id ],
+                              { 'group_PDB' => [ 'ATOM' ],
+                                'label_seq_id' => [ $second_residue_id ],
                                 'label_asym_id' => [ $second_chain ],
                                 'pdbx_PDB_model_num' => [ $second_pdbx_model_num ],
                                 'label_alt_id' => [ $second_alt_id ],
