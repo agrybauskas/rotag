4c41c980c83377b44a74683f83534dacab91d567
algirdas <algirdas.grybauskas@yahoo.com>
2025-01-22 15:06:59 +0200

    Trying to include heteroatom from the start in assign_hetatoms().

--- lib/ConnectAtoms.pm
+++ lib/ConnectAtoms.pm
@@ -354,9 +354,9 @@ sub assign_hetatoms
     if( @{ $filter_unique_keys } ) {
         my %filter_unique_keys = map { $_ => 1 } @{ $filter_unique_keys };
         $unique_residue_keys = {
-            map { $_ => $unique_residue_keys->{$_} }
+            map { $_ => $all_unique_residue_keys->{$_} }
             grep { exists $filter_unique_keys{$_} }
-            keys %{ $unique_residue_keys }
+            keys %{ $all_unique_residue_keys }
         };
     }
 
