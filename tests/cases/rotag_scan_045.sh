c8f23d181248bdbd83182dcb663701fe4a0c81a4
algirdas <algirdas.grybauskas@yahoo.com>
2025-01-25 13:44:04 +0200

    Removed passing stderr to stdout in the test case.

--- tests/cases/rotag_scan_045.sh
+++ tests/cases/rotag_scan_045.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/mg-with-sidechains-with-connections-005.cif
 
-rotag_scan -H -a '!; O.MG=3.0..0.5..4.5' ${pdbx_file} 2>&1
+rotag_scan -H -a '!; O.MG=3.0..0.5..4.5' ${pdbx_file}
