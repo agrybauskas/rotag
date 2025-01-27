2fc9cdb4b6fe37f1b0035cbea397f7d2f848cdc7
algirdas <algirdas.grybauskas@yahoo.com>
2025-01-27 13:49:54 +0200

    Started a test files for assign_hetatoms_mainchain().

---
+++ tests/cases/assign_hetatoms_mainchain_001.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PERL5LIB=$(dirname "$0")/../../lib
+
+pdbx_struct_conn_dump_file=$(dirname "$0")/../inputs/amino-acids/mg-with-sidechains-without-connections-and-struct-conn-002.dump
+assume_connections=1
+
+$(dirname "$0")/../scripts/assign_hetatoms_mainchain \
+    ${pdbx_struct_conn_dump_file} ${assume_connections} \
+    | sort -k 1 -n
