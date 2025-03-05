17a98286bd51ac66262b728638a3da27f4b93647
algirdas <algirdas.grybauskas@yahoo.com>
2025-03-05 15:20:27 +0200

    Trying to reduce the quantity of the bond parameters in order for the test case not to run for so long.

--- tests/cases/rotag_scan_062.sh
+++ tests/cases/rotag_scan_062.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/2qty-multiple-mg-001.cif
 
-rotag_scan -H -b '..120.0..;*.MG=1.2..0.5..3.2;*-*-*.MG=-180.0..18.0..180.0;*-*.MG=81.1..24.7..179.9' ${pdbx_file}
+rotag_scan -H -b '..120.0..;*.MG=2.2..1.0..3.2;*-*-*.MG=-180.0..72.0..180.0;*-*.MG=81.1..49.4..179.9' ${pdbx_file}
