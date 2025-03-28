40f796d94ed5bf4385248afd0188f23d6e9b82d5
agrybauskas <algirdas.grybauskas@yahoo.com>
2025-03-28 14:16:42 +0200

    Added test cases that use previously create test inputs with interactions between multiple side-chain atoms and heteroatoms.

--- tests/cases/rotag_scan_064.sh
+++ tests/cases/rotag_scan_064.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/hetatoms/2qty-multiple-mg-002.cif
+pdbx_file=$(dirname "$0")/../inputs/hetatoms/2qty-multiple-mg-004.cif
 
 rotag_scan -H -b '..120.0..;*.MG=2.2..1.0..3.2;*-*-*.MG=-180.0..72.0..180.0;*-*.MG=81.1..49.4..179.9' ${pdbx_file}
