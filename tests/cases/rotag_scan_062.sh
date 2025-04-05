f3e08d6a25816bf5f1771a6a1d1e39ff023a4d98
algirdas <algirdas.grybauskas@yahoo.com>
2025-04-05 12:40:59 +0300

    Adjusted cutoff value in order to get results.

--- tests/cases/rotag_scan_062.sh
+++ tests/cases/rotag_scan_062.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/2qty-multiple-mg-002.cif
 
-rotag_scan -H -b '..120.0..;*.MG=2.2..1.0..3.2;*-*-*.MG=-180.0..72.0..180.0;*-*.MG=81.1..49.4..179.9' ${pdbx_file}
+rotag_scan -H -p 'cutoff_atom=115.0' -b '..120.0..;*.MG=2.2..1.0..3.2;*-*-*.MG=-180.0..144.0..180.0;*-*.MG=81.1..49.4..179.9' ${pdbx_file}
