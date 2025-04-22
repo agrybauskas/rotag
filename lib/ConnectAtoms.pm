02adbc29220146ca03d0efa5a774f63e52d49961
Algirdas <algirdas.grybauskas@yahoo.com>
2025-04-22 14:22:31 +0300

    Using BondPath to retrieve all the related connections.

--- lib/ConnectAtoms.pm
+++ lib/ConnectAtoms.pm
@@ -1104,7 +1104,6 @@ sub connections_from_atom_site
     $ref_atom_site //= $atom_site;
     $struct_conn //= {};
 
-
     my $unique_residue_keys =
         unique_from_struct_conn( $atom_site, $struct_conn,
                                  { 'no_hetatoms' => 1 } );
@@ -1117,26 +1116,17 @@ sub connections_from_atom_site
                                       'start_atom_ids' => \@start_atom_ids,
                                       'include_hetatoms' => 1 } );
 
-    # use Data::Dumper;
-    # print STDERR Dumper "Call";
-    # for my $unique_residue_key ( sort keys %{ $unique_residue_keys } ) {
-    #     print STDERR Dumper $unique_residue_key;
-    #     my @next_atom_ids = @{ $unique_residue_keys->{$unique_residue_key} };
+    for my $unique_residue_key ( sort keys %{ $unique_residue_keys } ) {
+        my @next_atom_ids = @{ $unique_residue_keys->{$unique_residue_key} };
 
-    #     my %visited_atoms = ();
-    #     while( @next_atom_ids ) {
-    #         my ( $atom_id ) = shift @next_atom_ids;
+        my %visited_atoms = ();
+        while( @next_atom_ids ) {
+            my ( $atom_id ) = shift @next_atom_ids;
 
-    #         next if $visited_atoms{$atom_id};
-    #         $visited_atoms{$atom_id} = 1;
-
-    #         print STDERR Dumper $atom_site->{$atom_id}{'label_atom_id'};
-
-
-    #     }
-
-    #     print STDERR Dumper "-----------------------";
-    # }
+            next if $visited_atoms{$atom_id};
+            $visited_atoms{$atom_id} = 1;
+        }
+    }
 }
 
 
