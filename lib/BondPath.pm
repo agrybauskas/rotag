9194bbfa0afafb9c81d9fa13d750263c9c95214a
Algirdas <algirdas.grybauskas@yahoo.com>
2025-04-23 22:58:10 +0300

    Removed selection of only one residue in BondPath object.

--- lib/BondPath.pm
+++ lib/BondPath.pm
@@ -38,8 +38,7 @@ sub new
     my @next_atom_ids =
         grep { $atom_site->{$_}{'group_PDB'} ne 'HETATM' }
         grep { defined $_ }
-             ( shift @{ sort_by_unique_residue_key( $start_atom_ids,
-                                                    $atom_site ) } );
+             ( @{ sort_by_unique_residue_key( $start_atom_ids, $atom_site ) } );
 
     my %bond_paths = ();
     my $atom_order_idx = 1;
