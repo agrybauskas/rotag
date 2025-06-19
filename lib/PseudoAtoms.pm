d6a6bb93330a77b1c8de56b8f8306a95ff44669f
Algirdas <algirdas.grybauskas@yahoo.com>
2025-06-19 12:08:27 +0300

    Removed unnecessary hash and used the existing one.

--- lib/PseudoAtoms.pm
+++ lib/PseudoAtoms.pm
@@ -862,7 +862,6 @@ sub calc_favourable_angles
 
             my $unique_residue_key =
                 unique_residue_key( $residue_site->{$atom_id} );
-            my %no_used_existing_library = ();
             for my $parameter_name ( @parameter_names_sorted ) {
                 next if exists $visited_bond_parameters{$parameter_name};
 
@@ -886,8 +885,6 @@ sub calc_favourable_angles
                                                          {$parameter_key}
                                                          {'energy_combinations'};
                     next;
-                } else {
-                    $no_used_existing_library{$parameter_name} = 1;
                 }
 
                 my @default_allowed_bond_parameters =
@@ -957,8 +954,8 @@ sub calc_favourable_angles
 
             if( defined $min_max_ratio &&
                 @{ $next_allowed_bond_parameters } &&
-                any { exists $no_used_existing_library{$_} }
-                    @parameter_names_sorted ) {
+                exists $existing_bond_parameters->{$unique_residue_key}
+                                                  {$parameter_key_sorted} ) {
                 my @energy_values_sorted =
                     sort { $a->[0] <=> $b->[0] } @{ $next_allowed_energies };
 
