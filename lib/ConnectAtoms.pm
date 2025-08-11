35855240cf2d2124e59f8aba58fe3cfe582ac8f1
Algirdas <algirdas.grybauskas@yahoo.com>
2025-08-11 10:20:32 +0300

    Renamed alt id tracking variables in assign_hetatoms().

--- lib/ConnectAtoms.pm
+++ lib/ConnectAtoms.pm
@@ -338,14 +338,14 @@ sub assign_hetatoms
 {
     my ( $parameters, $atom_site, $struct_conn, $options ) = @_;
     my ( $sidechains_only, $heteroatom_depth_limit, $exclude_hetatom_connections,
-         $ref_atom_site, $filter_unique_keys, $track_alt_ids, $last_atom_id,
+         $ref_atom_site, $filter_unique_keys, $tracked_alt_ids, $last_atom_id,
          $keep_original ) =
         ( $options->{'sidechains_only'},
           $options->{'heteroatom_depth'},
           $options->{'exclude_hetatom_connections'},
           $options->{'ref_atom_site'},
           $options->{'filter_unique_keys'},
-          $options->{'track_alt_ids'},
+          $options->{'tracked_alt_ids'},
           $options->{'last_atom_id'},
           $options->{'keep_original'} );
 
@@ -355,7 +355,7 @@ sub assign_hetatoms
     $exclude_hetatom_connections //= {};
     $ref_atom_site //= $atom_site;
     $filter_unique_keys //= [];
-    $track_alt_ids //= {};
+    $tracked_alt_ids //= {};
     $last_atom_id //= max( keys %{ $ref_atom_site } );
     $last_atom_id++;
     # HACK: the default should be 0 as it is more intuitive.
@@ -405,7 +405,6 @@ sub assign_hetatoms
     my %sidechain_atom_names =
         map { $_ => 1 } @{ $parameters->{'_[local]_sidechain_atom_names'} };
 
-    my %tracked_alt_ids = ();
     for my $unique_residue_key ( sort keys %{ $unique_residue_keys } ) {
         my @next_atom_ids = @{ $unique_residue_keys->{$unique_residue_key} };
 
@@ -445,13 +444,13 @@ sub assign_hetatoms
                 my $connection_unique_key =
                     unique_residue_key( $ref_atom_site->{$connection_atom_id} );
 
-                if( ! exists $tracked_alt_ids{$connection_unique_key} ) {
-                    ( $tracked_alt_ids{$connection_unique_key} ) =
+                if( ! exists $tracked_alt_ids->{$connection_unique_key} ) {
+                    ( $tracked_alt_ids->{$connection_unique_key} ) =
                         max( grep { $_ ne '.' }
                              map { $ref_atom_site->{$_}{'label_alt_id'} }
                              keys %{ filter_by_unique_residue_key( $ref_atom_site,
                                                                    $connection_unique_key ) } );
-                    $tracked_alt_ids{$connection_unique_key} //= 1;
+                    $tracked_alt_ids->{$connection_unique_key} //= 1;
                 }
 
                 # Clones, assigns proper and next atom ids.
@@ -473,7 +472,7 @@ sub assign_hetatoms
                     $tracked_atom_ids{$connection_related_atom_id} = $last_atom_id;
 
                     $atom_site->{$last_atom_id}{'label_alt_id'} =
-                        $tracked_alt_ids{$connection_unique_key};
+                        $tracked_alt_ids->{$connection_unique_key};
                     $atom_site->{$last_atom_id}{'origin_atom_id'} =
                         $connection_related_atom_id;
 
@@ -536,7 +535,7 @@ sub assign_hetatoms
                     }
                 }
 
-                $tracked_alt_ids{$connection_unique_key}++;
+                $tracked_alt_ids->{$connection_unique_key}++;
             }
         }
 
