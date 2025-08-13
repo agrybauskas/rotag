cfca03a4ecbb859bb0a25096db52f7dec46f76c0
Algirdas <algirdas.grybauskas@yahoo.com>
2025-08-13 16:24:45 +0300

    Changed test input and fixed it's mistake regarding bond parameter names.

--- tests/cases/rotag_add_036.sh
+++ tests/cases/rotag_add_036.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/libraries/k-with-sidechains-with-connections-library-002.cif
+pdbx_file=$(dirname "$0")/../inputs/libraries/cd-with-sidechains-with-connections-library-001.cif
 
 rotag_add -k -S ${pdbx_file}
