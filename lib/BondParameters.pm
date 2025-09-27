b130672bf6cce773058ded19b64f68e1e8ac3ba4
Algirdas <algirdas.grybauskas@yahoo.com>
2025-09-27 11:11:10 +0300

    Renamed data structure's key for clarity purposes.

--- lib/BondParameters.pm
+++ lib/BondParameters.pm
@@ -239,8 +239,9 @@ sub rotatable_bonds
                 'order' => $bond_order{$bond_atom_ids->[1]}{$bond_atom_ids->[2]},
                 'rank' => 1,
                 'atom_ids' => $bond_atom_ids,
-                'atom_ids_key' => join( ',', map { defined $_ ? $_ : '.'  }
-                                             map { $bond_atom_ids->[$_] } 0..3 ),
+                'origin_atom_ids_key' =>
+                    join( ',', map { defined $_ ? $_ : '.'  }
+                               map { $bond_atom_ids->[$_] } 0..3 ),
                 'value' => $dihedral_angles_cache{$dihedral_angle_key},
                 'alt_name' => $alt_rotatable_bond_name
             };
@@ -380,8 +381,9 @@ sub stretchable_bonds
                 'order' => $bond_order{$bond_atom_ids->[0]}{$bond_atom_ids->[1]},
                 'rank' => 3,
                 'atom_ids' => $bond_atom_ids,
-                'atom_ids_key' => join( ',', map { defined $_ ? $_ : '.'  }
-                                             map { $bond_atom_ids->[$_] } 0..3 ),
+                'origin_atom_ids_key' =>
+                    join( ',', map { defined $_ ? $_ : '.'  }
+                               map { $bond_atom_ids->[$_] } 0..3 ),
                 'value' => $bond_lengths_cache{$bond_length_key},
                 'alt_name' => $alt_stretchable_bond_name
             };
@@ -529,8 +531,9 @@ sub bendable_angles
                                       {$bond_atom_ids->[2]},
                 'rank' => 2,
                 'atom_ids' => $bond_atom_ids,
-                'atom_ids_key' => join( ',', map { defined $_ ? $_ : '.'  }
-                                             map { $bond_atom_ids->[$_] } 0..3 ),
+                'origin_atom_ids_key' =>
+                    join( ',', map { defined $_ ? $_ : '.'  }
+                               map { $bond_atom_ids->[$_] } 0..3 ),
                 'value' => $bond_angles_cache{$bond_angle_key},
                 'alt_name' => $alt_bendable_angle_name
             };
