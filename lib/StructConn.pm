22645bdef7aff1d94251904a51a2cfba13f4b6db
Algirdas <algirdas.grybauskas@yahoo.com>
2025-12-05 21:01:29 +0200

    Retrieving and storing '_struct_conn' data in rotag_connect script.

---
+++ lib/StructConn.pm
@@ -0,0 +1,19 @@
+package StructConn;
+
+use strict;
+use warnings;
+
+use Exporter qw( import );
+our @EXPORT_OK = qw( create_pdbx_entry );
+
+use Carp;
+
+use Version qw( $VERSION );
+
+our $VERSION = $VERSION;
+
+# ----------------------------------------------------------------------------- #
+
+sub create_struct_conn
+{
+}
