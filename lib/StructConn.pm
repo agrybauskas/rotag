379ac1830efa4d59518aaac54076924e836027da
agrybauskas <algirdas.grybauskas@yahoo.com>
2025-12-17 13:47:26 +0200

    As the addition of certain data items were moved to rotag_add script, removed defaults in create_struct_conn().

--- lib/StructConn.pm
+++ lib/StructConn.pm
@@ -24,28 +24,18 @@ sub create_struct_conn
         "$type$id" => {
             'id' => "$type$id",
             'conn_type_id' => "$type",
-            'ptnr1_label_seq_id' =>
-                $atom1->{'group_PDB'} eq 'ATOM' ?
-                $atom1->{'label_seq_id'} : '.',
+            'ptnr1_label_seq_id' => $atom1->{'label_seq_id'},
             'ptnr1_label_asym_id' => $atom1->{'label_asym_id'},
             'pdbx_ptnr1_label_alt_id' => $atom1->{'label_alt_id'},
             'ptnr1_auth_seq_id' => $atom1->{'auth_seq_id'},
-            'ptnr1_auth_asym_id' =>
-                $atom1->{'auth_asym_id'} eq '?' ?
-                $atom2->{'auth_asym_id'} :
-                $atom1->{'auth_asym_id'},
+            'ptnr1_auth_asym_id' => $atom1->{'auth_asym_id'},
             '[local]_ptnr1_pdbx_PDB_model_num' => $atom1->{'pdbx_PDB_model_num'},
             'ptnr1_label_atom_id' => $atom1->{'label_atom_id'},
-            'ptnr2_label_seq_id' =>
-                $atom2->{'group_PDB'} eq 'ATOM' ?
-                $atom2->{'label_seq_id'} : '.',
+            'ptnr2_label_seq_id' => $atom2->{'label_seq_id'},
             'ptnr2_label_asym_id' => $atom2->{'label_asym_id'},
             'pdbx_ptnr2_label_alt_id' => $atom2->{'label_alt_id'},
             'ptnr2_auth_seq_id' => $atom2->{'auth_seq_id'},
-            'ptnr2_auth_asym_id' =>
-                $atom2->{'auth_asym_id'} eq '?' ?
-                $atom1->{'auth_asym_id'} :
-                $atom2->{'auth_asym_id'},
+            'ptnr2_auth_asym_id' => $atom2->{'auth_asym_id'},
             '[local]_ptnr2_pdbx_PDB_model_num' => $atom2->{'pdbx_PDB_model_num'},
             'ptnr2_label_atom_id' => $atom2->{'label_atom_id'},
         }
