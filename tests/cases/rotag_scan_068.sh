8ad5ed0d710881180274f342f9cd2110ddb67185
Algirdas <algirdas.grybauskas@yahoo.com>
2025-12-24 14:36:48 +0200

    Noticed that wrong bond parameters were written in the test case. Fixed it.

--- tests/cases/rotag_scan_068.sh
+++ tests/cases/rotag_scan_068.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/h2o-and-mg-with-sidechains-with-connections-002.cif
 
-rotag_scan -H -a '!; OD1.MG=3.0..0.5..3.5' ${pdbx_file}
+rotag_scan -H -a '!; OD1.H1=3.0..0.5..3.5; O.MG=3.0..0.5..3.5' ${pdbx_file}
