14eb9f464ae24f261e3aee083d9122321c9b2f2c
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-01-07 15:09:20 +0200

    Fixed libraries as bond parameter names were wrong. Also, patched test outputs.

--- tests/cases/rotag_scan_071.sh
+++ tests/cases/rotag_scan_071.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/hetatoms/h2o-and-mg-with-sidechains-with-connections-002.cif
 
-rotag_scan -H -a '!; OD1.H1-O.MG=170.0..10.0..180.0' ${pdbx_file}
+rotag_scan -H -a '!; CB-CG-OD1.H1=170.0..10.0..180.0; CG-OD1.H1-O=170.0..10.0..180.0; OD1.H1-O.MG=170.0..10.0..180.0' ${pdbx_file}
