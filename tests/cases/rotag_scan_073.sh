9ee7c21760d17fcda80fb3834d07a4e0c06546d6
Algirdas <algirdas.grybauskas@yahoo.com>
2026-02-24 17:13:57 +0200

    Patched test output for rotag_scan script.

--- tests/cases/rotag_scan_073.sh
+++ tests/cases/rotag_scan_073.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/libraries/mg-with-sidechains-library-003.cif
+pdbx_file=$(dirname "$0")/../inputs/libraries/mg-with-sidechains-library-002.cif
 
-rotag_scan --use-library -H -M 0.1 ${pdbx_file}
+rotag_scan --use-library -H -M 0.05 ${pdbx_file}
