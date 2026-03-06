d7e67f668c4baed4ff7260bb3056d43c6704e1e7
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-03-06 12:26:45 +0200

    Started writing a module to cluster both 1D and 3D data using DBScan algorithm.

---
+++ lib/DBScan.pm
@@ -0,0 +1,23 @@
+package DBScan;
+
+use strict;
+use warnings;
+
+use Exporter qw( import );
+our @EXPORT_OK = qw();
+
+use Version qw( $VERSION );
+
+our $VERSION = $VERSION;
+
+# ----------------------------------- DBScan ---------------------------------- #
+
+#
+#
+# Input:
+#     . - .
+# Output:
+#     . - .
+#
+
+1;
