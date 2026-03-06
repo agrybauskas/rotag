565d6c64af01d06f53ba8b5b8b27cb0e8bd5a730
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-03-06 14:24:26 +0200

    Started describing arguments in dbscan().

--- lib/DBScan.pm
+++ lib/DBScan.pm
@@ -13,11 +13,22 @@ our $VERSION = $VERSION;
 # ----------------------------------- DBScan ---------------------------------- #
 
 #
-#
+# Performs DBSCAN clusterization for given data points.
 # Input:
-#     . - .
+#     $points - data structure that stores both point coordination and label;
+#     $radius - distance between points to be included to the cluster;
+#     $min_points - minimum points required by the cluster;
+#     $distance_func - dinstance function to be calculated by;
 # Output:
-#     . - .
+#     labels points by cluster name.
 #
+
+sub dbscan
+{
+    my ( $points, $radius, $options ) = @_;
+    my ( $min_points, $distance_func ) =
+        ( $options->{'min_points'}, $options->{'distance_func'} );
+    $min_points //= 1;
+}
 
 1;
