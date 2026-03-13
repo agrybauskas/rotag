326b85581fd16933e3c01e9b7e444f2c44d916f4
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-03-13 11:29:41 +0200

    Added test cases to check if DBScan module works properly with 3D coordinate points.

---
+++ tests/cases/DBScan_003.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use DBScan;
+
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 1;
+
+my $cluster =
+    DBScan->new( [ [ -31.184, -6.586, -1.238 ],
+                   [ -31.549400, -8.643799, 6.083401 ],
+                   [ -30.882276, -8.018614, 5.698467 ],
+                   [ -30.770998, -7.217951, 5.123454 ],
+                   [ -31.258069, -6.547636, 4.577997 ],
+                   [ -32.157446, -6.263706, 4.270443 ] ] );
+
+$cluster->dbscan( 0.98 );
+
+for my $id ( sort { $a <=> $b } @{ $cluster->get_ids() } ) {
+    print $id . " " . join( ',', @{ $cluster->get_point( $id )->{'coord'} } ) .
+          " " . $cluster->get_point( $id )->{'label'} . "\n";
+}
