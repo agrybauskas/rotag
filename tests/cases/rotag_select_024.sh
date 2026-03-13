d0e81b89932e52b93b44c0c9d2fb1e166801dc2e
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-03-13 16:27:01 +0200

    Added a test case for rotag_select script to check how related '_atom_site' and '_struct_conn' data can be retrieved.

---
+++ tests/cases/rotag_select_024.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pdbx_file=$(dirname "$0")/../inputs/libraries/mg-with-sidechains-with-connections-library-001.cif
+
+rotag_select -t 'atomname MG' --related-data --tags '_atom_site,_struct_conn' ${pdbx_file}
