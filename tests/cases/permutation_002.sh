104d26f8364bd117f4d30fc581a127a6851695f4
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-03-31 12:49:17 +0300

    Completed permutation test case for paired variables.

--- tests/cases/permutation_002.sh
+++ tests/cases/permutation_002.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Combinatorics qw( permutation );
+
+my $permuted_list = permutation( 2,
+                                 [],
+                                 [ [ [ 0 ], [ 1 ], [ 2 ] ],
+                                   [ [ 3, 5 ], [ 6, 7 ] ] ],
+                                 [] );
+$permuted_list = [ map { [ @{ $_->[0] }, @{ $_->[1] } ] } @{ $permuted_list } ];
+
+print( join( " ", @{ $_ } ), "\n" ) for @{ $permuted_list };
