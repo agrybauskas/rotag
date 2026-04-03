52aabdce01369890c73dbbfbc8aaee2785af1e1a
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-04-03 13:58:53 +0300

    Patched tests after adding seeds for the random number generation.

--- tests/cases/rotag_scan_078.sh
+++ tests/cases/rotag_scan_078.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/libraries/mg-with-sidechains-library-002.cif
 
-rotag_scan --use-library -H -X 10 ${pdbx_file}
+rotag_scan --use-library -H -x 23 -X 10 ${pdbx_file}
