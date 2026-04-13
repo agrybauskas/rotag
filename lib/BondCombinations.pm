72b765667f4715ef4b84149ab1d7c011ea815d4b
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-04-13 16:39:34 +0300

    Finished the code part to track longest bond parameter combinations for each parameter name.

--- lib/BondCombinations.pm
+++ lib/BondCombinations.pm
@@ -51,6 +51,14 @@ sub combine_permuted_values
 
     # # Identifies the longest parameter key for given parameter name.
     # my %parameter_longest = ();
+    # for my $parameter_name ( @{ $names } ) {
+    #     for my $parameter_key ( @parameter_keys_sorted_rev ) {
+    #         if( $parameter_key =~ m/\Q$parameter_name\E$/ ) {
+    #             $parameter_longest{$parameter_name} = $parameter_key;
+    #             last;
+    #         }
+    #     }
+    # }
 
     # Finds unique values for each parameter.
     my %visited_parameters = ();
