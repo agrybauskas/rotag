8cedf8c5c2a5e137cf8dbf8ba19a305422c18eab
Algirdas <algirdas.grybauskas@yahoo.com>
2026-04-22 11:57:29 +0300

    Connected heteroatom in test input.

--- tests/cases/rotag_scan_083.sh
+++ tests/cases/rotag_scan_083.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdbx_file=$(dirname "$0")/../inputs/fragments/fragment-selected-003.cif
+pdbx_file=$(dirname "$0")/../inputs/fragments/fragment-selected-004.cif
 
 rotag_scan -H --pairwise --limit 5 ${pdbx_file}
