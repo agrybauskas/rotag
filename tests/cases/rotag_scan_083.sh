b4aeff5c37eabccfd215d04b711e05bdc1024c35
Algirdas <algirdas.grybauskas@yahoo.com>
2026-05-07 15:32:30 +0300

    Previously, used wrong options for rotag_scan in the test case. Fixed it.

--- tests/cases/rotag_scan_083.sh
+++ tests/cases/rotag_scan_083.sh
@@ -2,4 +2,4 @@
 
 pdbx_file=$(dirname "$0")/../inputs/fragments/fragment-selected-004.cif
 
-rotag_scan -H --pairwise --limit 5 -x 5 -X 23 ${pdbx_file}
+rotag_scan -H --pairwise --limit 5 -x 23 -X 5 ${pdbx_file}
