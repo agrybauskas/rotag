54fa2920f68c0f3734cb18888fee3d135d0cf492
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-08-26 12:51:24 +0300

    Changed the initial options for "split_by" test case to precisely differentiate the labels.

--- tests/cases/split_by_004.sh
+++ tests/cases/split_by_004.sh
@@ -3,9 +3,9 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/libraries/mg-with-sidechains-with-connections-library-001.dump
-split_by_attributes="pdbx_PDB_model_num,label_asym_id,label_alt_id"
+split_by_attributes="pdbx_PDB_model_num,label_asym_id,label_alt_id,pdbx_auth_alt_id"
 append_to_alt_ids=0
-append='label_alt_id=.'
+append='label_asym_id=A,label_alt_id=.,pdbx_auth_alt_id=.'
 
 "$(dirname "$0")"/../scripts/split_by ${split_by_attributes} \
                                       ${pdbx_dump_file} \
