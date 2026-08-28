431a740fc23dad49d8b3d86d47c0ac02a838afdf
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-08-28 14:15:20 +0300

    Changed splitting attributes in "split_by" test cases.

--- tests/cases/split_by_004.sh
+++ tests/cases/split_by_004.sh
@@ -3,7 +3,7 @@
 export PERL5LIB=$(dirname "$0")/../../lib
 
 pdbx_dump_file=$(dirname "$0")/../inputs/libraries/mg-with-sidechains-with-connections-library-001.dump
-split_by_attributes="pdbx_PDB_model_num,label_asym_id,label_alt_id,pdbx_auth_alt_id"
+split_by_attributes="pdbx_PDB_model_num,label_asym_id"
 append_to_alt_ids=0
 append='label_asym_id=A,label_alt_id=.,pdbx_auth_alt_id=.'
 
