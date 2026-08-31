a5218632c179c70ef567f0a3f871ec03175c0b85
agrybauskas <algirdas.grybauskas@yahoo.com>
2026-08-31 16:48:19 +0300

    Added grouping attribute values.

--- lib/PDBxParser.pm
+++ lib/PDBxParser.pm
@@ -1560,6 +1560,10 @@ sub group_unique_residue_keys
         'label_seq_id', 'label_asym_id', 'pdbx_PDB_model_num', 'label_alt_id',
         'auth_seq_id', 'auth_asym_id', 'pdbx_auth_alt_id'
     );
+    my %group_attribute_values = (
+        'label_alt_id' => '.',
+        'pdbx_auth_alt_id' => '.',
+    );
     my %unique_residue_keys = ();
     my $id = 1;
     for my $unique_residue_key ( @{ $unique_residue_keys } ) {
